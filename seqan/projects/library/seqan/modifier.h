1dc46868562f9d40f183789918ce7a083ce8326f
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-02-06 10:00:47 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@84 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/seqan/modifier.h
@@ -0,0 +1,10 @@
+#ifndef SEQAN_HEADER_MODIFIER_H
+#define SEQAN_HEADER_MODIFIER_H
+
+
+#include <seqan/sequence.h>
+#include <seqan/modifier/modifier_string.h>
+#include <seqan/modifier/modifier_iterator.h>
+
+
+#endif //#ifndef SEQAN_HEADER_...
