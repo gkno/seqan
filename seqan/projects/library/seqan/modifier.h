b6e0de47dd5ff8873d4211842fe08fd2f51f727f
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-02-09 13:33:14 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@99 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/modifier.h
+++ seqan/projects/library/seqan/modifier.h
@@ -1,11 +1,15 @@
 #ifndef SEQAN_HEADER_MODIFIER_H
 #define SEQAN_HEADER_MODIFIER_H
 
+#include <functional>
+
+//____________________________________________________________________________
 
 #include <seqan/sequence.h>
 #include <seqan/modifier/modifier_iterator.h>
 #include <seqan/modifier/modifier_string.h>
 
+#include <seqan/modifier/modifier_functors.h>
 #include <seqan/modifier/modifier_view.h>
 
 
