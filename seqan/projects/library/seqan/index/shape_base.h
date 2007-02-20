380270ca909b457c557faa8cb3044bde30a12c0e
emde@PCPOOL.MI.FU-BERLIN.DE <emde@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-02-20 16:07:54 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@141 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/index/shape_base.h
+++ seqan/projects/library/seqan/index/shape_base.h
@@ -5,82 +5,147 @@ namespace SEQAN_NAMESPACE_MAIN
 {
 
 struct SimpleShape;
-struct GappedShape1;
-struct GappedShape2;
-struct GappedShape3;
-
-// Objekt Shape, klassiefiziert die Seedart. Speichert die Anzahl und die genauen Stellen innerhalb eines q-Grams,
-// die bei der Seed-Suche irrelevant sind.
-// Weiterhin speichert es die Alpabhet-größe, die Größe des q-Grams q und berechnet damit einen für die 
-// Berechnung des hash-Wertes des entsprechenden q-Grams benötigten term.
-// Ist die Anzhal irrelevanter Stellen = 0, handelt es sich um exakte Seeds, sonst um exakte gegappte seeds.
+struct GappedShape;
 
 
-template <typename TString = Dna, typename TSpec = GappedShape3>
+/**
+.Class.Shape:
+..cat:Index
+..summary:Stores a shape for an ungapped or gapped q-gram.
+..signature:Shape<TString, TSpec>
+..remarks:The @Metafunction.ValueSize@ of Shape is the ValueSize of TString which is the alphabet size.
+..param.TString:The type of string the q-gram shape is applied to (e.g. $Dna$).
+..param.TSpec:The specializing type.
+...default:$GappedShape$, for gapped q-grams.
+*/
+template <typename TString = Dna, typename TSpec = GappedShape>
 class Shape;
 
+
+
+
+inline int intPow(int a, int b)
+{
+SEQAN_CHECKPOINT
+	int ret = 1;
+	while(b!=0)
+	{
+		if(b & 1)
+			ret*=a;
+		a*=a;
+		b >>= 1;
+	}	
+	
+	return ret;
+}
+
+
+
+/**
+Spec.SimpleShape
+..cat:Index
+..general:Class.Shape
+..summary:For ungapped q-grams.
+*/
 template<typename TString>
 class Shape<TString, SimpleShape>
 {
 
 public:
 
-	int q;
-	int const term;
+	int span;
+	int /*const*/ term;
 	
-
-	// Konstruktor
-	Shape( int q, int alp_size):
-	q(q),
-	term(intPow(alp_size, q-1))
+/**
+.Shape#Shape:
+..class:Class.Shape
+..summary:Constructor
+..signature:Shape<TString, TSpec> ()
+..signature:Shape<TString, TSpec> (shape)
+..signature:Shape<TString, SimpleShape> (span)
+..signature:Shape<TString, GappedShape> (num_blanks, span)
+..param.shape:Other Shape object. (copy constructor)
+..param.span:Total length of the q-gram (including blanks).
+..param.num_blanks:Number of blanks (irrelevant positions).
+*/
+	Shape()
 	{
+SEQAN_CHECKPOINT
+	}
+
+	Shape( int span):
+	span(span),
+	term(intPow( ValueSize<TString>::VALUE, span-1))
+	{
+SEQAN_CHECKPOINT
 	}
 
 	// Kopierkonstruktor
 	Shape(Shape const & rhs):
-	q(rhs.q),
-	term(rhs.term),
+	span(rhs.span),
+	term(rhs.term)
 	{
+SEQAN_CHECKPOINT
 	}	
 
 
-	~Shape(){}
+	~Shape()
+	{
+SEQAN_CHECKPOINT
+	}
 	
 };
 
 
 
-
-
+//------------------- Functions -----------------------------
+/**.Function.shapeSpan:
+..cat:Index
+..summary:Span of a q-gram.
+..signature:shapeSpan(object)
+..param.object.type:Class.Shape
+..returns:Span of object.
+*/
 template <typename TString, typename TSpec>
 inline int &
-powTerm(Shape<TString, TSpec>  & me)
+shapeSpan(Shape<TString, TSpec> & me)
 {
-	return me.term;
+SEQAN_CHECKPOINT
+	return me.span;
 }
 
-template <typename TString, typename TSpec>
-inline int &
-seedSize(Shape<TString, TSpec> & me)
+/**.Function.shapeCountBlanks:
+..cat:Index
+..summary:Number of blanks (irrelevant positions) in a shape.
+..signature:shapeCountBlanks(object)
+..param.object.type:Class.Shape
+..returns:Number of blanks in object.
+*/
+template <typename TString>
+inline int 
+shapeCountBlanks(Shape<TString, SimpleShape>  & me)
 {
-	return me.q;
+SEQAN_CHECKPOINT
+	return 0;
 }
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Hashfunktion, weist einem q-Gram eine Position zu (alphabetisch)
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**.Function.hash:
+..cat:Index
+..summary:Computes a hash value for a q-gram (alphabetical position value).
+..signature:hash(shape,q_gram)
+..param.shape:Shape to be used for hashing the q-gram.
+...type:Class.Shape
+..param.q_gram:Sequence iterator pointing to the first position of the q-gram.
+..returns:Hash value of the q_gram.
+*/
 template <typename TString, typename TSequenceIterator>
-int 
+typename Size<Shape<TString, SimpleShape> >::Type
 hash(Shape<TString, SimpleShape> & shape, TSequenceIterator qgram_it)	
 {
-
-//	typename Size<Shape<TString,SimpleShape> >::Type const alp_size = ValueSize<Shape<TString, SimpleShape> >::VALUE;
-
-	int pos = 0;			// resultierende Position des q-Grams
+SEQAN_CHECKPOINT
+	typename Size<Shape<TString, SimpleShape> >::Type pos = 0;		
 	int i = 0;
-	int span = seedSize(shape);
+	int span = shapeSpan(shape);
 	while(i < span)
 	{
 		pos = pos * ValueSize<Shape<TString, SimpleShape> >::VALUE + (int)*qgram_it;
@@ -92,23 +157,46 @@ hash(Shape<TString, SimpleShape> & shape, TSequenceIterator qgram_it)
 	return pos;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Hash_next, weist einem q-Gram eine Position zu (alphabetisch) (bei exakten Seeds abgeleitete Berechnung,
-// bei exakte gegappte Seeds Aufruf von hash)
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+.Function.hashNext:
+..cat:Index
+..summary:Computes the hash value for the next q-gram given the q-gram before (that starts one 
+position before the next q-gram) and its hash value.
+..signature:hashNext(shape,q_gram1,qgram2,hash1)
+..param.shape:Shape to be used for hashing the q-gram.
+...type:Class.Shape
+..param.q_gram1:Sequence iterator pointing to first position of the q-gram that the 
+hash value is supposed to be determined for.
+..param.q_gram1:Sequence iterator pointing to first position of the last q-gram.
+..param.hash1:Hash value of the last q-gram.
+..returns:Hash value of q_gram1.
+*/
 template <typename TSequenceIterator, typename TString>
-int
-hash_next(Shape<TString, SimpleShape> & shape, TSequenceIterator qgram_1_it, TSequenceIterator qgram_2_it, int & x)
+typename Size<Shape<TString, SimpleShape> >::Type
+hashNext(Shape<TString, SimpleShape> & shape, TSequenceIterator qgram_1_it, 
+		 TSequenceIterator qgram_2_it, 
+		 typename Size<Shape<TString, SimpleShape> >::Type & x)
 {
+SEQAN_CHECKPOINT
 
-//	typename Size<Shape<TString,SimpleShape> >::Type const alp_size = ValueSize<Shape<TString, SimpleShape> >::VALUE;
-	int term = powTerm(shape);
-	int span = seedSize(shape);
-	int y;
-    y = (x - (int)*qgram_1_it * term) * ValueSize<Shape<TString, SimpleShape> >::VALUE + (int)*(qgram_2_it+span);
+	typedef typename Size<Shape<TString, SimpleShape> >::Type TValue;
+	int span = shapeSpan(shape);
+	TValue y;
+    y = (x - (int)*qgram_1_it * shape.term) * ValueSize<Shape<TString, SimpleShape> >::VALUE + (int)*(qgram_2_it+span-1);
 	
 	return y;
 }
+
+
+
+template <typename TString>
+void
+stringToShape(Shape<TString,SimpleShape> & shape, String<char> & shape_string)
+{
+	shape.span = length(shape_string);
+	shape.term = intPow(ValueSize<TString>::VALUE, length(shape_string)-1);
+}
+
 
 
 
@@ -130,6 +218,7 @@ struct Value<Shape<TString,TSpec> >
 	typedef int Type;
 };
 
+///.Metafunction.ValueSize.param.T.type:Class.Shape
 template <typename TString, typename TSpec>
 struct ValueSize< Shape<TString, TSpec> > {
 	enum { VALUE = ValueSize<TString>::VALUE }; 
