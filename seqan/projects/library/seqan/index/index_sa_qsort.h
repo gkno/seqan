d921cde1e7d3c63ee239d270216851149c8ad297
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-05-04 15:43:21 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@501 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/seqan/index/index_sa_qsort.h
@@ -0,0 +1,109 @@
+/*
+ *  index_sa_qsort.h
+ *  SeqAn
+ *
+ *  Created by David Weese on 22.04.07.
+ *
+ */
+
+#ifndef SEQAN_HEADER_INDEX_SA_QSORT_H
+#define SEQAN_HEADER_INDEX_SA_QSORT_H
+
+namespace SEQAN_NAMESPACE_MAIN
+{
+
+	struct SAQSort {};
+
+    template < typename TSAValue, typename TText >
+	struct _SuffixLess : 
+		public ::std::binary_function < TSAValue, TSAValue, bool >
+    {
+		typedef typename Iterator<TText>::Type TIter;
+		TIter _begin, _end;
+
+		_SuffixLess() {}
+		_SuffixLess(TText &text): 
+			_begin(begin(text)),
+			_end(end(text)) {}
+
+		inline bool operator() (TSAValue const a, TSAValue const b) const 
+		{
+			TIter itA = _begin + a;
+			TIter itB = _begin + b;
+			if (a <= b) {
+				for(; itB != _end; ++itB, ++itA) {
+					if (lexLess(*itA, *itB)) return true;
+					if (lexLess(*itB, *itA)) return false;
+				}
+				return false;
+			} else {
+				for(; itA != _end; ++itA, ++itB) {
+					if (lexLess(*itA, *itB)) return true;
+					if (lexLess(*itB, *itA)) return false;
+				}
+				return true;
+			}
+		}
+	};
+
+    template < typename TSA,
+               typename TText >
+    inline void createSuffixArray(
+		TSA &SA,
+		TText &s,
+		SAQSort const &)
+	{
+		typedef typename Size<TSA>::Type TSize;
+		typedef typename Iterator<TSA, Standard>::Type TIter;
+
+		// 1. Fill suffix array with a permutation (the identity)
+		TIter it = begin(SA, Standard());
+		TIter itEnd = end(SA, Standard());
+		for(TSize i = 0; it != itEnd; ++it, ++i)
+			*it = i;
+
+		// 2. Sort suffix array with quicksort
+		sort(
+			begin(SA, Standard()), 
+			end(SA, Standard()), 
+			_SuffixLess<typename Value<TSA>::Type, TText const>(s));
+	}
+
+    //////////////////////////////////////////////////////////////////////////////
+    // suffix quicksort pipe
+    template < typename TInput >
+    struct Pipe< TInput, SAQSort >
+    {
+		typedef typename Value<TInput>::Type	TValue;
+		typedef typename SAValue<TInput>::Type	TSAValue;
+
+		typedef String<TValue, Alloc<> >		TText;
+		typedef String<TSAValue, Alloc<> >		TSA;
+		typedef Pipe<TSA, Source<> >			TSource;
+
+		TSA		sa;
+		TSource	in;
+
+		Pipe(TInput &_textIn):
+			in(sa)
+		{
+			TText text;
+			text << _textIn;
+
+			resize(sa, length(_textIn), Exact());
+			createSuffixArray(sa, text, SAQSort());
+		}
+
+		inline typename Value<TSource>::Type const & operator*() {
+            return *in;
+        }
+        
+        inline Pipe& operator++() {
+            ++in;
+            return *this;
+        }        
+	};
+
+}
+
+#endif
