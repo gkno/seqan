677e30444f3484282e2846d33b9a2fe13af90ff7
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-05-04 15:37:31 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@499 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/index.h
+++ seqan/projects/library/seqan/index.h
@@ -9,6 +9,7 @@
 #include <seqan/modifier.h>
 
 #include <seqan/find/find_base.h>
+#include <seqan/misc/misc_set.h>
 
 #include <climits>
 #include <functional>
@@ -20,8 +21,10 @@
 #include <utility>
 #include <string.h> // memset
 
-//____________________________________________________________________________
-// index construction algorithms
+
+//////////////////////////////////////////////////////////////////////////////
+// INDEX CONSTRUCTION
+//////////////////////////////////////////////////////////////////////////////
 
 #ifdef SEQAN_SWITCH_USE_FORWARDS
 #include <seqan/index/index_generated_forwards.h>
@@ -29,7 +32,15 @@
 
 #include <seqan/index/index_base.h>
 
+//____________________________________________________________________________
+// suffix array creators
+
 #include <seqan/index/radix.h>
+#include <seqan/index/index_sa_btree.h>
+#include <seqan/index/index_sa_lss.h>
+#include <seqan/index/index_sa_mm.h>
+#include <seqan/index/index_sa_qsort.h>
+
 #include <seqan/index/pump_extender3.h>
 #include <seqan/index/pipe_merger3.h>
 #include <seqan/index/index_skew3.h>
@@ -41,8 +52,8 @@
 #include <seqan/index/pump_separator7.h>
 #include <seqan/index/index_skew7_multi.h>
 
-#include <seqan/index/index_mm.h>
-#include <seqan/index/index_sa_btree.h>
+//____________________________________________________________________________
+// enhanced table creators
 
 #include <seqan/index/pump_lcp_core.h>
 #include <seqan/index/index_lcp.h>
@@ -52,20 +63,24 @@
 #include <seqan/index/index_bwt.h>
 
 //____________________________________________________________________________
-// q-gram index
+// q-gram index creator
 
 #include <seqan/index/shape_base.h>
 //#include <seqan/index/shape_gapped.h>
 #include <seqan/index/index_qgram.h>
 
-//____________________________________________________________________________
-// enhanced suffix array (suffix tree)
+
+//////////////////////////////////////////////////////////////////////////////
+// INDEX USAGE
+//////////////////////////////////////////////////////////////////////////////
 
 #include <seqan/index/index_find.h>
 #include <seqan/index/index_shims.h>
 
+//____________________________________________________________________________
+// (virtual) suffix tree
+
 #include <seqan/index/index_esa_base.h>
-#include <seqan/misc/misc_set.h>
 #include <seqan/index/index_esa_stree.h>
 #include <seqan/index/index_esa_algs.h>
 #include <seqan/index/index_esa_drawing.h>
