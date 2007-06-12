792800a8ba7eb34d2774752d466c5424c50103ff
doering@PCPOOL.MI.FU-BERLIN.DE <doering@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-06-12 16:52:36 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@689 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/chaining.h
+++ seqan/projects/library/seqan/chaining.h
@@ -14,6 +14,12 @@
 
 #include <seqan/misc/misc_random.h>
 
+#ifdef SEQAN_SWITCH_USE_FORWARDS
+#include <seqan/chaining/chaining_generated_forwards.h>
+#endif
+
+//Skip List
+
 #include <seqan/chaining/skip_list_type.h>
 #include <seqan/chaining/skip_list_base.h>
 #include <seqan/chaining/skip_list_iterator.h>
@@ -21,5 +27,18 @@
 #include <seqan/chaining/skip_element.h>
 #include <seqan/chaining/skip_list_impl.h>
 #include <seqan/chaining/skip_list_dynamic.h>
+
+//Range Tree
+
+#include <seqan/chaining/rt_base.h>
+#include <seqan/chaining/rt_skip_element.h>
+#include <seqan/chaining/rt_skip_base_element.h>
+#include <seqan/chaining/rt_sl_impl.h>
+#include <seqan/chaining/rt_sl_base.h>
+#include <seqan/chaining/rt_base2.h>
+#include <seqan/chaining/rt_common_algos.h>
+#include <seqan/chaining/rt_impl.h>
+#include <seqan/chaining/rt_sl_def_algos.h>
+#include <seqan/chaining/rt_sl_compl_algos.h>
 
 #endif // SEQAN_HEADER_SKIP_LIST
