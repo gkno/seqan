c8506ca9757a61fb20a50f0e2029797efe41a146
doering@PCPOOL.MI.FU-BERLIN.DE <doering@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-07-01 17:10:41 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@782 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/tests/score/test_score.cpp
+++ seqan/projects/tests/score/test_score.cpp
@@ -13,7 +13,31 @@ using namespace std;
 using namespace seqan;
 
 //////////////////////////////////////////////////////////////////////////////
+//compare to http://www.bioinformatics.nl/tools/pam.html
 
+void testScorePAM()
+{
+	PamDayhoff pam;
+	//Score<int, Pam<> > pam(250, -1, 0);
+	//Score<int, Pam<AminoAcid, Pam_Data_Dayhoff_MDM78> > pam;
+
+	SEQAN_TASSERT(getDist(pam) == 250);
+	SEQAN_TASSERT(scoreGapExtend(pam) == -1);
+	SEQAN_TASSERT(scoreGapOpen(pam) == 0);
+
+	//print out the matrix
+	for (unsigned int i = 0; i < 24; ++i)
+	{
+		AminoAcid a = i;
+		cout << a << " ";
+		for (unsigned int j = 0; j < 24; ++j)
+		{
+			AminoAcid b = j;
+			printf("%2i ", scorePair(pam, a, b));
+		}
+		cout << "\n";
+	}
+}
 
 
 //////////////////////////////////////////////////////////////////////////////
@@ -21,6 +45,9 @@ using namespace seqan;
 int main() 
 {
 	SEQAN_TREPORT("TEST BEGIN")
+
+	testScorePAM();
+
 //____________________________________________________________________________
 
 	SEQAN_TREPORT("TEST END")
