69983fda311ad17fe47c7a5d8ae3072d1161838d
doering@PCPOOL.MI.FU-BERLIN.DE <doering@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-07-02 14:34:51 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@788 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/demos/index.cpp
+++ seqan/projects/demos/index.cpp
@@ -1,17 +1,27 @@
 #include <seqan/index.h>
 using namespace seqan;
+using namespace std;
 
 int main ()
 {
 ///The following code builds a suffix array and searches in it
-    Index< String<char> > index("tobeornottobe");
-	Finder< Index< String<char> > > finder(index);
+    Index< String<char> > index_esa("tobeornottobe");
+	Finder< Index< String<char> > > finder_esa(index_esa);
 
-	std::cout << "hit at ";
-	while (find(finder, "be"))
-		std::cout << position(finder) << " ";
-	std::cout << ::std::endl;
+	cout << "hit at ";
+	while (find(finder_esa, "be"))
+		cout << position(finder_esa) << " ";
+	cout << endl;
 
+///Doing the same using a q-gram index (q==2)
+	typedef Index< String<char>, Index_QGram< FixedShape<2> > > TQGramIndex;
+    TQGramIndex index_2gram("tobeornottobe");
+	Finder<TQGramIndex> finder_2gram(index_2gram);
+
+	cout << "hit at ";
+	while (find(finder_2gram, "be"))
+		cout << position(finder_2gram) << " ";
+	cout << endl;
 
 	return 0;
 }
