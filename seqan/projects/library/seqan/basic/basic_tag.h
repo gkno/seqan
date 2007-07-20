22cfe65063be6f38cd585371e79d22f00e16433e
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-07-20 13:20:47 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@912 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/seqan/basic/basic_tag.h
@@ -0,0 +1,24 @@
+#ifndef SEQAN_HEADER_BASIC_TAG_H
+#define SEQAN_HEADER_BASIC_TAG_H
+
+namespace SEQAN_NAMESPACE_MAIN
+{
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+/**
+.Tag.DotDrawing
+..summary:Switch to trigger drawing in dot format.
+..value.DotDrawing:Graphs in dot format.
+*/
+
+struct DotDrawing_;
+typedef Tag<DotDrawing_> const DotDrawing;
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+}// namespace SEQAN_NAMESPACE_MAIN
+
+#endif //#ifndef SEQAN_HEADER_...
