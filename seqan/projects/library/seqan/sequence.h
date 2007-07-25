09de64c7fe509e8c492e3a0ffa28edb84365ddb7
doering@PCPOOL.MI.FU-BERLIN.DE <doering@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-07-25 13:55:40 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@930 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/sequence.h
+++ seqan/projects/library/seqan/sequence.h
@@ -1,12 +1,22 @@
-//============================================================================
-// SeqAn - The Library for Sequence Analysis
-// http://www.seqan.de 
+//////////////////////////////////////////////////////////////////////////////
+//                SeqAn - The Library for Sequence Analysis
+//                          http://www.seqan.de 
 //
-//============================================================================
-// $Author$
-// $Revision$
-// $Date$
-//============================================================================
+//  Copyright (C) 2007 authors of SeqAn
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU Lesser General Public
+//  License as published by the Free Software Foundation; either
+//  version 3 of the License, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//	Lesser General Public License for more details.
+//
+//////////////////////////////////////////////////////////////////////////////
+//  $Id$
+//////////////////////////////////////////////////////////////////////////////
 
 #ifndef SEQAN_HEADER_SEQUENCE_H
 #define SEQAN_HEADER_SEQUENCE_H
