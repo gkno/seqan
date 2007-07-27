259c06d1e819b14f41fe34cbc45e7366bbed8abe
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-07-27 12:50:38 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@956 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/misc/snapshot/main.cpp
@@ -0,0 +1,12 @@
+//Projekt, mit dem die Demos getestet werden koennen
+
+//#include "demos/allocator.cpp"
+//#include "demos/alphabet.cpp"
+//#include "demos/iterator.cpp"
+//#include "demos/rooted_iterator.cpp"
+//#include "demos/string_1.cpp"
+//#include "demos/sufarray.cpp"
+#include "demos/index.cpp"
+//#include "demos/graph.cpp"
+//#include "demos/find.cpp"
+//#include "demos/file_format.cpp"
