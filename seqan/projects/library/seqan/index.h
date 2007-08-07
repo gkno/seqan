f10adae39baf67874b2cda76ef5d1fbf5202cf55
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-08-07 07:04:39 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1003 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/index.h
+++ seqan/projects/library/seqan/index.h
@@ -107,7 +107,7 @@
 #include <seqan/index/index_esa_algs.h>
 #include <seqan/index/index_esa_algs_multi.h>
 #include <seqan/index/index_esa_drawing.h>
-#include <seqan/index/index_wotd.h>
+//#include <seqan/index/index_wotd.h>
 
 
 
