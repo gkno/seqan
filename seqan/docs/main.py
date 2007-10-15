32d8e4941e9aa75d8c2b2d115fc5eced840521aa
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-10-15 12:19:31 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1162 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/docs/main.py
+++ seqan/docs/main.py
@@ -1,30 +1,33 @@
 import dddoc
 import dddoc_html
 import sys
+import os
 
 # Command line: main.py <inpath> [ <module> ]*
 
-if len(sys.argv) < 2: inpath = "..\\..\\projects\\library"
+if len(sys.argv) < 2: inpath = "../../projects/library"
 else: inpath =  sys.argv[1]
 
 print("Welcome to Dot.Dot.Doc")
 
 if len(sys.argv) < 3:
     print("Scanning " + inpath + "...")
+    os.path.normpath(inpath)
     dddoc.loadFiles(inpath)
 else:
     i = 2;
     while (i < len(sys.argv)):
-        modulepath = inpath + "\\seqan\\" + sys.argv[i]
+        modulepath = inpath + "/seqan/" + sys.argv[i]
+        os.path.normpath(modulepath)
         print("Scanning " + modulepath + "...")
         dddoc.loadFiles(modulepath)
         i += 1
 
 
-print("Scanning .\\pages...")
+print("Scanning pages...")
 dddoc.loadFiles("pages")
 
-print("Scanning .\\concepts...")
+print("Scanning concepts...")
 dddoc.loadFiles("concepts")
 
 
