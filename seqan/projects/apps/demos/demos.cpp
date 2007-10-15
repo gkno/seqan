4649b99b93a4ebf3c75dba27f05870a375fa05ad
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-10-15 16:43:36 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1163 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/apps/demos/demos.cpp
+++ seqan/projects/apps/demos/demos.cpp
@@ -1,12 +1,57 @@
-//Projekt, mit dem die Demos getestet werden koennen
+// Projekt, mit dem die Demos getestet werden koennen
 
-//#include "../../demos/allocator.cpp"
-//#include "../../demos/alphabet.cpp"
-//#include "../../demos/iterator.cpp"
-//#include "../../demos/rooted_iterator.cpp"
-//#include "../../demos/string_1.cpp"
-//#include "../../demos/sufarray.cpp"
-//#include "../../demos/index.cpp"
+#define main runAllocator
+#include "../../demos/allocator.cpp"
+#undef main
+
+#define main runAlphabet
+#include "../../demos/alphabet.cpp"
+#undef main
+
+/* !kompiliert nicht!
+#define main runIterator
+#include "../../demos/iterator.cpp"
+#undef main
+*/
+
+#define main runRootedIterator
+#include "../../demos/rooted_iterator.cpp"
+#undef main
+
+#define main runString
+#include "../../demos/string_1.cpp"
+#undef main
+
+#define main runSufarray
+#include "../../demos/sufarray.cpp"
+#undef main
+
+#define main runIndex
+#include "../../demos/index.cpp"
+#undef main
+
+#define main runGraph
 #include "../../demos/graph.cpp"
-//#include "../../demos/find.cpp"
-//#include "../../demos/file_format.cpp"
+#undef main
+
+#define main runFind
+#include "../../demos/find.cpp"
+#undef main
+
+#define main runFileFormat
+#include "../../demos/file_format.cpp"
+#undef main
+
+int main() 
+{
+	runAllocator();
+	runAlphabet();
+//	runIterator();
+	runRootedIterator();
+	runString();
+	runSufarray();
+	runIndex();
+	runGraph();
+	runFind();
+	runFileFormat();
+}
