d78f39157d6ce2ac4f7c45ea75c890ef0d6bf97e
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-11-01 12:36:32 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1220 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/index/shape_predefined.h
+++ seqan/projects/library/seqan/index/shape_predefined.h
@@ -29,10 +29,84 @@ namespace SEQAN_NAMESPACE_MAIN
 //////////////////////////////////////////////////////////////////////////////
 // some predefined gapped shapes
 
-	// seed shape of PatternHunter 111010010100110111
+
+	//////////////////////////////////////////////////////////////////////////////
+	// Single seed of
+	// B.Ma and J.Tromp and M.Li, 
+	// "PatternHunter: faster and more sensitive homology search"
+	// Bioinformatics 18, 2002
+	//
+	// weight:11 
+	// length:18
+	// 
+	// shape:
+	// 111010010100110111
+
 	typedef FixedGappedShape< 
 		HardwiredShape< 1, 1, 2, 3, 2, 3, 1, 2, 1, 1 > 
-	> PatternHunterShape;
+	> ShapePatternHunter;
+
+
+
+	//////////////////////////////////////////////////////////////////////////////
+	// Multiple seeds of
+	// L.Ilie and S.Ilie, "Fast Computation of Good Multiple Spaced Seeds"
+	// WABI, 2007
+	//
+	// weight:9 
+	// length:15 
+	//
+	// shapes:
+	// 111010100100111
+	// 110100110011101
+	// 111010001011011
+	//
+	// sensitivity:
+	// 65% 0.747975		70% 0.897741
+	// 75% 0.973134		80% 0.996226
+
+	typedef FixedGappedShape< 
+		HardwiredShape< 1, 1, 2, 2, 3, 3, 1, 1 > 
+	> ShapeIlie_9_15_1;
+
+	typedef FixedGappedShape< 
+		HardwiredShape< 1, 2, 3, 1, 3, 1, 1, 2 > 
+	> ShapeIlie_9_15_2;
+
+	typedef FixedGappedShape< 
+		HardwiredShape< 1, 1, 2, 4, 2, 1, 2, 1 > 
+	> ShapeIlie_9_15_3;
+
+
+
+	//////////////////////////////////////////////////////////////////////////////
+	// Multiple seeds of
+	// L.Ilie and S.Ilie, "Fast Computation of Good Multiple Spaced Seeds"
+	// WABI 2007
+	//
+	// weight:9 
+	// length:13..23 
+	//
+	// shapes:
+	// 1110110100111
+	// 11010000110010111
+	// 11100010010000101011
+	//
+	// sensitivity:
+	// 65% 0.767413		70% 0.910949
+	// 75% 0.978558		80% 0.997357
+
+	typedef FixedGappedShape< 
+		HardwiredShape< 1, 1, 2, 1, 2, 3, 1, 1 > 
+	> ShapeIlie_9_1323_1;
+
+	typedef FixedGappedShape< 
+		HardwiredShape< 1, 2, 5, 1, 3, 2, 1, 1 > 
+	> ShapeIlie_9_1323_2;
+
+	typedef FixedGappedShape< 
+		HardwiredShape< 1, 1, 4, 3, 5, 2, 2, 1 > 
+	> ShapeIlie_9_1323_3;
 
 
 }	// namespace seqan
