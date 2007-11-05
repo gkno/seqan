c5608e918d8a0e1eac4875498f058e3b27cdf360
jhlim@PCPOOL.MI.FU-BERLIN.DE <jhlim@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-11-05 13:08:16 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1249 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/tests/find_motif/test_find_motif.cpp
+++ seqan/projects/tests/find_motif/test_find_motif.cpp
@@ -9,15 +9,7 @@ int main()
 {
 	SEQAN_TREPORT("TEST BEGIN")
 
-	/*typedef FrequencyDistribution<Dna> TFrequencyDistribution;
-	TFrequencyDistribution fd('a');
-	//Iterator<TFrequencyDistribution>::Type fd_begin = begin(fd);
-	//Iterator<TFrequencyDistribution>::Type fd_end = end(fd);
-	if(std::find(begin(fd),end(fd),static_cast<double>(0.5))!=end(fd))
-	{
-		std::cout << "TEST\n";
-	}*/
-
+	srand((unsigned) time(NULL));
 
 	//Motif search on a small set of nucleotide sequences.
 	unsigned int t = 3;		//number of input sequences
@@ -31,7 +23,7 @@ int main()
 	appendValue(dataset,DnaString("ACAGCA"));
 	appendValue(dataset,DnaString("AGGCAG"));
 	appendValue(dataset,DnaString("TCAGTC"));
-
+	
 	//Application of ePatternBranching (h=0)
 	MotifFinder<Dna, EPatternBranching> finder_epb1(t,l,d,is_exact,h);
 	findMotif(finder_epb1,dataset,OMOPS());
@@ -48,7 +40,6 @@ int main()
 	//[0] AGCC
 	//[1] CCAG
 
-	
 	//Application of PMS1-ZOOPS 
 	MotifFinder<Dna, PMS1> finder_pms1(l,d,is_exact);
 	findMotif(finder_pms1,dataset,ZOOPS());
@@ -65,22 +56,20 @@ int main()
 	//output: 
 	//[0] AAGC
 	//...
-	//[13] TGCA
+	//[13] TGCA	
 
-
-	/*
 	//Application of PROJECTION-OOPS
-	unsigned int m_total = t*(n-l+1);
-    MotifFinder<Dna, Projection> finder_proj(t,l,m_total,d);
+	unsigned int m = t*(n-l+1);
+    MotifFinder<Dna, Projection> finder_proj(t,l,m,d,is_exact);
 	findMotif(finder_proj, dataset, OOPS());
 	displayResult(finder_proj);
-
+	
+	/*
 	//Application of PROJECTION-TCM
-    MotifFinder<Dna, Projection> finder_proj_tcm(t,l,m_total,d);
+    MotifFinder<Dna, Projection> finder_proj_tcm(t,l,m,d,is_exact);
 	findMotif(finder_proj_tcm, dataset, TCM());
 	displayResult(finder_proj);
 	*/
-
 	
 	SEQAN_TREPORT("TEST END")
 	return 0;
