143485ba1214d9ff17110eeda69bbc5c83084c77
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-11-06 15:24:29 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1259 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/demos/index_find.cpp
+++ seqan/projects/demos/index_find.cpp
@@ -6,7 +6,9 @@ using namespace seqan;
 
 int main ()
 {
-///The following code builds a suffix array and searches in it
+///The following code creates an @Class.Index@ of $"tobeornottobe"$. 
+///As there is no second template parameter given to $Index<..>$,
+///the default index based on an enhanced suffix array is used.
     Index< String<char> > index_esa("tobeornottobe");
 	Finder< Index< String<char> > > finder_esa(index_esa);
 
@@ -15,7 +17,9 @@ int main ()
 		cout << position(finder_esa) << " ";
 	cout << endl;
 
-///Doing the same using a q-gram index (q==2)
+///Now we explicitly create a q-gram index using an ungapped 2-gram and do the same.
+///Instead of this @Spec.FixedShape.fixed-size shape@ you can use arbitrary 
+///@Class.Shape.shapes@ and assign them before calling @Function.find@ via @Function.indexShape@.
 	typedef Index< String<char>, Index_QGram< FixedShape<2> > > TQGramIndex;
     TQGramIndex index_2gram("tobeornottobe");
 	Finder< TQGramIndex > finder_2gram(index_2gram);
