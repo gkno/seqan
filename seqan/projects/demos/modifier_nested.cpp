08542ad93d2d293fc4de7e67a8917f45e60f0d3d
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-11-08 17:46:20 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1275 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/demos/modifier_nested.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <seqan/file.h>
+#include <seqan/modifier.h>
+
+using namespace std;
+using namespace seqan;
+
+int main ()
+{
+	String<Dna> myString = "attacgg";
+
+	typedef ModifiedString<DnaString, ModView< FunctorComplement<Dna> > > TMyComplement;
+	typedef ModifiedString<TMyComplement, ModReverse> TMyReverseComplement;
+
+	TMyComplement myComplement(myString);
+	TMyReverseComplement myReverseComplement(myString);
+
+	cout << myString << endl;
+	cout << myReverseComplement << endl;
+
+	infix(myString, 1, 1) = "cgt";
+
+	cout << myString << endl;
+	cout << myReverseComplement << endl;
+
+	return 0;
+}
