43bc3de3bb06d65d4a73a57d4e9ff8bc62354825
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-11-08 16:36:18 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1273 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/demos/modifier_modreverse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <seqan/file.h>
+#include <seqan/modifier.h>
+
+using namespace std;
+using namespace seqan;
+
+
+int main ()
+{
+	String<char> myString = "A man, a plan, a canal-Panama";
+	ModifiedString< String<char>, ModReverse > myModifier(myString);
+
+	cout << myString << endl;
+	cout << myModifier << endl;
+
+	infix(myString, 9, 9) = "master ";
+
+	cout << myString << endl;
+	cout << myModifier << endl;
+
+	return 0;
+}
---
+++ seqan/projects/demos/modifier_modview.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <seqan/file.h>
+#include <seqan/modifier.h>
+
+using namespace std;
+using namespace seqan;
+
+struct MyFunctor : public unary_function<char,char> 
+{
+	inline char operator()(char x) const 
+	{
+		if (('a' <= x) && (x <= 'z')) return (x + ('A' - 'a'));
+		return x; 
+	}
+};
+
+
+int main ()
+{
+	String<char> myString = "A man, a plan, a canal-Panama";
+	ModifiedString< String<char>, ModView<MyFunctor> > myModifier(myString);
+
+	cout << myString << endl;
+	cout << myModifier << endl;
+
+	infix(myString, 9, 9) = "master ";
+
+	cout << myString << endl;
+	cout << myModifier << endl;
+
+	return 0;
+}
