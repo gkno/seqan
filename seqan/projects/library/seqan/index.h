942c2ca2209ca11cef950a2f7ae58c6affb38277
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-11-26 15:54:52 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1332 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/index.h
+++ seqan/projects/library/seqan/index.h
@@ -89,7 +89,7 @@
 #include <seqan/index/shape_gapped.h>
 #include <seqan/index/shape_predefined.h>
 #include <seqan/index/index_qgram.h>
-#include <seqan/index/index_qgram_nested.h>
+//#include <seqan/index/index_qgram_nested.h>
 
 
 //////////////////////////////////////////////////////////////////////////////
