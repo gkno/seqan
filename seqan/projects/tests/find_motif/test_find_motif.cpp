c164a51ac3cc11c6a989bb2da20feba85838f659
jhlim@PCPOOL.MI.FU-BERLIN.DE <jhlim@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2007-12-20 22:10:37 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1451 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/tests/find_motif/test_find_motif.cpp
+++ seqan/projects/tests/find_motif/test_find_motif.cpp
@@ -9,87 +9,183 @@ using namespace seqan;
 
 //////////////////////////////////////////////////////////////////////////////
 
-void Test_approximationAlgorithms()
+template<typename TIter, typename TString, typename TType>
+bool
+isOOPSMotif(TIter ds_iter, TIter ds_end, TString const & motif, TType const & d, bool & is_exact)
 {
-	//Testing Projection & ePatternBranching algorithm
-
-	unsigned int t = 0;		//number of input sequences
-	unsigned int n = 0;		//length of sequence
-	unsigned int l = 0;		//length of motif
-	unsigned int d = 0;		//number of substitutions
-	bool is_exact = false;	//size of Hamming distance
-	unsigned int m =0;		//total number of possible l-mers
-	unsigned int h = 0;		//size of the neighborhood considering at first
-	unsigned int i = 0;
-
-	srand((unsigned) time(NULL));
-
-//____________________________________________________________________________
-// Test1 - Search for OOPS motifs on a small set of nucleotide sequences
-//         given the exact Hamming distance (=d)
-	t = 3;		
-	n = 6;		
-	l = 4;		
-	d = 1;		
-	is_exact = true;	
-	m = t*(n-l+1);
-
-	String<DnaString> dataset1;
-	appendValue(dataset1,DnaString("ACAGCA"));
-	appendValue(dataset1,DnaString("AGGCAG"));
-	appendValue(dataset1,DnaString("TCAGTC"));
-
-	//Application of PROJECTION-OOPS
-    MotifFinder<Dna, Projection> motif_finder1(t,l,m,d,is_exact);
-	findMotif(motif_finder1, dataset1, OOPS());
-	SEQAN_TASSERT(motif_finder1.consensus_pattern=="AGCC");
-
-	//Application of ePatternBranching-OOPS
-    MotifFinder<Dna, EPatternBranching> motif_finder2(t,l,d,is_exact,h);
-	findMotif(motif_finder2, dataset1, OOPS());
-	SEQAN_TASSERT(motif_finder2.set_of_motifs[0]=="AGCC");
-
-//____________________________________________________________________________
-// Test2 - Search for OMOPS motifs on a small set of nucleotide sequences
-//         given the exact Hamming distance (<=d)
-
-	is_exact = false;
-
-	//Application of PROJECTION-OMOPS
-    MotifFinder<Dna, Projection> motif_finder3(t,l,m,d,is_exact);
-	findMotif(motif_finder3, dataset1, OMOPS());
-	displayResult(motif_finder3);
-
-	//Application of ePatternBranching-OMOPS
-	MotifFinder<Dna, EPatternBranching> motif_finder4(t,l,d,is_exact,h);
-	findMotif(motif_finder4, dataset1, OMOPS());
-	displayResult(motif_finder4);
-
-//____________________________________________________________________________
-// Test3 - Search for ZOOPS motifs on a set of small nucleotide sequences
-//         given the inexact Hamming distance (<=d)
-
-	//Application of PROJECTION-ZOOPS
-    MotifFinder<Dna, Projection> motif_finder5(t,l,m,d,is_exact);
-	findMotif(motif_finder5, dataset1, ZOOPS());
-	SEQAN_TASSERT(motif_finder5.consensus_pattern=="TCAG");
-
-//____________________________________________________________________________
-// Test4 - Search for TCM motifs on a set of small nucleotide sequences
-//         given the exact Hamming distance (=d)
-
-	is_exact = true;
-
-	//Application of PROJECTION-TCM
-    MotifFinder<Dna, Projection> motif_finder6(t,l,m,d,is_exact);
-	findMotif(motif_finder6, dataset1, ZOOPS());
-	SEQAN_TASSERT(motif_finder6.consensus_pattern=="TCAG");
+	bool isRelevantMotif = false;
+	int t = (int) (ds_end-ds_iter);
+	int counter = 0;
+	int hd = 0;
+	typename Size<TString>::Type l = length(motif);
+	typename Iterator<TString>::Type seq_iter, seq_end, l_mer_begin;
+	while( (ds_iter!=ds_end) & (((int)(ds_end-ds_iter))==t) )
+	{
+		seq_iter = begin(*ds_iter);
+		seq_end = seq_iter+length(*ds_iter)-l+1;
+		counter = 0;
+		while( (seq_iter!=seq_end) & (counter<2) )
+		{
+			l_mer_begin = begin(motif);
+			hd = hammingDistance(seq_iter, seq_iter+l, l_mer_begin);
+			if( is_exact & (hd==d) )
+			{
+				++counter;
+			}
+			else if( !is_exact & (hd<=d))
+			{
+				++counter;
+			}
+			++seq_iter;
+		}
+		if(counter==1)
+		{
+			--t;
+		}
+		++ds_iter;
+	}
+	if(t==0)
+	{
+		isRelevantMotif = true;
+	}
+	return isRelevantMotif;
 }
+
+template<typename TIter, typename TString, typename TType>
+bool
+isOMOPSMotif(TIter ds_iter, TIter ds_end, TString const & motif, TType const & d, bool & is_exact)
+{
+	bool isRelevantMotif = false;
+	int t = (int) (ds_end-ds_iter);
+	int counter = 0;
+	int hd = 0;
+	typename Size<TString>::Type l = length(motif);
+	typename Iterator<TString>::Type seq_iter, seq_end, l_mer_begin;
+	while( (ds_iter!=ds_end) & (((int)(ds_end-ds_iter))==t) )
+	{
+		seq_iter = begin(*ds_iter);
+		seq_end = seq_iter+length(*ds_iter)-l+1;
+		counter = 0;
+		while( (seq_iter!=seq_end) & (counter<1) )
+		{
+			l_mer_begin = begin(motif);
+			hd = hammingDistance(seq_iter, seq_iter+l, l_mer_begin);
+			if( is_exact & (hd==d) )
+			{
+				++counter;
+			}
+			else if( !is_exact & (hd<=d))
+			{
+				++counter;
+			}
+			++seq_iter;
+		}
+		if(counter==1)
+		{
+			--t;
+		}
+		++ds_iter;
+	}
+	if(t==0)
+	{
+		isRelevantMotif = true;
+	}
+	return isRelevantMotif;
+}
+
+template<typename TIter, typename TString, typename TType>
+bool
+isZOOPSMotif(TIter ds_iter, TIter ds_end, TString const & motif, TType const & d, bool & is_exact)
+{
+	bool isRelevantMotif = false;
+	int t = (int) (ds_end-ds_iter);
+	int upper_limit = t-((int) floor(t*(ZOOPS().threshold)+0.5)-1);
+	int counter = 0;
+	int hd = 0;
+	typename Size<TString>::Type l = length(motif);
+	typename Iterator<TString>::Type seq_iter, seq_end, l_mer_begin;
+	while( (ds_iter!=ds_end) )
+	{
+		seq_iter = begin(*ds_iter);
+		seq_end = seq_iter+length(*ds_iter)-l+1;
+		counter = 0;
+		while( (seq_iter!=seq_end) & (counter<2) )
+		{
+			l_mer_begin = begin(motif);
+			hd = hammingDistance(seq_iter, seq_iter+l, l_mer_begin);
+			if( is_exact & (hd==d) )
+			{
+				++counter;
+			}
+			else if( !is_exact & (hd<=d))
+			{
+				++counter;
+			}
+			++seq_iter;
+		}
+		if(counter==1)
+		{
+			--t;
+		}
+		++ds_iter;
+	}
+	if(t<=upper_limit)
+	{
+		isRelevantMotif = true;
+	}
+	return isRelevantMotif;
+}
+
+template<typename TIter, typename TString, typename TType>
+bool
+isTCMMotif(TIter ds_iter, TIter ds_end, TString const & motif, TType const & d, bool & is_exact)
+{
+	bool isRelevantMotif = false;
+	int t = (int) (ds_end-ds_iter);
+	int upper_limit = t-((int) floor(t*(TCM().threshold)+0.5)-1);
+	int counter = 0;
+	int hd = 0;
+	typename Size<TString>::Type l = length(motif);
+	typename Iterator<TString>::Type seq_iter, seq_end, l_mer_begin;
+	while( (ds_iter!=ds_end) )
+	{
+		seq_iter = begin(*ds_iter);
+		seq_end = seq_iter+length(*ds_iter)-l+1;
+		counter = 0;
+		while( (seq_iter!=seq_end) & (counter<1) )
+		{
+			l_mer_begin = begin(motif);
+			hd = hammingDistance(seq_iter, seq_iter+l, l_mer_begin);
+			if( is_exact & (hd==d) )
+			{
+				++counter;
+			}
+			else if( !is_exact & (hd<=d))
+			{
+				++counter;
+			}
+			++seq_iter;
+		}
+		if(counter==1)
+		{
+			--t;
+		}
+		++ds_iter;
+	}
+	if(t<=upper_limit)
+	{
+		isRelevantMotif = true;
+	}
+	return isRelevantMotif;
+}
+
+//////////////////////////////////////////////////////////////////////////////
 
 void Test_exactAlgorithms()
 {
 	//Testing PMS1 & PMSP algorithm
 
+	unsigned int t = 0;      //number of sequences
 	unsigned int l = 0;		//length of motif
 	unsigned int d = 0;		//number of substitutions
 	bool is_exact = false;	//size of Hamming distance
@@ -99,14 +195,16 @@ void Test_exactAlgorithms()
 // Test1 - Search for OOPS motifs on a small set of nucleotide sequences
 //         given the exact Hamming distance (=d)
 
+	t = 3;
 	l = 4;		
 	d = 1;		
 	is_exact = true;	
 
 	String<DnaString> dataset1;
-	appendValue(dataset1,DnaString("ACAGCA"));
-	appendValue(dataset1,DnaString("AGGCAG"));
-	appendValue(dataset1,DnaString("TCAGTC"));
+	resize(dataset1, t);
+	dataset1[0] = "ACAGCA";
+	dataset1[1] = "AGGCAG";
+	dataset1[2] = "TCAGTC";
 
 	//Application of PMS1-OOPS
 	MotifFinder<Dna, PMS1> motif_finder1(l,d,is_exact);
@@ -117,7 +215,6 @@ void Test_exactAlgorithms()
 	findMotif(motif_finder2,dataset1,OOPS());
 
 	SEQAN_TASSERT(length(motif_finder1.set_of_motifs)==length(motif_finder2.set_of_motifs));
-
 	for(i=0; i<length(motif_finder1.set_of_motifs); ++i)
 	{
 		SEQAN_TASSERT(motif_finder1.set_of_motifs[i]==motif_finder2.set_of_motifs[i]);
@@ -132,11 +229,11 @@ void Test_exactAlgorithms()
 	is_exact = false;	
 
 	String<DnaString> dataset2;
-	appendValue(dataset2,DnaString("TTTAGGCCACTCGATAAGAG"));
-	appendValue(dataset2,DnaString("GAGCTACAACCGAGGAATTG"));
-	appendValue(dataset2,DnaString("CGAATGTTGTGACTCGATAA"));
-	appendValue(dataset2,DnaString("GTCCGAAAATACCTGGAGCC"));
-	appendValue(dataset2,DnaString("CCGCTTAAAAATGTATTATC"));
+	appendValue(dataset2,DnaString("GCTGGACGTG"));
+	appendValue(dataset2,DnaString("TCTAGACATA"));
+	appendValue(dataset2,DnaString("AGTGGGGGAC"));
+	appendValue(dataset2,DnaString("CTAGTCAAGA"));
+	appendValue(dataset2,DnaString("CTCGAGGGGT"));
 
 	//Application of PMS1-OMOPS
 	MotifFinder<Dna, PMS1> motif_finder3(l,d,is_exact);
@@ -147,7 +244,6 @@ void Test_exactAlgorithms()
 	findMotif(motif_finder4,dataset2,OMOPS());
 
 	SEQAN_TASSERT(length(motif_finder3.set_of_motifs)==length(motif_finder4.set_of_motifs));
-
 	for(i=0; i<length(motif_finder3.set_of_motifs); ++i)
 	{
 		SEQAN_TASSERT(motif_finder3.set_of_motifs[i]==motif_finder4.set_of_motifs[i]);
@@ -162,9 +258,11 @@ void Test_exactAlgorithms()
 	is_exact = true;	
 
 	String<DnaString> dataset3;
-	appendValue(dataset3,DnaString("CTATTACCCAGAGTGCCAAG"));
-	appendValue(dataset3,DnaString("AACGGACCCAACGGCAAAGA"));
-	appendValue(dataset3,DnaString("TCTTGTCTTCACAGTCAGAT"));
+	appendValue(dataset3,DnaString("AGCCGTCTGA"));
+	appendValue(dataset3,DnaString("TCCAGGCAAG"));
+	appendValue(dataset3,DnaString("GAACGTCCAA"));
+	appendValue(dataset3,DnaString("GCTTTCTAAC"));
+	appendValue(dataset3,DnaString("AGTAGCTCGC"));
 
 	//Application of PMS1-ZOOPS
 	MotifFinder<Dna, PMS1> motif_finder5(l,d,is_exact);
@@ -175,7 +273,6 @@ void Test_exactAlgorithms()
 	findMotif(motif_finder6,dataset3,ZOOPS());
 
 	SEQAN_TASSERT(length(motif_finder5.set_of_motifs)==length(motif_finder6.set_of_motifs));
-
 	for(i=0; i<length(motif_finder5.set_of_motifs); ++i)
 	{
 		SEQAN_TASSERT(motif_finder5.set_of_motifs[i]==motif_finder6.set_of_motifs[i]);
@@ -189,9 +286,9 @@ void Test_exactAlgorithms()
 	d = 1;		
 
 	String<DnaString> dataset4;
-	appendValue(dataset4,DnaString("GCGGAGTACGATAACATTTC"));
-	appendValue(dataset4,DnaString("TCCATGATGGAGATGAAGAA"));
-	appendValue(dataset4,DnaString("AAAGGTCATCAGATCTAGAT"));
+	appendValue(dataset4,DnaString("CAATTAACTC"));
+	appendValue(dataset4,DnaString("ATAAACAGTG"));
+	appendValue(dataset4,DnaString("GAATGCATTG"));
 
 	//Application of PMS1-TCM
 	MotifFinder<Dna, PMS1> motif_finder7(l,d,is_exact);
@@ -202,19 +299,137 @@ void Test_exactAlgorithms()
 	findMotif(motif_finder8,dataset4,TCM());
 	
 	SEQAN_TASSERT(length(motif_finder7.set_of_motifs)==length(motif_finder8.set_of_motifs));
-
 	for(i=0; i<length(motif_finder7.set_of_motifs); ++i)
 	{
 		SEQAN_TASSERT(motif_finder7.set_of_motifs[i]==motif_finder8.set_of_motifs[i]);
 	}
+
+}
+
+void Test_approximationAlgorithms()
+{
+	//Testing Projection & ePatternBranching algorithm
+	
+	unsigned int t = 0;      //number of sequences
+	unsigned int n = 0;		//length of sequence
+	unsigned int l = 0;		//length of motif
+	unsigned int d = 0;		//number of substitutions
+	bool is_exact = false;	//size of Hamming distance
+	unsigned int m =0;		//total number of possible l-mers
+	unsigned int h = 0;		//size of the neighborhood considering at first
+	unsigned int i = 0;
+
+	srand((unsigned) time(NULL));
+
+//____________________________________________________________________________
+// Test1 - Search for OOPS motifs on a small set of nucleotide sequences
+//         given the exact Hamming distance (=d)
+
+	t = 3;  
+	n = 6;		
+	l = 4;		
+	d = 1;		
+	is_exact = true;	
+	m = t*(n-l+1);
+
+	String<DnaString> dataset1;
+	appendValue(dataset1,DnaString("ACAGCA"));
+	appendValue(dataset1,DnaString("AGGCAG"));
+	appendValue(dataset1,DnaString("TCAGTC"));
+
+	//Application of PROJECTION-OOPS
+    MotifFinder<Dna, Projection> motif_finder1(t,l,m,d,is_exact);
+	findMotif(motif_finder1, dataset1, OOPS());
+	//check whether found motif is really an OOPS motif
+	SEQAN_TASSERT(isOOPSMotif(begin(dataset1),
+				  end(dataset1),
+				  motif_finder1.consensus_pattern,
+				  d,
+				  is_exact)==true);
+
+//____________________________________________________________________________
+//
+	//Application of ePatternBranching-OOPS
+    MotifFinder<Dna, EPatternBranching> motif_finder2(t,l,d,is_exact,h);
+	findMotif(motif_finder2, dataset1, OOPS());
+	//check whether found motif is really an OOPS motif
+	for(i=0; i<length(motif_finder2.set_of_motifs); ++i)
+	{
+		SEQAN_TASSERT(isOOPSMotif(begin(dataset1),
+					  end(dataset1),
+				      motif_finder2.set_of_motifs[i],
+				      d,
+				      is_exact)==true);
+	}
+
+//____________________________________________________________________________
+// Test2 - Search for OMOPS motifs on a small set of nucleotide sequences
+//         given the exact Hamming distance (<=d)
+
+	is_exact = false;
+
+	//Application of PROJECTION-OMOPS
+    MotifFinder<Dna, Projection> motif_finder3(t,l,m,d,is_exact);
+	findMotif(motif_finder3, dataset1, OMOPS());
+	//check whether found motif is really an OMOPS motif
+	SEQAN_TASSERT(isOMOPSMotif(begin(dataset1),
+				  end(dataset1),
+				  motif_finder3.consensus_pattern,
+				  d,
+				  is_exact)==true);
+
+//____________________________________________________________________________
+//
+	//Application of ePatternBranching-OMOPS
+	MotifFinder<Dna, EPatternBranching> motif_finder4(t,l,d,is_exact,h);
+	findMotif(motif_finder4, dataset1, OMOPS());
+	//check whether found motif is really an OMOPS motif
+	for(i=0; i<length(motif_finder4.set_of_motifs); ++i)
+	{
+		SEQAN_TASSERT(isOMOPSMotif(begin(dataset1),
+					  end(dataset1),
+				      motif_finder4.set_of_motifs[i],
+				      d,
+				      is_exact)==true);
+	}
+
+//____________________________________________________________________________
+// Test3 - Search for ZOOPS motifs on a set of small nucleotide sequences
+//         given the inexact Hamming distance (<=d)
+
+	//Application of PROJECTION-ZOOPS
+    MotifFinder<Dna, Projection> motif_finder5(t,l,m,d,is_exact);
+	findMotif(motif_finder5, dataset1, ZOOPS());
+	//check whether found motif is really a ZOOPS motif
+	SEQAN_TASSERT(isZOOPSMotif(begin(dataset1),
+				  end(dataset1),
+				  motif_finder5.consensus_pattern,
+				  d,
+				  is_exact)==true);
+
+//____________________________________________________________________________
+// Test4 - Search for TCM motifs on a set of small nucleotide sequences
+//         given the exact Hamming distance (=d)
+
+	is_exact = true;
+
+	//Application of PROJECTION-TCM
+    MotifFinder<Dna, Projection> motif_finder6(t,l,m,d,is_exact);
+	findMotif(motif_finder6, dataset1, TCM());
+	//check whether found motif is really a TCM motif
+	SEQAN_TASSERT(isTCMMotif(begin(dataset1),
+				  end(dataset1),
+				  motif_finder6.consensus_pattern,
+				  d,
+				  is_exact)==true);
 }
 
 int main() 
 {
 	SEQAN_TREPORT("TEST FIND MOTIF BEGIN")
 
-	Test_approximationAlgorithms();
 	Test_exactAlgorithms();
+	Test_approximationAlgorithms();
 
 	SEQAN_TREPORT("TEST FIND MOTIF END");
 
