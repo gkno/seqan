0acf7f924fea0fd78d9b3fc330bdc2ca4a50fd81
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2008-02-25 14:59:51 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@1752 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/index_node_predicate.cpp
+++ seqan/projects/library/demos/index_node_predicate.cpp
@@ -4,40 +4,78 @@
 using namespace std;
 using namespace seqan;
 
+/// constraint parameters
+struct TMyConstraints {
+	double p_min;
+	int replen_max;
+};
 
-	struct TMySpec;
+/// SeqAn extensions
+namespace seqan 
+{
+	// custom TSpec for our customized wotd-Index
+	struct TMyIndex;
 
-	template <typename TString, typename TSpec>
-	bool nodePredicate(Iter<Index<TString, Index_Wotd<TMySpec> >, TSpec> const &it) {
-		return (countOccurrences(it) >= 2) && (repLength(it) >= 4);
+	template <typename TText>
+	struct Cargo<Index<TText, Index_Wotd<TMyIndex> > > {
+		typedef TMyConstraints Type;
+	};
+
+	// node predicate
+	template <typename TText, typename TSpec>
+	bool nodePredicate(Iter<Index<TText, Index_Wotd<TMyIndex> >, TSpec> const &it) 
+	{
+		TMyConstraints const &cons = cargo(container(it));
+		unsigned replen = repLength(it);
+		unsigned textLen = length(container(it));
+
+		if (textLen == replen) return false;
+		return ((double)countOccurrences(it) / 
+				(double)(textLen - replen)) > cons.p_min;
 	}
 
+	// monotonic hull
+	template <typename TText, typename TSpec>
+	bool nodeHullPredicate(Iter<Index<TText, Index_Wotd<TMyIndex> >, TSpec> const &it) 
+	{
+		Index<TText, Index_Wotd<TMyIndex> > const &index = container(it);
+		TMyConstraints const &cons = cargo(index);
+
+		if (repLength(it) > cons.replen_max) return false;
+		return ((double)countOccurrences(it) / 
+				(double)(length(index) - cons.replen_max)) > cons.p_min;
+	}
+}
 
 int main ()
 {
 ///We begin with a @Class.String@ to store our sequence.
 	String<char> myString = "How many wood would a woodchuck chuck.";
 
-///Then we create an @Class.Index@ of this @Class.StringSet@
-	typedef Index< String<char>, Index_Wotd<TMySpec> > TMyIndex;
+///Then we create our customized index which is a specialization
+///of the deferred @Class.Index.wotd-Index@
+	typedef Index< String<char>, Index_Wotd<TMyIndex> > TMyIndex;
 	TMyIndex myIndex(myString);
 
-///To find maximal repeats, we use SeqAn's @Spec.MaxRepeats Iterator@
-///and set the minimum repeat length to 3.
-	typedef Iterator< TMyIndex, TopDown<ParentLinks<> > >::Type TMaxRepeatIterator;
-	TMaxRepeatIterator myRepeatIterator(myIndex);
+	cargo(myIndex).replen_max = 10;
+	cargo(myIndex).p_min = 0.05;
 
-	goBegin(myRepeatIterator);
-	while (!atEnd(myRepeatIterator))
+///To find all strings that fulfill our constraints,
+///we simply do a dfs-traversal via @Function.goBegin@ and @Function.goNext@
+	typedef Iterator< TMyIndex, TopDown<ParentLinks<> > >::Type TConstrainedIterator;
+	TConstrainedIterator myConstrainedIterator(myIndex);
+
+	goBegin(myConstrainedIterator);
+	while (!atEnd(myConstrainedIterator))
 	{
 
 ///@Function.countOccurrences@ returns the number of hits of the representative.
-		cout << countOccurrences(myRepeatIterator) << "x  ";
+		cout << countOccurrences(myConstrainedIterator) << "x  ";
 
-///The repeat string itself can be determined with @Function.representative@
-		cout << "\t\"" << representative(myRepeatIterator) << '\"' << endl;
+///The representative string can be determined with @Function.representative@
+		cout << "\t\"" << representative(myConstrainedIterator) << '\"' << endl;
 
-		goNext(myRepeatIterator);
+		goNext(myConstrainedIterator);
 	}
 
 	return 0;
