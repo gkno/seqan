f4e53e2f8f147487f8278c9dc626efe18c3f49ab
doering@PCPOOL.MI.FU-BERLIN.DE <doering@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2008-06-06 17:29:08 +0000

    added script for generating visual studio project files in "demos" for .net 2008
    git-svn-id: http://svn.seqan.de/seqan/trunk@2354 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/misc/vs_demo_projectfile/vs_demo_projectfile.py
+++ seqan/misc/vs_demo_projectfile/vs_demo_projectfile.py
@@ -4,6 +4,7 @@ import os
 
 BUILD_FLAGS_7 = '/I ".." /I "../../../platforms/windows" /Op- /EHsc /D "DEBUG" /D "WIN32" /Zi /GR /W2 /Zc:wchar_t'
 BUILD_FLAGS_8 = '/I ".." /I "../../../platforms/windows" /EHsc /MTd /Zi /D "_CONSOLE" /D "_CRT_SECURE_NO_DEPRECATE" /D "DEBUG" /D "WIN32" /Zc:wchar_t /W2 /wd4996'
+BUILD_FLAGS_9 = '/I ".." /I "../../../platforms/windows" /EHsc /MTd /Zi /D "_CONSOLE" /D "_CRT_SECURE_NO_DEPRECATE" /D "DEBUG" /D "WIN32" /Zc:wchar_t /W2 /wd4996'
 
 ################################################################################
 
@@ -83,6 +84,11 @@ def main():
     scanDemos("../../projects/library/demos", BUILD_FLAGS_8)
     createProject("../../projects/library/demos", "SeqAn_8")
     
+    print
+    print "Visual Studio 9 (.net 2008) Files:",
+    scanDemos("../../projects/library/demos", BUILD_FLAGS_9)
+    createProject("../../projects/library/demos", "SeqAn_9")
+
     print
     print "Files created."
     
