4d4ec58e2abacd8819418d75d8df7a11010d2a4c
kemena@PCPOOL.MI.FU-BERLIN.DE <kemena@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2008-06-09 15:07:58 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@2407 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/seqan/sequence/adapt_std_list.h
@@ -0,0 +1,85 @@
+ /*==========================================================================
+                SeqAn - The Library for Sequence Analysis
+                          http://www.seqan.de 
+ ============================================================================
+  Copyright (C) 2007
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+ ============================================================================
+  $Id$
+ ==========================================================================*/
+
+
+//SEQAN_NO_GENERATED_FORWARDS: no forwards are generated for this file
+
+#ifndef SEQAN_HEADER_ADAPT_STD_LIST_H
+#define SEQAN_HEADER_ADAPT_STD_LSIT_H
+
+#include <list>
+
+
+namespace SEQAN_NAMESPACE_MAIN
+{
+
+template <typename TValue>
+struct Value<std::list<TValue> >
+{
+	typedef TValue Type;
+};
+
+template <typename TValue>
+struct Iterator<std::list<TValue> >
+{
+	typedef typename std::list<TValue>::iterator Type;
+};
+
+template <typename TValue>
+struct Iterator<const std::list<TValue> >
+{
+	typedef typename std::list<TValue>::const_iterator Type;
+};
+
+template<typename TValue>
+inline typename Iterator<std::list<TValue>, Standard >::Type
+begin(std::list<TValue> &list_){
+	return list_.begin();
+}
+
+template<typename TValue>
+typename Iterator<std::list<TValue>, Standard >::Type
+inline end(std::list<TValue> &list_){
+	return list_.end();
+}
+
+template<typename TValue>
+inline typename Iterator<const std::list<TValue>, Standard >::Type
+begin(const std::list<TValue> &list_){
+	return list_.begin();
+}
+
+template<typename TValue>
+inline typename Iterator<const std::list<TValue>, Standard >::Type
+end(const std::list<TValue> &list_){
+	return list_.end();
+}
+
+template <typename T, typename T2>
+inline void
+appendValue(std::list<T> &chain, 
+			T2 value)
+{
+	chain.push_back(value);
+}
+
+}
+
+#endif //#ifndef SEQAN_HEADER_
