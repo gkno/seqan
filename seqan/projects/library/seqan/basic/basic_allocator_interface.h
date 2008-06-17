ebed5ae9451a923fbafe04c189c9ecd09b0bb42c
doering@PCPOOL.MI.FU-BERLIN.DE <doering@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2008-06-17 13:40:52 +0000

    fixed: some compiler warnings concerning empty if statements
    git-svn-id: http://svn.seqan.de/seqan/trunk@2415 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/basic/basic_allocator_interface.h
+++ seqan/projects/library/seqan/basic/basic_allocator_interface.h
@@ -146,8 +146,11 @@ allocate(T const &,
 		 Tag<TUsage> const)
 {
 	data = (TValue *) operator new(count * sizeof(TValue));
+
+#ifdef SEQAN_PROFILE
 	if (data)
 	    SEQAN_PROADD(SEQAN_PROMEMORY, count * sizeof(TValue));
+#endif
 }
 template <typename T, typename TValue, typename TSize, typename TUsage>
 inline void
@@ -157,8 +160,11 @@ allocate(T &,
 		 Tag<TUsage> const)
 {
 	data = (TValue *) operator new(count * sizeof(TValue));
+
+#ifdef SEQAN_PROFILE
 	if (data)
 	    SEQAN_PROADD(SEQAN_PROMEMORY, count * sizeof(TValue));
+#endif
 }
 
 
@@ -211,22 +217,34 @@ template <typename T, typename TValue, typename TSize, typename TUsage>
 inline void 
 deallocate(T const & /*me*/,
 		   TValue * data, 
-		   TSize count,
+		   TSize 
+#ifdef SEQAN_PROFILE 
+					count
+#endif
+					,
 		   Tag<TUsage> const)
 {
+#ifdef SEQAN_PROFILE
 	if (data && count)	// .. to use count if SEQAN_PROFILE is not defined
 	    SEQAN_PROSUB(SEQAN_PROMEMORY, count * sizeof(TValue));
+#endif
 	operator delete ((void *) data);
 }
 template <typename T, typename TValue, typename TSize, typename TUsage>
 inline void 
 deallocate(T & /*me*/,
 		   TValue * data, 
-		   TSize count,
+		   TSize
+#ifdef SEQAN_PROFILE 
+					count
+#endif
+					,
 		   Tag<TUsage> const)
 {
+#ifdef SEQAN_PROFILE
 	if (data && count)	// .. to use count if SEQAN_PROFILE is not defined
 	    SEQAN_PROSUB(SEQAN_PROMEMORY, count * sizeof(TValue));
+#endif
 	operator delete ((void *) data);
 }
 //////////////////////////////////////////////////////////////////////////////
