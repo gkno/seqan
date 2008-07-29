0ba261f69e1196a35787f331013cc37834da6712
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2008-07-29 21:54:23 +0000

    added: Memory Mapped String
    git-svn-id: http://svn.seqan.de/seqan/trunk@2492 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/file_speed.cpp
+++ seqan/projects/library/demos/file_speed.cpp
@@ -1,9 +1,9 @@
 #define SEQAN_PROFILE
+//#define SEQAN_DEBUG
 
-#include <iostream>
 #include <seqan/sequence.h>
 #include <seqan/file.h>
-#include <seqan/system.h>
+#include <iostream>
 
 using namespace seqan;
 using namespace std;
@@ -11,8 +11,8 @@ using namespace std;
 const int blockSize = 1 << 12;
 const int repeats = 1 << 17;
 
-char block1[blockSize] = "This a test string";
-char block2[blockSize];
+CharString block1 = "This a test string";
+CharString block2;
 
 template <typename TFile>
 void testThroughput(const char *fileName)
@@ -27,12 +27,14 @@ void testThroughput(const char *fileName)
 
 	SEQAN_PROTIMESTART(iotime);
 
-	for (unsigned i = 0; i < repeats; ++i) 
+	awriteAt(myFile, toCString(block1), blockSize, 0 * blockSize, req1);
+	awriteAt(myFile, toCString(block2), blockSize, 1 * blockSize, req2);
+	for (unsigned i = 1; i < repeats; ++i) 
 	{
 		waitFor(req1);
-		awriteAt(myFile, block1, blockSize,    2*i  * blockSize, req1);
+		awriteAt(myFile, toCString(block1), blockSize,    2*i  * blockSize, req1);
 		waitFor(req2);
-		awriteAt(myFile, block2, blockSize, (2*i+1) * blockSize, req2);
+		awriteAt(myFile, toCString(block2), blockSize, (2*i+1) * blockSize, req2);
 	}
 	waitFor(req1);
 	waitFor(req2);
@@ -43,10 +45,60 @@ void testThroughput(const char *fileName)
 	close(myFile);
 }
 
+template <typename TFile>
+void testExtString(const char *fileName)
+{
+	String<char, External<ExternalConfig<TFile> > > myString;
+
+	if (!open(myString, fileName, OPEN_WRONLY | OPEN_CREATE)) {
+		cout << "Could not open for writing" << endl;
+		return;
+	}
+
+	SEQAN_PROTIMESTART(iotime);
+
+	for (unsigned i = 0; i < repeats; ++i) 
+	{
+		append(myString, block1);
+		append(myString, block2);
+	}
+
+	cout << ((repeats*blockSize / (512.0 * 1024.0)) / SEQAN_PROTIMEDIFF(iotime));
+	cout << " MB/s" << endl;
+}
+
+template <typename TFile>
+void testMMapString(const char *fileName)
+{
+	String<char, MMap<ExternalConfig<TFile> > > myString;
+
+	if (!open(myString, fileName, OPEN_RDWR | OPEN_CREATE)) {
+		cout << "Could not open for writing" << endl;
+		return;
+	}
+
+	SEQAN_PROTIMESTART(iotime);
+
+	for (unsigned i = 0; i < repeats; ++i) 
+	{
+		append(myString, block1);
+		append(myString, block2);
+	}
+
+	cout << ((repeats*blockSize / (512.0 * 1024.0)) / SEQAN_PROTIMEDIFF(iotime));
+	cout << " MB/s" << endl;
+}
+
 int main() 
 {
-	testThroughput< FILE* >("file_speedF.bin");
-	testThroughput< File< Sync<> > >("file_speedS.bin");
-	testThroughput< File< Async<> > >("file_speedA.bin");
+	resize(block1, blockSize);
+	resize(block2, blockSize);
+	cout << "awrite() using FILE*        ";		testThroughput< FILE* >				("file_speed1.bin");
+	cout << "awrite() using sync. File   ";		testThroughput< File< Sync<> > >	("file_speed2.bin");
+	cout << "awrite() using async. File  ";		testThroughput< File< Async<> > >	("file_speed3.bin");
+	cout << "ExtString using FILE*       ";		testExtString< FILE* >				("file_speed4.bin");
+	cout << "ExtString using sync. File  ";		testExtString< File< Sync<> > >		("file_speed5.bin");
+	cout << "ExtString using async. File ";		testExtString< File< Async<> > >	("file_speed6.bin");
+	cout << "Memory Mapped String        ";		testMMapString< File< Sync<> > >	("file_speed7.bin");
 	return 0;
 }
