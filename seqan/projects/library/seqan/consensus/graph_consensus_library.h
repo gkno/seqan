a4a6125d41180362a275e1c5718ecff083d2a63e
rausch@PCPOOL.MI.FU-BERLIN.DE <rausch@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2008-10-14 13:39:39 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@2866 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/consensus/graph_consensus_library.h
+++ seqan/projects/library/seqan/consensus/graph_consensus_library.h
@@ -600,9 +600,12 @@ appendSegmentMatches(StringSet<TString, TSpec> const& str,
 				// Create a corresponding edge
 				TSize i = idToPosition(str, id1);
 				TSize j = idToPosition(str, id2);
-				if (i<j) __getAlignmentStatistics(dist, i, j, nseq, matchLen, quality);
-				else __getAlignmentStatistics(dist, j, i, nseq, matchLen, quality);
-				
+				//if (i<j) __getAlignmentStatistics(dist, i, j, nseq, matchLen, quality);
+				//else __getAlignmentStatistics(dist, j, i, nseq, matchLen, quality);
+				if (i<j) __getAlignmentStatistics(dist, i, j, nseq, 1, 1);
+				else __getAlignmentStatistics(dist, j, i, nseq, 1, 1);
+
+
 				// Record the scores
 				resize(scores, to);
 				typedef typename Iterator<TScoreValues>::Type TScoreIter;
