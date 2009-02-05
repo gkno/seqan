317da64d269efd0acfa369fb40e924262ede4e2a
doering@PCPOOL.MI.FU-BERLIN.DE <doering@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2009-02-05 16:09:21 +0000

    Further improvements of lagan.cpp demo
    git-svn-id: http://svn.seqan.de/seqan/trunk@3359 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/lagan.cpp
+++ seqan/projects/library/demos/lagan.cpp
@@ -1,68 +1,54 @@
+#include <fstream>
 #include <seqan/seeds.h>
 #include <seqan/file.h>
-#include <fstream>
 
 using namespace seqan;
 using namespace std;
 
-template <typename TSeedSet, typename TSegment, typename TSize>
-TSize
-findSeeds(TSeedSet & seedset,
-		  TSegment const & seg_0,
-		  TSegment const & seg_1,
-		  TSize q,
-		  TSize q_min)
-{
-	//add all common q-grams into seetset
-
-	typedef Index< TSegment, Index_QGram<SimpleShape > > TQGramIndex;
-	TQGramIndex index_qgram(seg_1);
-
-	while ((length(seedset) == 0) && (q >= q_min))
-	{
-		resize(indexShape(index_qgram), q);
-
-		typedef Finder<TQGramIndex> TFinder;
-		TFinder finder(index_qgram);
-
-		int q_gram_count = length(seg_0)-q+1;
-		for (int i = 0; i < q_gram_count; ++i)
-		{
-			while (find(finder, infix(seg_0, i, i+q)))
-			{
-				typedef typename Position<TFinder>::Type TPosition;
-				TPosition pos = position(finder);
-				if (!addSeed(seedset, beginPosition(seg_0)+i, beginPosition(seg_1)+pos, q, 0, Merge()))
-				if (!addSeed(seedset, beginPosition(seg_0)+i, beginPosition(seg_1)+pos, q, host(seg_0), host(seg_1), 5, Chaos()))
-					 addSeed(seedset, beginPosition(seg_0)+i, beginPosition(seg_1)+pos, q, Single());
-			}
-			clear(finder);
-		}
-		--q;
-	}
-	return q;
-}
-
 template <typename TSeed, typename TSegment, typename TSize, typename TScoring, typename TScore>
-void
-lagan(list<TSeed> & chain,
-	  TSegment const & seg_0,
-	  TSegment const & seg_1,
-	  TSize q,
-	  TSize q_min,
-	  TSize limit,
-	  TSize gaps_max,
-	  TScoring scoring_scheme,
-	  TScore score_min)
+void laganChaining(list<TSeed> & chain,
+				   TSegment const & a,
+				   TSegment const & b,
+				   TSize q,
+				   TSize q_min,
+				   TSize limit,
+				   TSize gaps_max,
+				   TScoring scoring_scheme,
+				   TScore score_min)
 {
-	if (((TSize)length(seg_0) <= gaps_max) && ((TSize)length(seg_1) <= gaps_max)) return;
+	if (((TSize)length(a) <= gaps_max) && ((TSize)length(b) <= gaps_max)) return;
 
 	//Step 1: find seeds
 	typedef typename Value<TSeed>::Type TPosition;
 	typedef SeedSet<TPosition, SimpleSeed, DefaultScore> TSeedSet;
 	TSeedSet seedset(limit, score_min, scoring_scheme);
-	q = findSeeds(seedset, seg_0, seg_1, q, q_min);
-	if (!length(seedset)) return;
+
+	typedef Index< TSegment, Index_QGram<SimpleShape > > TQGramIndex;
+	TQGramIndex index_qgram(b);
+
+	typedef Finder<TQGramIndex> TFinder;
+	TFinder finder(index_qgram);
+
+	while (length(seedset) == 0)
+	{
+		if (q <= q_min) return;
+
+		resize(indexShape(index_qgram), q);
+		for (int i = 0; i < length(a)-q+1; ++i)
+		{
+			while (find(finder, infix(a, i, i+q)))
+			{
+				typedef typename Position<TFinder>::Type TPosition;
+				TPosition a_pos = beginPosition(a)+i;
+				TPosition b_pos = beginPosition(b)+position(finder);
+				if (!addSeed(seedset, a_pos, b_pos, q, 0, Merge()))
+				if (!addSeed(seedset, a_pos, b_pos, q, host(a), host(b), 5, Chaos()))
+					 addSeed(seedset, a_pos, b_pos, q, Single());
+			}
+			clear(finder);
+		}
+		--q;
+	}
 
 	//Step 2: global chaining
 	globalChaining(seedset, chain);
@@ -78,17 +64,17 @@ lagan(list<TSeed> & chain,
 		TIterator it2 = it; 
 		++it2;
 
-		lagan(subchain, 
-			infix(host(seg_0), beginPosition(seg_0), leftDim0(*it)), 
-			infix(host(seg_1), beginPosition(seg_1), leftDim1(*it)), 
+		laganChaining(subchain, 
+			infix(host(a), beginPosition(a), leftDim0(*it)), 
+			infix(host(b), beginPosition(b), leftDim1(*it)), 
 			q, q_min, limit, gaps_max, scoring_scheme, score_min);
 		chain.splice(it, subchain);
 
 		while(it2 != chain.end())
 		{
-			lagan(subchain, 
-				infix(host(seg_0), rightDim0(*it), leftDim0(*it2)), 
-				infix(host(seg_1), rightDim1(*it), leftDim1(*it2)), 
+			laganChaining(subchain, 
+				infix(host(a), rightDim0(*it), leftDim0(*it2)), 
+				infix(host(b), rightDim1(*it), leftDim1(*it2)), 
 				q, q_min, limit, gaps_max, scoring_scheme, score_min);
 			chain.splice(it2, subchain);
 
@@ -96,65 +82,46 @@ lagan(list<TSeed> & chain,
 			++it2;
 		}
 
-		lagan(subchain, 
-			infix(host(seg_0), rightDim0(*it), endPosition(seg_0)), 
-			infix(host(seg_1), rightDim1(*it), endPosition(seg_1)),
+		laganChaining(subchain, 
+			infix(host(a), rightDim0(*it), endPosition(a)), 
+			infix(host(b), rightDim1(*it), endPosition(b)),
 			q, q_min, limit, gaps_max, scoring_scheme, score_min);
 		chain.splice(it2, subchain);
 	}
 }
 
-template <typename TString>
-void 
-loadFile(char const * filename,
-		 TString & str)
-{
-	//load file into str
-	fstream fstrm;
-	fstrm.open(filename, ios_base::in | ios_base::binary);
-	if (fstrm.fail())
-	{
-		cout << "error: " << filename << " not found" << endl;
-		return;
-	}
-    read(fstrm, str, Fasta());
-    fstrm.close(); 
-}
-
-int
-main()
+int main()
 {
 	//load sequences
 	typedef String<Dna> TString;
-	TString str_0;
-	loadFile("lagan1.fasta", str_0);
-	TString str_1;
-	loadFile("lagan2.fasta", str_1);
+	TString a = String<Dna, FileReader<Fasta> >("lagan1.fasta");
+	TString b = String<Dna, FileReader<Fasta> >("lagan2.fasta");
 
 	//call lagan
-	if ((length(str_0) > 0) && (length(str_1) > 0))
+	if ((length(a) > 0) && (length(b) > 0))
 	{
 		typedef Seed<int, SimpleSeed> TSeed;
 		list<TSeed> chain;
 
 		//Step 1 to 3
-		lagan(chain, 
-			infix(str_0, 0, length(str_0)), 
-			infix(str_1, 0, length(str_1)),
+		laganChaining(chain, 
+			infix(a, 0, length(a)), 
+			infix(b, 0, length(b)),
 			13, 8, 6, 200, SimpleScore(3,-2,-1,-3), 30);
 
 		//Step 4: banded alignment
 		Align<TString, ArrayGaps> alignment;
 		resize(rows(alignment), 2);
-		setSource(row(alignment, 0), str_0);
-		setSource(row(alignment, 1), str_1);
+		setSource(row(alignment, 0), a);
+		setSource(row(alignment, 1), b);
 		int score = bandedChainAlignment(chain, 7, alignment, SimpleScore(3,-2,-1,-3));
 
-		//cout << "Score: " << score << endl;
+		cout << "Score: " << score << endl;
 		cout << alignment << endl;
 	}
 	else
 	{
 		cout << "Error - File problem" << endl;
 	}
+	return 0;
 }
