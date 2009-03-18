18b580e766593340a36357a03227aac4e75c3710
rausch@PCPOOL.MI.FU-BERLIN.DE <rausch@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2009-03-18 16:48:38 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@3692 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/iterator.cpp
+++ seqan/projects/library/demos/iterator.cpp
@@ -1,46 +1,42 @@
+///A tutorial about the use of iterators.
 #include <iostream>
 #include <seqan/sequence.h>
 #include <seqan/file.h>
 
 int main()
 {
-	seqan::String<char> str = "admn";
 ///The metafunction @Metafunction.Iterator@ returns the iterator type for a given container type.
+	seqan::String<char> str = "admn";
 	seqan::Iterator<seqan::String<char> >::Type it = begin(str);
-
-	std::cout << *it;                //output: 'a'
-///The following lines show a loop through $str$ in the standard library style:
-	while (it != end(str))           //output: "admn"
-	{
-		std::cout << *it;
+	seqan::Iterator<seqan::String<char> >::Type itEnd = end(str);
+///We can use iterators to iterate over the elements of a container.
+	while (it != itEnd) {
+		::std::cout << *it;
 		++it;
 	}
-	std::cout << std::endl;
-///Seqan offers an alternative style for accessing iterators that avoids operators.
-///Note that the functions @Function.goBegin@ and @Function.atEnd@ do net get $str$ as arguments,
-/// because $it2$ is a @Concept.Rooted Iterator.rooted iterator@.
-///The following loop increments each character in $str$:
+	::std::cout << ::std::endl;
+///Rooted iterators know their container (@Concept.Rooted Iterator.rooted iterator@).
+///Hence, the functions @Function.goBegin@ and @Function.atEnd@ do
+///not get $str$ as an argument.
+///The following loop increments each character in $str$.
 	seqan::Iterator<seqan::String<char>, seqan::Rooted >::Type it2 = begin(str);
 	for (goBegin(it2); !atEnd(it2); goNext(it2)) 
 	{
 		++value(it2);
 	}
-///This is a reverse loop through $str$.
-///Note that @Function.goPrevious@ is called before the value of $it2$ is accessed,
-/// because the end position of a container is the position behind the last item in the container:
+///Some iterators support an iteration in reverse order.
+///Note that @Function.goPrevious@ is called before the value of $it2$ is accessed.
+///Remember that the end position of a container is always the position behind the last item in the container.
 	goEnd(it2);
-
-	while (!atBegin(it2))              //output: "oneb"
+	while (!atBegin(it2))              
 	{
 		goPrevious(it2);
-		std::cout << getValue(it2);
+		::std::cout << getValue(it2);
 	}
-	std::cout << std::endl;
-///Another (write only) way to access the value of an iterator is @Function.assignValue@:
+	::std::cout << ::std::endl;
+///@Function.assignValue@ can be used to change the value of an iterator.
 	assignValue(begin(str), 'X');
-
-	std::cout << str << std::endl;        //output: "Xeno"
+	::std::cout << str << ::std::endl;
 	
-
 	return 0;
 }
