88fa507a4b6fbf2f3ec3e4e553acd2f2a53ab2be
rausch@PCPOOL.MI.FU-BERLIN.DE <rausch@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2009-03-20 12:03:41 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@3700 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/rooted_iterator.cpp
+++ seqan/projects/library/demos/rooted_iterator.cpp
@@ -1,19 +1,22 @@
+///A tutorial about the use of rooted iterators.
 #include <iostream>
 #include <seqan/sequence.h>
 #include <seqan/file.h>
 
+using namespace seqan;
+
 int main()
 {
-	seqan::String<char> str = "abcdefg";
-///The @Metafunction.Iterator.Iterator metafunction@ returns a @Concept.Rooted Iterator.rooted iterator@ by default. 
-///	We can also specify the iterator kind explicitly by passing the @Tag.Iterator Spec.iterator spec Rooted@ as second argument.
-	seqan::Iterator<seqan::String<char>, seqan::Rooted>::Type it = begin(str);
-///The same iterator spec can be used as a tag for functions that return an iterator, e.g. @Function.begin@ or @Function.end@
-	it = end(str, seqan::Rooted());
-///A rooted iterator "knows" its container, so it supports the function @Function.container@:
+	typedef String<char> TText;
+	TText str = "abcdefg";
+///The rooted iterator can be specified by passing the @Tag.Iterator Spec.iterator spec Rooted@ as a second argument.
+	Iterator<TText, Rooted>::Type it = begin(str);
+///The same iterator spec can be used as a tag for functions that return an iterator, e.g. @Function.begin@ or @Function.end@.
+	it = begin(str, Rooted());
+///A rooted iterator "knows" its container, so it supports the function @Function.container@.
 	std::cout << container(it);          //output: "abcdefg"
-///Moreover, it is possible to apply functions like @Function.goBegin@ or @Function.position@ to rooted iterators without specify the container.
+	goNext(it);
+///It also supports functions such as @Function.goBegin@ or @Function.position@.
 	std::cout << position(it);           //output: 7
-
 	return 0;
 }
