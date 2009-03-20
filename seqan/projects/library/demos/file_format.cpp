ef651e82cc043c190eef44fd476e6277b06949d9
rausch@PCPOOL.MI.FU-BERLIN.DE <rausch@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2009-03-20 12:25:09 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@3703 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/file_format.cpp
+++ seqan/projects/library/demos/file_format.cpp
@@ -1,3 +1,4 @@
+///A tutorial about the use of files and file formats.
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -10,29 +11,27 @@ using namespace std;
 
 int main()
 {
-///Create a fasta file:
-/// Open a standard library stream for binary write. 
-/// You can use both C++ iostreams or old style FILE pointer.
+///Open a standard library stream for binary write. 
+///You can use both C++ iostreams or old style FILE pointer.
+///The file format is Fasta.
 	FILE * fl = fopen("testfile.fa", "wb");
     write(fl, "aacagtattagaccactaggaccct", "a test file", Fasta());
 	close (fl);
-///Read a fasta file into a string:
-/// Open a stram for binary read.
+///Read a fasta file.
 	fstream fstrm;
 	fstrm.open("testfile.fa", ios_base::in | ios_base::binary);
 	String<char> fasta_tag;
 	String<Dna> fasta_seq;
-
+///Read the meta-information.
 	readMeta(fstrm, fasta_tag, Fasta());
 	cout << fasta_tag << "\n";	//prints "a test file"
-
+///Read the Fasta file.
 	read(fstrm, fasta_seq, Fasta());
 	cout << fasta_seq << "\n";	//prints the sequence
 	fstrm.close();
-///Opens a file using a file reader string:
+///Open a file using a file reader string.
 	String<Dna, FileReader<Fasta> > fr("testfile.fa");
 	cout << fr << "\n";			//prints the sequence
-
 	return 0;
 }
 
