ef5ce9ac2e417d1a53a08660f7c1a811460d6001
rausch@PCPOOL.MI.FU-BERLIN.DE <rausch@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2009-03-20 14:25:21 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@3715 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/graph_algo_bfs.cpp
+++ seqan/projects/library/demos/graph_algo_bfs.cpp
@@ -1,7 +1,6 @@
-/// This code example illustrates a breadth-first search.
-
-#include <seqan/graph_algorithms.h>
+///A tutorial about breadth-first search.
 #include <iostream>
+#include <seqan/graph_algorithms.h>
 
 using namespace seqan;
 
@@ -11,38 +10,38 @@ int main()
 	typedef VertexDescriptor<TGraph>::Type TVertexDescriptor;
 	typedef EdgeDescriptor<TGraph>::Type TEdgeDescriptor;
 	typedef Size<TGraph>::Type TSize;
-/// Graph creation: 10 undirected edges {0,1}, {0,4}, ...
+///Graph creation: 10 undirected edges {0,1}, {0,4}, ...
 	TSize numEdges = 10;
 	TVertexDescriptor edges[] = {0,1, 0,4, 1,5, 2,5, 2,6, 2,3, 3,6, 3,7, 5,6, 6,7};
 	TGraph g;
 	addEdges(g, edges, numEdges);
-	std::cout << g << ::std::endl;
-/// One external property map: Vertex names
+	::std::cout << g << ::std::endl;
+///One external property map: Vertex names
 	String<char> nameMap;
 	char names[] = {'r', 's', 't', 'u', 'v', 'w', 'x', 'y'};
 	resizeVertexMap(g,nameMap, names);
-/// Out-parameters: Predecessor and distance map
+///Out-parameters: Predecessor and distance map
 	String<unsigned int> predMap;
 	String<unsigned int> distMap;
-/// Breadth-frist search from vertex 1
+///Breadth-frist search from vertex 1
 	breadth_first_search(g, 1, predMap, distMap);
-/// Console output
-	std::cout << "Breadth-First search: " << ::std::endl;
+///Console output
+	::std::cout << "Breadth-First search: " << ::std::endl;
 	typedef Iterator<TGraph, VertexIterator>::Type TVertexIterator;
 	TVertexIterator it(g);
 	while(!atEnd(it)) {
-		std::cout << "Vertex " << getProperty(nameMap, getValue(it)) << ": ";
+		::std::cout << "Vertex " << getProperty(nameMap, getValue(it)) << ": ";
 		if (getProperty(distMap, getValue(it))== _getInfinityDistance(distMap)) {
-			std::cout << "Not reachable!";
+			::std::cout << "Not reachable!";
 		} else {
-			std::cout << "Level = " << getProperty(distMap, getValue(it));
+			::std::cout << "Level = " << getProperty(distMap, getValue(it));
 		}
 		typedef Value<String<unsigned int> >::Type TPredVal;
 		TPredVal pre = getProperty(predMap, getValue(it));
 		if (pre != getNil<TVertexDescriptor>()) {
-			std::cout << ", Predecessor = " << getProperty(nameMap, pre) << ::std::endl;
+			::std::cout << ", Predecessor = " << getProperty(nameMap, pre) << ::std::endl;
 		} else {
-			std::cout << ", Predecessor = nil" << ::std::endl;
+			::std::cout << ", Predecessor = nil" << ::std::endl;
 		}
 		goNext(it);
 	}
--- seqan/projects/library/demos/graph_algo_dfs.cpp
+++ seqan/projects/library/demos/graph_algo_dfs.cpp
@@ -1,7 +1,6 @@
-/// This code example illustrates a depth-first search.
-
-#include <seqan/graph_algorithms.h>
+///A tutorial about depth-first search.
 #include <iostream>
+#include <seqan/graph_algorithms.h>
 
 using namespace seqan;
 
@@ -12,36 +11,36 @@ int main()
 	typedef VertexDescriptor<TGraph>::Type TVertexDescriptor;
 	typedef EdgeDescriptor<TGraph>::Type TEdgeDescriptor;
 	typedef Size<TGraph>::Type TSize;
-/// Graph creation: 8 directed edges (0,3), (0,1), ...
+///Graph creation: 8 directed edges (0,3), (0,1), ...
 	TSize numEdges = 8;
 	TVertexDescriptor edges[] = {0,3, 0,1, 1,4, 2,4, 2,5, 3,1, 4,3, 5,5};
 	TGraph g;
 	addEdges(g, edges, numEdges);
-	std::cout << g << ::std::endl;
-/// One external property map: Vertex names
+	::std::cout << g << ::std::endl;
+///One external property map: Vertex names
 	char names[] = {'u', 'v', 'w', 'x', 'y', 'z'};
 	String<char> nameMap;
 	resizeVertexMap(g,nameMap, names);
-/// Out-parameters: Predecessor and discovery maps
+///Out-parameters: Predecessor and discovery maps
 	String<unsigned int> predMap;
 	String<unsigned int> discoveryTimeMap;
 	String<unsigned int> finishingTimeMap;
-/// Depth-frist search
+///Depth-frist search
 	depth_first_search(g, predMap, discoveryTimeMap, finishingTimeMap);
-/// Console output
-	std::cout << "Depth-First search: " << ::std::endl;
+///Console output
+	::std::cout << "Depth-First search: " << ::std::endl;
 	typedef Iterator<Graph<>, VertexIterator>::Type TVertexIterator;
 	TVertexIterator it(g);
 	while(!atEnd(it)) {
-		std::cout << "Vertex " << getProperty(nameMap, getValue(it)) << ": ";
-		std::cout << "Discovery time = " << getProperty(discoveryTimeMap, getValue(it)) << ",";
-		std::cout << "Finishing time = " << getProperty(finishingTimeMap, getValue(it)) << ",";
+		::std::cout << "Vertex " << getProperty(nameMap, getValue(it)) << ": ";
+		::std::cout << "Discovery time = " << getProperty(discoveryTimeMap, getValue(it)) << ",";
+		::std::cout << "Finishing time = " << getProperty(finishingTimeMap, getValue(it)) << ",";
 		typedef Value<String<unsigned int> >::Type TPredVal;
 		TPredVal pre = getProperty(predMap, getValue(it));
 		if (pre != getNil<TVertexDescriptor>()) {
-			std::cout << "Predecessor = " << getProperty(nameMap, pre) << ::std::endl;
+			::std::cout << "Predecessor = " << getProperty(nameMap, pre) << ::std::endl;
 		} else {
-			std::cout << "Predecessor = nil" << ::std::endl;
+			::std::cout << "Predecessor = nil" << ::std::endl;
 		}
 		goNext(it);
 	}
