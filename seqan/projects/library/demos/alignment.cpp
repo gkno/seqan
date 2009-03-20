84f86ea13f33cbff986b77e9a4e13188b2dcfac2
rausch@PCPOOL.MI.FU-BERLIN.DE <rausch@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2009-03-20 13:00:56 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@3707 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/alignment.cpp
+++ seqan/projects/library/demos/alignment.cpp
@@ -1,35 +1,37 @@
+///A tutorial about global alignments.
 #include <iostream>
 #include <seqan/align.h>
 #include <seqan/graph_align.h>
 
-using namespace std;
 using namespace seqan;
 
 int main()
 {
-///Here are some sequences to align:
-	DnaString seq1 = "atcgaatgcgga";
-	DnaString seq2 = "actcgttgca";
-///Now we choose a scoring scheme with affine gap costs ("gap open" == -2, "gap extend" == -1).
+///Two DNA sequences that shall be aligned.
+	typedef String<Dna> TSequence;
+	TSequence seq1 = "atcgaatgcgga";
+	TSequence seq2 = "actcgttgca";
+///Scoring objects are used to define a scoring scheme.
+///In this case, affine gap costs with match = 0, mismatch = -1, gapextend = -1 and gapopen = -2.
 	Score<int> score(0, -1, -1, -2);
 ///Example 1: We use @Class.Align@ to align the two sequences. 
-///Since we does not specify an @Tag.Global Alignment Algorithms|algorithm tag@ when we call @Function.globalAlignment@, 
+///Since we do not specify an @Tag.Global Alignment Algorithms|algorithm tag@ when we call @Function.globalAlignment@, 
 ///a suitable algorithm (@Tag.Global Alignment Algorithms|Gotoh@) is automatically choosen.
-	Align<DnaString, ArrayGaps> align;
+	Align<TSequence, ArrayGaps> align;
 	resize(rows(align), 2);
 	assignSource(row(align, 0), seq1);
 	assignSource(row(align, 1), seq2);
 
-	cout << "Score = " << globalAlignment(align, score) << endl;
-	cout << align << endl;
+	::std::cout << "Score = " << globalAlignment(align, score) << ::std::endl;
+	::std::cout << align << ::std::endl;
 ///Example 2: We now choose explicitely the algorithm @Tag.Global Alignment Algorithms|MyersHirschberg@.
 ///Since this algorithm always works on Levenshtein distance, $score$ is ignored here.
 ///Therefore, this algorithm computes a different alignment and returns a different score.
-	cout << "Score = " << globalAlignment(align, score, MyersHirschberg()) << endl;
-	cout << align << endl;
+	::std::cout << "Score = " << globalAlignment(align, score, MyersHirschberg()) << ::std::endl;
+	::std::cout << align << ::std::endl;
 ///Example 3: We now do the same as in case 1, but now we use an @Spec.Alignment Graph@ for storing the alignment.
 ///Here we use @Tag.Global Alignment Algorithms|Hirschberg's algorithm@.
-	typedef StringSet<DnaString, Dependent<> > TStringSet;
+	typedef StringSet<TSequence, Dependent<> > TStringSet;
 	typedef Graph<Alignment<TStringSet, void> > TAlignmentGraph;
 
 	TStringSet string_set;
@@ -37,8 +39,7 @@ int main()
 	appendValue(string_set, seq2);
 	TAlignmentGraph alignment_graph(string_set);
 
-	cout << "Score = " << globalAlignment(alignment_graph, score, Hirschberg()) << endl;
-	cout << alignment_graph << endl;
-
+	::std::cout << "Score = " << globalAlignment(alignment_graph, score, Hirschberg()) << ::std::endl;
+	::std::cout << alignment_graph << ::std::endl;
 	return 0;
 }
