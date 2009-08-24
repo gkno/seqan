46340f5e124b35099567cb111f287b6b3ce27aca
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2009-08-24 13:38:02 +0000

    added LGPL headers
    git-svn-id: http://svn.seqan.de/seqan/trunk@4714 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/blast/blast_stream_hit.h
+++ seqan/projects/library/seqan/blast/blast_stream_hit.h
@@ -1,3 +1,22 @@
+/*==========================================================================
+ SeqAn - The Library for Sequence Analysis
+ http://www.seqan.de
+ ============================================================================
+ Copyright (C) 2007
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ Lesser General Public License for more details.
+ 
+ ============================================================================
+ $Id$
+ ==========================================================================*/
 #ifndef SEQAN_HEADER_BLAST_STREAM_HIT_H
 #define SEQAN_HEADER_BLAST_STREAM_HIT_H
 
