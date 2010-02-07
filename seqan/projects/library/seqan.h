43b1a635484e651d4bec075d18d4f20dd148360f
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-02-07 22:12:39 +0000

    Removing whitespace at very beginning of file.
    Moving the description of the header into the comment in the top.
    
    Reordering includes alphabetically.
    
    
    git-svn-id: http://svn.seqan.de/seqan/trunk@5119 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan.h
+++ seqan/projects/library/seqan.h
@@ -1,4 +1,4 @@
- /*==========================================================================
+/*==========================================================================
                 SeqAn - The Library for Sequence Analysis
                           http://www.seqan.de 
  ============================================================================
@@ -16,29 +16,36 @@
 
  ============================================================================
   $Id: cstream.h 954 2007-07-27 11:48:23Z doering@PCPOOL.MI.FU-BERLIN.DE $
+ ============================================================================
+  This is the musketeer header ("one for all and all for one").
  ==========================================================================*/
 
-// the musketeer header ("one for all and all for one")
+#ifndef SEQAN_H_
+#define SEQAN_H_
 
+// TODO(holtgrewe): Some headers are missing here: statistical_index.h.
+
+#include <seqan/align.h>
 #include <seqan/basic.h>
-#include <seqan/sequence.h>
+#include <seqan/blast.h>
+#include <seqan/chaining.h>
+#include <seqan/consensus.h>
 #include <seqan/file.h>
-#include <seqan/system.h>
-#include <seqan/modifier.h>
-#include <seqan/score.h>
 #include <seqan/find.h>
 #include <seqan/find_motif.h>
-#include <seqan/graph_types.h>
 #include <seqan/graph_algorithms.h>
 #include <seqan/graph_align.h>
 #include <seqan/graph_msa.h>
-//#include <seqan/graph_utils.h>
-#include <seqan/map.h>
-#include <seqan/align.h>
-#include <seqan/seeds.h>
-#include <seqan/chaining.h>
-#include <seqan/pipe.h>
+#include <seqan/graph_types.h>
 #include <seqan/index.h>
+#include <seqan/map.h>
+#include <seqan/modifier.h>
+#include <seqan/pipe.h>
 #include <seqan/refinement.h>
-#include <seqan/consensus.h>
-#include <seqan/blast.h>
+#include <seqan/score.h>
+#include <seqan/seeds.h>
+#include <seqan/sequence.h>
+#include <seqan/statistical_index.h>
+#include <seqan/system.h>
+
+#endif  // SEQAN_H_
