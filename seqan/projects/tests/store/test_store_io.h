ab5602dfe7ef914975b784bc7672a5158bbf7d97
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-02-10 14:05:37 +0000

    Adding dummy tests for fragment store (module store).
    git-svn-id: http://svn.seqan.de/seqan/trunk@5137 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/tests/store/test_store_io.h
@@ -0,0 +1,29 @@
+/*==========================================================================
+  SeqAn - The Library for Sequence Analysis
+  http://www.seqan.de 
+  ===========================================================================
+  Copyright (C) 2010
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+  
+  ===========================================================================
+  Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
+  ===========================================================================
+  Tests for the SeqAn model store, I/O functionality.
+  ===========================================================================*/
+
+#include <seqan/basic/basic_testing.h>  // For test functionality.
+#include <seqan/store.h>  // Header under test.
+
+// TODO(holtgrew): David, write me!
+SEQAN_DEFINE_TEST(test_store_io_writeme) {
+    SEQAN_ASSERT_TRUE(false);
+}
