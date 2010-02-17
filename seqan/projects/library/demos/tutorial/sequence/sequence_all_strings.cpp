4ea18d446e9157855fb7d3d634c1440cc353ff71
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-02-17 23:33:48 +0000

    Renaming permutation to all strings.
    git-svn-id: http://svn.seqan.de/seqan/trunk@5182 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/tutorial/sequence/sequence_all_strings.cpp
+++ seqan/projects/library/demos/tutorial/sequence/sequence_all_strings.cpp
@@ -6,11 +6,11 @@
 using namespace seqan;
 
 // Helper function for printPermutations().
-void printPermutationsRec(String<char> &current, int pos) {
+void printStringsRec(String<char> &current, int pos) {
   if (pos < length(current)) {
     for (char c = 'a'; c <= 'z'; ++c) {
       current[pos] = c;
-      printPermutationsRec(current, pos + 1);
+      printStringsRec(current, pos + 1);
     }
   } else {
     std::cout << current << std::endl;;
@@ -20,8 +20,8 @@ void printPermutationsRec(String<char> &current, int pos) {
 }
 
 
-// Print all permutations of the alphabet {a, ..., z} of length len.
-void printPermutations(int len) {
+// Print all strings of the alphabet {a, ..., z} of length len.
+void printStrings(int len) {
   String<char> current;
   // Build first value;
   for (int i = 0; i < len; ++i)
@@ -29,11 +29,11 @@ void printPermutations(int len) {
   // Now, current == "a" * len.
   
   // To iterate: human -- to recurse: DIVINE.
-  printPermutationsRec(current, 0);
+  printStringsRec(current, 0);
 }
 
 
 int main(int argc, char **argv) {
-  printPermutations(3);
+  printStrings(3);
   return 0;
 }
