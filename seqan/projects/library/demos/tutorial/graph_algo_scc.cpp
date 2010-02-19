9b46ba0570301112f1a19749a4c093a021fcf904
bkehr@PCPOOL.MI.FU-BERLIN.DE <bkehr@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-02-19 11:38:02 +0000

    fragment comments
    git-svn-id: http://svn.seqan.de/seqan/trunk@5193 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/tutorial/graph_algo_scc.cpp
+++ seqan/projects/library/demos/tutorial/graph_algo_scc.cpp
@@ -1,28 +1,32 @@
+// FRAGMENT(includes)
 #include <iostream>
 #include <seqan/graph_algorithms.h>
-
 using namespace seqan;
 
-
+// FRAGMENT(typedefs)
 int main() {
 	typedef Graph<Directed<> > TGraph;
 	typedef VertexDescriptor<TGraph>::Type TVertexDescriptor;
 	typedef EdgeDescriptor<TGraph>::Type TEdgeDescriptor;
 	typedef Size<TGraph>::Type TSize;
-// part 2
+
+// FRAGMENT(main-graph-construction)
 	TSize numEdges = 14;
 	TVertexDescriptor edges[] = {1,0, 0,4, 2,1, 4,1, 5,1, 6,2, 3,2, 2,3, 7,3, 5,4, 6,5, 5,6, 7,6, 7,7};
 	TGraph g;
 	addEdges(g, edges, numEdges);
 	::std::cout << g << ::std::endl;
-// part 3
+
+// FRAGMENT(vertex-map)
 	String<char> nameMap;
 	char names[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
 	resizeVertexMap(g,nameMap, names);
-// part 4
+
+// FRAGMENT(connected-components)
 	String<unsigned int> component;
 	strongly_connected_components(g, component);
-// part 5
+
+// FRAGMENT(output-connected-components)
 	::std::cout << "Strongly Connected Components: " << ::std::endl;
 	typedef Iterator<TGraph, VertexIterator>::Type TVertexIterator;
 	TVertexIterator it(g);
