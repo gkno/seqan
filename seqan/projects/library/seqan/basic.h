912effe43e9f159fa615c88d7c50278568681f92
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-02-23 17:07:26 +0000

    basic_testing.h should be included before basic_debug.h...
    git-svn-id: http://svn.seqan.de/seqan/trunk@5219 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/basic.h
+++ seqan/projects/library/seqan/basic.h
@@ -48,8 +48,8 @@
 #include <seqan/basic/basic_generated_forwards.h>
 #endif
 
-#include <seqan/basic/basic_debug.h>
 #include <seqan/basic/basic_testing.h>
+#include <seqan/basic/basic_debug.h>
 #include <seqan/basic/basic_profile.h>
 #include <seqan/basic/basic_definition.h>
 #include <seqan/basic/basic_metaprogramming.h>
