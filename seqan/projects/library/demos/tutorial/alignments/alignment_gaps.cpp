44cd9c9cfb7d25eab0e15222dd1e7e2519f9812b
emde@PCPOOL.MI.FU-BERLIN.DE <emde@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-02-26 16:41:58 +0000

    alignment demo fuer tutorial: gaps
    git-svn-id: http://svn.seqan.de/seqan/trunk@5247 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/demos/tutorial/alignments/alignment_gaps.cpp
@@ -0,0 +1,57 @@
+// FRAGMENT(main)
+#include <iostream>
+#include <seqan/align.h>
+
+using namespace seqan;
+
+int main()
+{
+// FRAGMENT(typedefs)
+	typedef String<char> TSequence;
+	typedef Gaps<String<char>,ArrayGaps> TGaps;
+
+
+// FRAGMENT(init)
+	TSequence seq1 = "CDFGHC";
+	TSequence seq2 = "CDEFGAHC";
+
+	TGaps gaps1;
+	TGaps gaps2;
+	setSource(gaps1,seq1);
+	setSource(gaps2,seq2);
+
+// FRAGMENT(manipulation)
+	insertGap(gaps1,2);
+	insertGap(gaps1,5);
+
+// FRAGMENT(printing)
+	::std::cout << (TSequence)gaps1 << ::std::endl;
+	::std::cout << (TSequence)gaps2 << ::std::endl;
+
+// FRAGMENT(printingViewPos)
+	::std::cout << ::std::endl << "ViewToSource1: ";
+	for(unsigned i = 0; i < length(gaps1); ++i)
+		::std::cout << toSourcePosition(gaps1, i) << ",";
+	::std::cout << "\n";
+
+	::std::cout << "ViewToSource2: ";
+	for(unsigned i = 0; i < length(gaps2); ++i)
+		::std::cout << toSourcePosition(gaps2, i) << ",";
+	::std::cout << "\n";
+
+
+// FRAGMENT(printingSourcePos)
+	::std::cout << ::std::endl << "SourceToView1: ";
+	for(unsigned i = 0; i < length(source(gaps1)); ++i)
+		::std::cout << toViewPosition(gaps1, i) << ",";
+	::std::cout << "\n";
+
+	::std::cout << "SourceToView2: ";
+	for(unsigned i = 0; i < length(source(gaps2)); ++i)
+		::std::cout << toViewPosition(gaps2, i) << ",";
+	::std::cout << "\n";
+
+
+
+	return 0;
+}
