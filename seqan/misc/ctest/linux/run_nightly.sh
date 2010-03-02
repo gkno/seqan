ee497e1bc36fff46b6a8ccf99b0b2a94059a5d7f
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-02 09:46:12 +0000

    Redirect instead of tee in run_nightly scripts.
    git-svn-id: http://svn.seqan.de/seqan/trunk@5277 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/misc/ctest/linux/run_nightly.sh
+++ seqan/misc/ctest/linux/run_nightly.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $HOME/Nightly
-ctest -S Linux_Nightly.cmake -VV -d 2>&1 | tee CTEST_nightly.log
+ctest -S Linux_Nightly.cmake -VV -d 2>&1 > CTEST_nightly.log
--- seqan/misc/ctest/mac/run_nightly.sh
+++ seqan/misc/ctest/mac/run_nightly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $HOME/Nightly
-/opt/local/bin/ctest -S Mac_Nightly.cmake -VV -d 2>&1 | tee CTEST_nightly.log
+/opt/local/bin/ctest -S Mac_Nightly.cmake -VV -d 2>&1 > CTEST_nightly.log
 
