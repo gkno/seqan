308c718d97d5e262d078ccce56e3e19fb61bdc9a
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-02 17:03:35 +0000

    Converting test for consensus module to new style. test_consensus.h was empty, removing.
    git-svn-id: http://svn.seqan.de/seqan/trunk@5282 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/tests/consensus/test_consensus.cpp
+++ seqan/projects/tests/consensus/test_consensus.cpp
@@ -1,41 +1,13 @@
-#define SEQAN_DEBUG
-#define SEQAN_TEST
-#define SEQAN_VERBOSE
-
-// Test path
-#define TEST_PATH "projects/tests/consensus/"
-
-// External / STL
-#include <iostream>
-#include <fstream>
-#include <string>
-
-//SeqAn
+#include <seqan/basic.h>
 #include <seqan/consensus.h>
 
-// Test files
-#include "test_consensus.h"
+SEQAN_BEGIN_TESTSUITE(test_consensus) {
+    // Yep, no tests here :(
 
-
-using namespace seqan;
-
-//////////////////////////////////////////////////////////////////////////////
-
-int main() 
-{
-	SEQAN_TREPORT("TEST BEGIN")
-
-	// Redirect std::cout
-    std::ofstream file(TEST_PATH "redirect.txt");
-    std::streambuf* strm_puffer = std::cout.rdbuf();
-    std::cout.rdbuf(file.rdbuf());
-
-	Test_Consensus();		// Test Graph Consensus
-
-	// Restore std::cout
-	std::cout.rdbuf(strm_puffer);
-
-	SEQAN_TREPORT("TEST END")
-
-	return 0;
+    SEQAN_VERIFY_CHECKPOINTS("projects/library/seqan/consensus/consensus_base.h");
+    SEQAN_VERIFY_CHECKPOINTS("projects/library/seqan/consensus/consensus_generated_forwards.h");
+    SEQAN_VERIFY_CHECKPOINTS("projects/library/seqan/consensus/consensus_library.h");
+    SEQAN_VERIFY_CHECKPOINTS("projects/library/seqan/consensus/consensus_realign.h");
+    SEQAN_VERIFY_CHECKPOINTS("projects/library/seqan/consensus/consensus_score.h");
 }
+SEQAN_END_TESTSUITE
