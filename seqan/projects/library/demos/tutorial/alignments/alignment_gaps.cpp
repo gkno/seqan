aa403622924529e4b53a05eec35b1092a2bd63b2
emde@PCPOOL.MI.FU-BERLIN.DE <emde@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-05 11:58:15 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@5317 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/tutorial/alignments/alignment_gaps.cpp
+++ seqan/projects/library/demos/tutorial/alignments/alignment_gaps.cpp
@@ -7,44 +7,45 @@ using namespace seqan;
 int main()
 {
 // FRAGMENT(typedefs)
-	typedef String<char> TSequence;
-	typedef Gaps<String<char>,ArrayGaps> TGaps;
-
+	typedef String<char> TSequence;					// sequence type
+	typedef Align<TSequence,ArrayGaps> TAlign;		// align type
+	typedef Row<TAlign>::Type TRow;					// gapped sequence type
 
 // FRAGMENT(init)
 	TSequence seq1 = "CDFGHC";
 	TSequence seq2 = "CDEFGAHC";
 
-	TGaps gaps1;
-	TGaps gaps2;
-	setSource(gaps1,seq1);
-	setSource(gaps2,seq2);
+	TAlign align;
+	resize(rows(align), 2); 
+	assignSource(row(align,0),seq1);
+	assignSource(row(align,1),seq2);
 
 // FRAGMENT(manipulation)
-	insertGap(gaps1,2);
-	insertGap(gaps1,5);
+	TRow &row1 = row(align,0);
+	TRow &row2 = row(align,1);
+	insertGap(row1,2);
+	insertGap(row1,5);
 
-	::std::cout << (TSequence)gaps1 << ::std::endl;
-	::std::cout << (TSequence)gaps2 << ::std::endl;
-
+	::std::cout << align << ::std::endl;
+	
 // FRAGMENT(printingViewPos)
 	::std::cout << ::std::endl << "ViewToSource1: ";
-	for(unsigned i = 0; i < length(gaps1); ++i)
-		::std::cout << toSourcePosition(gaps1, i) << ",";
+	for(unsigned i = 0; i < length(row1); ++i)
+		::std::cout << toSourcePosition(row1, i) << ",";
 	
 	::std::cout << ::std::endl << "ViewToSource2: ";
-	for(unsigned i = 0; i < length(gaps2); ++i)
-		::std::cout << toSourcePosition(gaps2, i) << ",";
+	for(unsigned i = 0; i < length(row2); ++i)
+		::std::cout << toSourcePosition(row2, i) << ",";
 	::std::cout << ::std::endl;
 
 // FRAGMENT(printingSourcePos)
 	::std::cout << ::std::endl << "SourceToView1: ";
-	for(unsigned i = 0; i < length(source(gaps1)); ++i)
-		::std::cout << toViewPosition(gaps1, i) << ",";
+	for(unsigned i = 0; i < length(source(row1)); ++i)
+		::std::cout << toViewPosition(row1, i) << ",";
 
 	::std::cout << ::std::endl << "SourceToView2: ";
-	for(unsigned i = 0; i < length(source(gaps2)); ++i)
-		::std::cout << toViewPosition(gaps2, i) << ",";
+	for(unsigned i = 0; i < length(source(row2)); ++i)
+		::std::cout << toViewPosition(row2, i) << ",";
 	::std::cout << ::std::endl;
 
 
