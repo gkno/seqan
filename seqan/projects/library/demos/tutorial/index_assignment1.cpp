e7eb1e48d3ea9f7a36328837d7beec2f7170a015
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-10 09:51:30 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@5372 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/demos/tutorial/index_assignment1.cpp
@@ -0,0 +1,40 @@
+// FRAGMENT(includes)
+#include <iostream>
+#include <seqan/index.h>
+
+using namespace seqan;
+
+// FRAGMENT(initialization)
+int main ()
+{
+	StringSet<CharString> myStringSet;
+  appendValue(myStringSet, "tobeornottobe");
+  appendValue(myStringSet, "thebeeonthecomb");
+  appendValue(myStringSet, "beingjohnmalkovich");
+
+	typedef Index< StringSet<CharString> > TMyIndex;
+	TMyIndex myIndex(myStringSet);
+
+// FRAGMENT(iteration1)
+	Iterator< TMyIndex, TopDown< ParentLinks<Postorder> > >::Type myIterator(myIndex);
+
+  // top-down iterators start in the root node which is the first node of a postorder DFS
+  // thus we have to manually go the DFS start with goBegin
+  goBegin(myIterator);
+	while (!atEnd(myIterator))
+	{
+		std::cout << representative(myIterator) << std::endl;
+		++myIterator;
+	}
+
+// FRAGMENT(iteration2)
+	Iterator< TMyIndex, BottomUp<> >::Type myIterator2(myIndex);
+
+	while (!atEnd(myIterator2))
+	{
+		std::cout << representative(myIterator2) << std::endl;
+		++myIterator2;
+	}
+
+	return 0;
+}
