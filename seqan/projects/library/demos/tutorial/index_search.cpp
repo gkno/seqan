36c0ae0dd874425f2de9fad5832de93f560854c7
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-11 08:56:26 +0000

    index tut
    git-svn-id: http://svn.seqan.de/seqan/trunk@5392 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/demos/tutorial/index_search.cpp
@@ -0,0 +1,34 @@
+// FRAGMENT(includes)
+#include <iostream>
+#include <seqan/index.h>
+
+using namespace seqan;
+
+// FRAGMENT(initialization)
+int main ()
+{
+	typedef Index<CharString> TIndex;
+	TIndex index("How many wood would a woodchuck chuck.");
+	Iterator< TIndex, TopDown<> >::Type it(index);
+// FRAGMENT(iteration)
+
+	CharString pattern = "wood";
+	while (repLength(it) < length(pattern))
+	{
+		// go down edge starting with the next pattern character
+		if (!goDown(it, pattern[repLength(it)])) break;
+		unsigned endPos = _min(repLength(it), length(pattern));
+		// compare remaining edge characters with pattern
+		std::cout << representative(it) << std::endl;
+		if (infix(representative(it), parentRepLength(it) + 1, endPos) !=
+			infix(pattern, parentRepLength(it) + 1, endPos)) break;
+	}
+
+	// output match positions if pattern was found
+
+	if (repLength(it) >= length(pattern))
+		for (unsigned i = 0; i < length(getOccurrences(it)); ++i)
+			std::cout << getOccurrences(it)[i] << std::endl;
+
+	return 0;
+}
