0612979fd688c47b6df30c8b3cac4f5544bca9e6
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-17 21:23:00 +0000

    Fixing path to test files for score tests (hopefully for good this time).
    git-svn-id: http://svn.seqan.de/seqan/trunk@5452 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/tests/score/test_score.cpp
+++ seqan/projects/tests/score/test_score.cpp
@@ -93,7 +93,7 @@ SEQAN_DEFINE_TEST(test_score_matrix) {
 
     // Define path to BLOSUM62 matrix that we want to load.
     // TODO(holtgrew): It should be easier to construct these paths.
-    String<char> pathToTestSrc = SEQAN_PROGRAM_PATH;
+    String<char> pathToTestSrc = SEQAN_PATH_TO_PROJECTS();
     append(pathToTestSrc, "/../../../tests/score/");
     String<char> pathToBlosum62(pathToTestSrc);
     append(pathToBlosum62, "BLOSUM62");
@@ -219,7 +219,7 @@ SEQAN_DEFINE_TEST(test_score_matrix) {
 SEQAN_DEFINE_TEST(test_score_matrix_file) {
     // TODO(holtgrew): It should be easier to construct these paths.
     // The path to the directory with the test's sources and fixtures.
-    String<char> pathToTestSrc = SEQAN_PROGRAM_PATH;
+    String<char> pathToTestSrc = SEQAN_PATH_TO_PROJECTS();
     append(pathToTestSrc, "/../../../tests/score/");
 
     // Load fixture BLOSUM62 matrix.
