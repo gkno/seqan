060746519364f4e04c427ef5a7e1e9dd472c29ef
bkehr@PCPOOL.MI.FU-BERLIN.DE <bkehr@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-18 13:53:35 +0000

    removed unnecessary resizeEdgeMap
    git-svn-id: http://svn.seqan.de/seqan/trunk@5459 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/tutorial/graph/graph_dijkstra.cpp
+++ seqan/projects/library/demos/tutorial/graph/graph_dijkstra.cpp
@@ -59,7 +59,11 @@ int main ()
 // FRAGMENT(dijkstra-containers)
 	typedef Size<TGraph>::Type TSize;
 	InternalMap<TCargo> cargoMap;
-	resizeEdgeMap(g,cargoMap);
+    Iterator<TGraph, EdgeIterator>::Type itE(g);
+    while(!atEnd(itE)) {
+        ::std::cout << property(cargoMap, value(itE)) << std::endl;
+        ++itE;
+    }
 	String<TVertexDescriptor> predMap;
 	String<TSize> distMap;
 // FRAGMENT(dijkstra)
