734310e051bff346ffab416692c8d36f92168814
bkehr@PCPOOL.MI.FU-BERLIN.DE <bkehr@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-19 10:28:39 +0000

    Seed extension and banded alignment demo
    git-svn-id: http://svn.seqan.de/seqan/trunk@5470 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/demos/tutorial/seeds/seeds_extend_bandedAlign.cpp
@@ -0,0 +1,58 @@
+//FRAGMENT(includes)
+#include <seqan/seeds.h>
+
+using namespace seqan;
+
+// FRAGMENT(writeSeed)
+template<typename TSeed, typename TSeq>
+void
+writeSeed(TSeed & seed, TSeq const & seq0, TSeq const & seq1) {
+    std::cout << "Seed from position " << leftPosition(seed, 0);
+    std::cout << " to " << rightPosition(seed, 0) << ": ";
+    std::cout << infix(seq0, leftPosition(seed, 0), rightPosition(seed, 0)+1) << std::endl;
+    std::cout << "Seed from position " << leftPosition(seed, 1);
+    std::cout << " to " << rightPosition(seed, 1) << ": ";
+    std::cout << infix(seq1, leftPosition(seed, 1), rightPosition(seed, 1)+1) << std::endl;
+}
+
+//FRAGMENT(main)
+int main() {
+    typedef Seed<> TSeed;
+    
+    DnaString seq0 = "ATCATCAGTTATACTTTACCCAGGC";
+    DnaString seq1 = "ATTCAGCATACTTTCCATGAAGC";
+
+    TSeed seed(10, 7, 7);
+    writeSeed(seed, seq0, seq1);    
+
+// FRAGMENT(extension)
+    typedef int TScore;
+    Score<TScore, Simple> scoreMatrix(1, -1, -1);
+    TScore scoreDropOff = 1;
+    extendSeed(seed, scoreDropOff, scoreMatrix, seq0, seq1, 2, GappedXDrop());
+
+    std::cout << std::endl << "After extension:" << std::endl;
+    writeSeed(seed, seq0, seq1);  
+
+// FRAGMENT(banded-alignment)
+    Align<DnaString> align;
+    resize(rows(alignment), 2);
+    assignSource(row(align, 0), seq0);
+    assignSource(row(align, 1), seq1);
+    
+    std::cout << std::endl << "Banded Alignment:" << std::endl;
+    std::cout << "Score: " << bandedAlignment(align, seed, 2, scoreMatrix) << std::endl;
+    std::cout << align;
+
+// FRAGMENT(global-alignment)
+    Align<DnaString> globalAlign;
+    resize(rows(globalAlign), 2);
+    assignSource(row(globalAlign, 0), seq0);
+    assignSource(row(globalAlign, 1), seq1);
+
+    std::cout << std::endl << "Global Alignment:" << std::endl;
+    std::cout << "Score: " << globalAlignment(globalAlign, scoreMatrix) << std::endl;
+    std::cout << globalAlign;
+
+    return 0;
+}
