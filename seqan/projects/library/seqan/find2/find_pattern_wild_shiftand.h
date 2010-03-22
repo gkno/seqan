d2959492b7ae63e7bf53f6259a4cdd1e34b7f887
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-22 20:04:35 +0000

    Adding pattern wild shift and algorithm to find2.
    git-svn-id: http://svn.seqan.de/seqan/trunk@5513 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/seqan/find2/find_pattern_wild_shiftand.h
@@ -0,0 +1,118 @@
+/*==========================================================================
+                SeqAn - The Library for Sequence Analysis
+                          http://www.seqan.de 
+ ============================================================================
+  Copyright (C) 2007-2010
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+ ============================================================================
+  Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
+ ============================================================================
+  Wildcard pattern matching using a modification of the Shift-And Algorithm.
+ ==========================================================================*/
+
+#ifndef SEQAN_FIND2_FIND_PATTERN_WILD_SHIFTAND_H_
+#define SEQAN_FIND2_FIND_PATTERN_WILD_SHIFTAND_H_
+
+namespace seqan {
+
+template <typename TNeedle>
+struct Pattern2<TNeedle, WildShiftAnd> {
+    Holder<TNeedle> _host;
+
+    Pattern2() {}
+};
+
+
+template <typename TNeedle>
+TNeedle const & host(Pattern2<TNeedle, WildShiftAnd> const & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename TNeedle>
+TNeedle const & needle(Pattern2<TNeedle, WildShiftAnd> const & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename TNeedle>
+typename Position<TNeedle>::Type length(Pattern2<TNeedle, WildShiftAnd> const & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename TNeedle>
+Segment<TNeedle, InfixSegment> infix(Pattern2<TNeedle, WildShiftAnd> const & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename TNeedle>
+typename Iterator<TNeedle>::Type begin(Pattern2<TNeedle, WildShiftAnd> const & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename TNeedle>
+typename Iterator<TNeedle>::Type end(Pattern2<TNeedle, WildShiftAnd> const & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename TNeedle>
+typename Position<TNeedle>::Type beginPosition(Pattern2<TNeedle, WildShiftAnd> const & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename TNeedle>
+typename Position<TNeedle>::Type endPosition(Pattern2<TNeedle, WildShiftAnd> const & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename THaystack, typename TNeedle>
+bool find(Finder2<THaystack, void> & finder,  // TODO(holtgrew): "Default" better than void?
+          Pattern2<TNeedle, WildShiftAnd> & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename THaystack, typename TNeedle>
+bool findBegin(Finder2<THaystack, void> & finder,  // TODO(holtgrew): "Default" better than void?
+               Pattern2<TNeedle, WildShiftAnd> & pattern) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+
+template <typename THaystack, typename TNeedle, typename TAlignSeq, typename TAlignSpec>
+bool alignment(Finder2<THaystack, void> &finder,  // TODO(holtgrew): "Default" better than void?
+               Pattern2<TNeedle, WildShiftAnd> &pattern,
+               Align<TAlignSeq, TAlignSpec> &outAlignment) {
+    SEQAN_CHECKPOINT;
+    SEQAN_ASSERT_FAIL("Implement me!");
+}
+
+}  // namespace seqan
+          
+#endif  // SEQAN_FIND2_FIND_PATTERN_WILD_SHIFTAND_H_
