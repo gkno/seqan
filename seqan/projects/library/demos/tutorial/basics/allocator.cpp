2b9dda8159dc062abe552ccc03646789e6643813
reinert@PCPOOL.MI.FU-BERLIN.DE <reinert@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-23 10:19:44 +0000

    git-svn-id: http://svn.seqan.de/seqan/trunk@5527 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/projects/library/demos/tutorial/basics/allocator.cpp
@@ -0,0 +1,43 @@
+// FRAGMENT(defintions)
+#define SEQAN_PROFILE // enable time measurements
+
+#include <iostream>
+#include <seqan/basic.h>
+
+
+using namespace seqan;
+
+int main()
+{
+	Allocator<MultiPool< > > mpa;
+	Allocator<SimpleAlloc< > > sa;
+	unsigned runs = 100000;
+	char *buf;
+// FRAGMENT(time-measurements)
+	// block size 10
+	unsigned bs[3];
+	bs[0] = 10;
+	bs[1] = 100;
+	bs[2] = 1000;
+	SEQAN_PROTIMESTART(timeAlloc);
+	
+	// loop through the different block sizes
+	for (int i=0; i<3; ++i) {
+		for (int j=0; j<runs; ++j) {
+			allocate(mpa,buf,bs[i],TagAllocateTemp());
+		}
+		clear(mpa);
+		std::cout << "Allocating " << runs << " times blocks of size " 
+					<< bs[i] << " with MultiPool Allocator took " << SEQAN_PROTIMEDIFF(timeAlloc) << std::endl;	
+	// the PROTIMEDIFF macro sets the internal timer clock to zero
+		for (int j=0; j<runs; ++j) {
+			allocate(sa,buf,bs[i],TagAllocateTemp());
+		}
+		clear(sa);
+		std::cout << "Allocating " << runs << " times blocks of size " 
+		<< bs[i] << " with Standard Allocator took " << SEQAN_PROTIMEDIFF(timeAlloc) << std::endl;	
+	}
+
+	return 0;
+}
+
