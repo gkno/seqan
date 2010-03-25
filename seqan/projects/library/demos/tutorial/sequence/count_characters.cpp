e2bc710d037814cb0fc7b6b94f2a216b086ed4f7
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-25 15:38:10 +0000

    Fragments for the character counting assingment.
    git-svn-id: http://svn.seqan.de/seqan/trunk@5573 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/tutorial/sequence/count_characters.cpp
+++ seqan/projects/library/demos/tutorial/sequence/count_characters.cpp
@@ -1,23 +1,29 @@
+//FRAGMENT(includes)
 #include <iostream>
 #include <seqan/sequence.h>
 
 using namespace seqan;
 
+//FRAGMENT(count-one-mers-begin)
 void countOneMers(CharString const & str) {
+	//FRAGMENT(count-one-mers-initialize-table)
 	String<int> table;
-	resize(table, 'z' - 'a' + 1);
+	fill(table, 'z' - 'a' + 1, 0);
 	
+	//FRAGMENT(count-one-mers-count-chars)
 	for (int i = 0; i < length(str); ++i) {
 		table[str[i] - 'a'] += 1;
 	}
 	
+	//FRAGMENT(count-one-mers-print-chars)
 	for (int i = 0; i < 'z' - 'a' + 1; ++i) {
 		if (table[i] == 0)
 			continue;
-		std::cout << 'a' + i << " " << table[i] << std::endl;
+		std::cout << static_cast<char>('a' + i) << " " << table[i] << std::endl;
 	}
 }
 
+//FRAGMENT(main)
 int main(int argc, char **argv) {
 	std::cout << "String: helloworld" << std::endl;
 	countOneMers("helloworld");
