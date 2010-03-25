9a58b21337249927adb0df4b12c45293f4c9c1e8
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-25 15:28:43 +0000

    Fixing count_characters demo.
    git-svn-id: http://svn.seqan.de/seqan/trunk@5572 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/tutorial/sequence/count_characters.cpp
+++ seqan/projects/library/demos/tutorial/sequence/count_characters.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <seqan/sequence.h>
 
-void countOneMers(const CharString &str) {
-  String<int> table;
-  resize(table, 'z' - 'a' + 1);
+using namespace seqan;
 
-  for (int i = 0; i < length(str); ++i) {
-    table[str[i] - 'a'] += 1;
-  }
-
-  for (int i = 0; i < 'z' - 'a' + 1; ++i) {
-    std::cout << 'a' + i << " " << table[i] << std::endl;
-  }
+void countOneMers(CharString const & str) {
+	String<int> table;
+	resize(table, 'z' - 'a' + 1);
+	
+	for (int i = 0; i < length(str); ++i) {
+		table[str[i] - 'a'] += 1;
+	}
+	
+	for (int i = 0; i < 'z' - 'a' + 1; ++i) {
+		if (table[i] == 0)
+			continue;
+		std::cout << 'a' + i << " " << table[i] << std::endl;
+	}
 }
 
 int main(int argc, char **argv) {
-  std::cout << "String: hello world" << std::endl;
-  countOneMers("hello world");
-  std::cout << "String: mississippi" << std::endl;
-  countOneMers("mississippi");
-  std::cout << "String: banana" << std::endl;
-  countOneMers("banana");
-  return 0;
+	std::cout << "String: helloworld" << std::endl;
+	countOneMers("helloworld");
+	
+	std::cout << "String: mississippi" << std::endl;
+	countOneMers("mississippi");
+	
+	std::cout << "String: banana" << std::endl;
+	countOneMers("banana");
+	
+	return 0;
 }
