ce219892d966083741b0117043973d396da70a66
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-30 12:13:55 +0000

    changed style
    git-svn-id: http://svn.seqan.de/seqan/trunk@5623 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/tutorial/basics/strings.cpp
+++ seqan/projects/library/demos/tutorial/basics/strings.cpp
@@ -12,32 +12,27 @@ int main()
 // FRAGMENT(iterate-and-replace)
 	typedef Iterator<TAminoAcidString>::Type TIter;
 
-	TIter it = begin(sourceSeq);
 	TIter itEnd = end(sourceSeq);
-	for(;it != itEnd; goNext(it)) {
+	for (TIter it = begin(sourceSeq); it != itEnd; goNext(it))
+	{
 		if (value(it) == 'R') value(it) = 'A';
-		::std::cout << value(it) << ',';
+		std::cout << value(it) << ',';
 	}
-	::std::cout << ::std::endl;
+	std::cout << std::endl;
 // FRAGMENT(count-occurences)
 	typedef Size<TAminoAcidString>::Type TSize;
 	typedef String<TSize> TCounterString;
 	TCounterString counter;
 	TSize alphSize = ValueSize<AminoAcid>::VALUE;
 	fill(counter, alphSize, 0);
-	it = begin(sourceSeq);
-	for(;it != itEnd; goNext(it)) {
+	for (TIter it = begin(sourceSeq); it != itEnd; goNext(it))
 		value(counter, ordValue(value(it))) += 1;
-	}
 // FRAGMENT(frequency-table)
 	typedef Iterator<TCounterString>::Type TCounterIter;
 	TCounterIter countIt = begin(counter);
 	TCounterIter countItEnd = end(counter);
-	TSize pos = 0;
-	for(;countIt != countItEnd; ++countIt, ++pos) {
-		::std::cout << AminoAcid(pos) << ':' << value(countIt) << ::std::endl;
-	}
-
+	for (TSize pos = 0; countIt != countItEnd; ++countIt, ++pos)
+		std::cout << AminoAcid(pos) << ':' << value(countIt) << std::endl;
 
 	return 0;
 }
