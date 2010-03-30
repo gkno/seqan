3df2d941f2da3144b38f2b67a1e12fecf608ae09
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-30 11:51:36 +0000

    changed style
    git-svn-id: http://svn.seqan.de/seqan/trunk@5621 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/tutorial/basics/show_alphabets.cpp
+++ seqan/projects/library/demos/tutorial/basics/show_alphabets.cpp
@@ -1,19 +1,18 @@
 // FRAGMENT(includes)
-#include <iostream>
 #include <seqan/sequence.h>
 #include <seqan/basic.h>
+#include <iostream>
 
 using namespace seqan;
 // FRAGMENT(showAllLetterOfMyAlphabet)
-template<typename TAlphabet>
-void
-showAllLetterOfMyAlphabet(TAlphabet const&) {
+template <typename TAlphabet>
+void showAllLetterOfMyAlphabet(TAlphabet const &)
+{
 	typedef typename Size<TAlphabet>::Type TSize;
 	TSize alphSize = ValueSize<TAlphabet>::VALUE;
-	for(TSize i = 0; i < alphSize; ++i) {
-		::std::cout << i << "," << TAlphabet(i) << "  ";
-	}
-	::std::cout << ::std::endl;
+	for (TSize i = 0; i < alphSize; ++i)
+		std::cout << i << ',' << TAlphabet(i) << "  ";
+	std::cout << std::endl;
 }
 // FRAGMENT(main)
 int main()
