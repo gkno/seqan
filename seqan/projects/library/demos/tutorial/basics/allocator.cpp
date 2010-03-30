17f323eac875ce905e11cb006d64f8164e4ce194
weese@PCPOOL.MI.FU-BERLIN.DE <weese@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-30 11:44:55 +0000

    changed to sysTime
    git-svn-id: http://svn.seqan.de/seqan/trunk@5620 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/demos/tutorial/basics/allocator.cpp
+++ seqan/projects/library/demos/tutorial/basics/allocator.cpp
@@ -1,9 +1,6 @@
 // FRAGMENT(definitions)
-#define SEQAN_PROFILE // enable time measurements
-
 #include <iostream>
 #include <seqan/basic.h>
-
 
 using namespace seqan;
 
@@ -11,31 +8,31 @@ int main()
 {
 	Allocator<MultiPool< > > mpa;
 	Allocator<SimpleAlloc< > > sa;
-	unsigned runs = 100000;
-	char *buf;
-// FRAGMENT(time-measurements)
 	// store blocksizes in an array
-	unsigned bs[3];
-	bs[0] = 10;
-	bs[1] = 100;
-	bs[2] = 1000;
-	SEQAN_PROTIMESTART(timeAlloc);
-	
+	int bs[3] = {10, 100, 1000};
+	int runs = 100000;
+	char *buf;
+	double startTime;
+// FRAGMENT(time-measurements)
+
 	// loop through the different block sizes
-	for (int i=0; i<3; ++i) {
-		for (int j=0; j<runs; ++j) {
+	for (int i=0; i<3; ++i)
+	{
+		startTime = sysTime();
+		for (int j=0; j<runs; ++j)
 			allocate(mpa,buf,bs[i],TagAllocateTemp());
-		}
 		clear(mpa);
-		std::cout << "Allocating and clearing " << runs << " times blocks of size " 
-					<< bs[i] << " with MultiPool Allocator took " << SEQAN_PROTIMEDIFF(timeAlloc) << std::endl;	
-		// the PROTIMEDIFF macro sets the internal timer clock to zero
-		for (int j=0; j<runs; ++j) {
+
+		std::cout << "Allocating and clearing " << runs << " times blocks of size ";
+		std::cout << bs[i] << " with MultiPool Allocator took " << sysTime() - startTime << std::endl;
+
+		startTime = sysTime();
+		for (int j=0; j<runs; ++j)
 			allocate(sa,buf,bs[i],TagAllocateTemp());
-		}
 		clear(sa);
-		std::cout << "Allocating and clearing " << runs << " times blocks of size " 
-		<< bs[i] << " with Standard Allocator took " << SEQAN_PROTIMEDIFF(timeAlloc) << std::endl;	
+
+		std::cout << "Allocating and clearing " << runs << " times blocks of size ";
+		std::cout << bs[i] << " with Standard Allocator took " << sysTime() - startTime << std::endl;
 	}
 
 	return 0;
