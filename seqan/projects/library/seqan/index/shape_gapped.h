d4839d38476bab97f4448fb0ab5cb4ae341f7e9d
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-03-31 13:01:30 +0000

    Suppressing unknown parameter warning for HardwiredShape and Pi.
    git-svn-id: http://svn.seqan.de/seqan/trunk@5639 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/index/shape_gapped.h
+++ seqan/projects/library/seqan/index/shape_gapped.h
@@ -33,6 +33,7 @@ namespace SEQAN_NAMESPACE_MAIN
 ..summary:A structure to define a fixed gapped shape.
 ..signature:HardwiredShape<P1, P2, ..., Pn>
 ..param.Pi:Pi is the distance of the i'th '1' to the next '1' in the shape.
+...nowarn:unknown param
 ...remarks:At most 20 parameters are allowed, so the maximal shape weight is 21.
 ..remarks:You can use this structure to define your one gapped shapes in conjunction with @Spec.GappedShape@.
 ...note:The shape $1100101$ corresponds to $HardwiredShape<1,3,2>$.
