96d6cec7389d905b1605536e244a5defcfab7a84
patel@PCPOOL.MI.FU-BERLIN.DE <patel@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-04-14 11:47:08 +0000

    missing gap removed
    git-svn-id: http://svn.seqan.de/seqan/trunk@5772 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/misc/swig/alignment.h
+++ seqan/misc/swig/alignment.h
@@ -7,22 +7,26 @@ namespace seqan
 {
 
 	#ifdef SWIG
+	//typemap(in) -> python to c++
+	//typemap(out) -> c++ to python
 
-	%typemap(in,numinputs=0)int& scoreMatrix(int temp)
+	//typemap(in,numinputs=0)int& scoreAlignment -> masked the variable scoreAlignment as a shadows argument. Meaning that SWIG will not consider this variable to be filled by calling the function in python, instead it is declaring the variable with a temporary value. This value will be replaced in the typemap(argout) later with the wanted value. 
+	%typemap(in,numinputs=0)int& scoreAlignment(int temp)
 	{
 		$1=&temp;
 	}
-	%typemap(argout)int& scoreMatrix
+	//this template shows how to return a tupel into python using a variable from the argument list of the calling function.
+	%typemap(argout)int& scoreAlignment
 	{    
-		PyObject *o, *o2, *o3;
-    o = PyInt_FromLong(*$1);
-    if ((!$result) || ($result == Py_None))
+		PyObject *o, *o2, *o3; 
+    o = PyInt_FromLong(*$1); //$1 -> the value which has to be convert to python
+    if ((!$result) || ($result == Py_None))  //check if already exist an result if not set the actual result
 		{
 			$result = o;
     }
 		else
 		{
-      if(!PyTuple_Check($result))
+      if(!PyTuple_Check($result)) //build a tuple in python 
 			 {
             PyObject *o2 = $result;
             $result = PyTuple_New(1);
@@ -36,7 +40,7 @@ namespace seqan
         Py_DECREF(o3);
     }
 	}
-
+	//see above... by using char** we have to pre allocate memory
 	%typemap(in,numinputs=0) char** str1(char* temp) 
 	{
 		$1 = (char **) malloc((strlen(temp)+1)*sizeof(char *));
@@ -45,7 +49,7 @@ namespace seqan
 	{
 		  $1 = (char **) malloc((strlen(temp)+1)*sizeof(char *));
 	}
-
+	//returns a list of char using the Python_AppendOupt function.
 	%typemap(argout)char** str1
 	{
 		$result=SWIG_Python_AppendOutput($result, PyString_FromStringAndSize(*$1,strlen(*$1)));
@@ -54,7 +58,7 @@ namespace seqan
 	{
 		$result=SWIG_Python_AppendOutput($result,PyString_FromString(*$1));
 	}
-
+	//destroy the pre allocate memory -> avoid memory leaks
 	%typemap(freearg) char ** 
 	{
 		free((char *) $1);
@@ -68,11 +72,11 @@ namespace seqan
 	{
 		  $1 = &temp;
 	}
-
+	//int array -> list of integer in python. Two information are important, the array itself and its size.
 	%typemap(argout) (int** scoreMatrix, int& aSize)
 	{
   	int i;
-  	$result = PyList_New(*$2);
+  	$result = PyList_New(*$2); //build a list with the the size aSize
   	for (i = 0; i < *$2; i++)
 	 {
     	PyObject *o =PyInt_FromLong(*(*$1+i));
@@ -86,7 +90,7 @@ namespace seqan
 		free((int *) $1);
 	}
 
-
+	//python integer list -> int array
 	%typemap(in) (int len, int *value)
 	{
 		int i;
@@ -95,8 +99,8 @@ namespace seqan
 		  PyErr_SetString(PyExc_ValueError, "Expecting a list");
 		  return NULL;
 		}
-		$1 = PyList_Size($input);
-		$2 = (int*) malloc(($1+1)*sizeof(int));
+		$1 = PyList_Size($input); //get size of the list
+		$2 = (int*) malloc(($1+1)*sizeof(int)); //allocate memory with the correct size
 		for (i = 0; i < $1; i++)
 		{
 		  PyObject *s = PyList_GetItem($input,i);
@@ -106,7 +110,7 @@ namespace seqan
 		      PyErr_SetString(PyExc_ValueError, "List items must be integers");
 		      return NULL;
 		  }
-		  $2[i] = (int)PyInt_AS_LONG(s);
+		  $2[i] = (int)PyInt_AS_LONG(s); //set the value into the array
 		}
 		$2[i] = 0;
 	}
@@ -115,7 +119,7 @@ namespace seqan
 	{
 		 if ($2) free($2);
 	}
-
+	//shows how to set default values in SWIG
 	%typemap(default) unsigned int matrix
 	{
 		 $1 = 0;
@@ -144,6 +148,7 @@ namespace seqan
 
 	#endif
 
+	//returns a specific amino acid scoring matrix selected by the input name.
 	void getAminoAcidScoreMatrix(char* str,int** scoreMatrix, int& aSize)
 	{
 		if(std::strcmp(str,"Blosum30")==0)
@@ -151,7 +156,7 @@ namespace seqan
 			typedef Score<int, ScoreMatrix<AminoAcid, _Blosum30> > TScore;
 			int const* matrix=_ScoringMatrixData<int ,AminoAcid, _Blosum30 >::getData();
 			*scoreMatrix = new int[TScore::TAB_SIZE];
-			arrayCopy(matrix, matrix + TScore::TAB_SIZE, *scoreMatrix);
+			arrayCopy(matrix, matrix + TScore::TAB_SIZE, *scoreMatrix); 
 			aSize=TScore::TAB_SIZE;
 		}
 
@@ -223,9 +228,10 @@ namespace seqan
 			aSize=TScore::TAB_SIZE;
 		}
 	}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
+//returns the score of an alignment and save the alignment itself using a reference on AlignObject.
+//char algo determines what kind of alignment algorithm is chosen.
 	template<typename Type,typename TSpec, typename TAlignConfig>
 	int _alignment(seqan::Align<seqan::String<Type> >*align,seqan::Score<int,TSpec>& score,TAlignConfig& ac,unsigned int dia1, unsigned int dia2,char *algo)
 	{
@@ -235,7 +241,8 @@ namespace seqan
 		else if(std::strcmp(algo,"BandedGotoh")==0) return globalAlignment(stringSet(*align),score,ac,dia1,dia2,BandedGotoh());
 	}
 	 
-
+	//returns the score of an alignment by calling the above _alignment() function. This function determines the
+	//correct AlignConfig using the integer given by the user.
 	template<typename Type, typename TSpec>
 	int _alignment(seqan::Align<seqan::String<Type> >*align,seqan::Score<int,TSpec>& score,unsigned int ac,char * algo,unsigned int dia1, unsigned int dia2)
 	{
@@ -323,53 +330,55 @@ namespace seqan
 									break;
 								}
 			}
-
 	return alignmentScore;
 	}
-
-//allgemeine Wrapper funktion fürs Alignment
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//main wrapper function for calling the alignment.
 	template< typename Type >
-	seqan::Align<seqan::String<Type> >* alignSequence(char* seq1, char*seq2, int len, int *value, char* algo, unsigned int matrix, unsigned int dia1, unsigned int dia2, int& scoreMatrix)
+	seqan::Align<seqan::String<Type> >* alignSequence(char* seq1, char*seq2, int len, int *value, char* algo, unsigned int matrix, unsigned int dia1, unsigned int dia2, int& scoreAlignment)
 	{
 		Align<String<Type> >* align = new Align<String<Type> >();
 		
-		String<Type> _seq1=seq1; //ist nötig!! <-python strings are immutable!! swig does not change this behaviour
+		String<Type> _seq1=seq1; //this is necessary!!! Python strings are immutable!! Swig does not care what happens with strings
 		String<Type> _seq2=seq2;
 		
 		resize(rows(*align), 2);
 		assignSource(row(*align, 0), _seq1);
 		assignSource(row(*align, 1), _seq2);
-
+		//check if the actual chosen alignment algorithm supports a score matrix
 		if(len >=4 && ValueSize<Type>::VALUE * ValueSize<Type>::VALUE == len && std::strcmp(algo,"MyersHirschberg")!=0 && std::strcmp(algo,"Hirschberg")!=0 && std::strcmp(algo,"SmithWaterman")!=0)
 		{
 			Score<int, ScoreMatrix<Type> > score;
  			arrayCopy(value, value + len, score.data_tab);
-			scoreMatrix=_alignment(align,score,matrix,algo,dia1,dia2);
+			scoreAlignment=_alignment(align,score,matrix,algo,dia1,dia2);
 		}
-		else if(len == 4)
+		else if(len == 4) 
 		{   
 			Score<int, Simple > score(*(value),*(value+1),*(value+2),*(value+3));
+			//check if we have to set AlignConfig for the current alignment algorithm
 			if(std::strcmp(algo,"MyersHirschberg")!=0 && std::strcmp(algo,"Hirschberg")!=0&&std::strcmp(algo,"SmithWaterman")!=0 )
 				{ 
-					scoreMatrix=_alignment(align,score,matrix,algo,dia1,dia2);
+					scoreAlignment=_alignment(align,score,matrix,algo,dia1,dia2);
 				}
 				else if(std::strcmp(algo,"MyersHirschberg")==0)
 				{
-					scoreMatrix=globalAlignment(*align,score,MyersHirschberg());
+					scoreAlignment=globalAlignment(*align,score,MyersHirschberg());
 				}
 				else if(std::strcmp(algo,"Hirschberg")==0)
 				{
-					scoreMatrix=globalAlignment(*align,score,Hirschberg());		
+					scoreAlignment=globalAlignment(*align,score,Hirschberg());		
 				}
 				else if(std::strcmp(algo,"SmithWaterman")==0)
 				{
-					scoreMatrix=localAlignment(*align,score, SmithWaterman());
+					scoreAlignment=localAlignment(*align,score, SmithWaterman());
 				}
 		}	
 //std::cout << *align << std::endl;
 		return align;
 	} 
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+	//returns the alignment into different output variables ->via typemap into a string list
 	template<typename Type>
 	char* printAlignment(seqan::Align<seqan::String<Type> >&align,char **str1,char **str2)
 	{
@@ -390,9 +399,10 @@ namespace seqan
 			if ((begin_ + windowSize_)>end_) windowSize_=end_ - begin_;
 			for(TRowsPosition i=0;i<2*row_count-1;++i)
 			{
-				for(unsigned int j = 0;j<leftSpace+2;++j) {
-	//std::cout << " " << strlen(pAlignment) << std::endl; //strcat(pAlignment,"\n");
-	pAlignment+=" "; }
+				for(unsigned int j = 0;j<leftSpace+2;++j)
+				{
+				pAlignment+=" "; 
+				}
 				if ((i % 2)==0) 
 				{
 					TRow& row_ = row(align, i/2);
@@ -403,22 +413,20 @@ namespace seqan
 					{
 						if (isGap(begin1_))
 						{
-							 //strcat(pAlignment, "-");
 							pAlignment+="-";
-							if(i==0) {
-	//						strcat(seq1Alignment, "-");
+							if(i==0) 
+							{
 								seq1Alignment+="-";
 							}
-							else if(i==1)seq2Alignment+="-";   // strcat(seq2Alignment, "-");
+							else if(i==2)seq2Alignment+="-"; 
 						}
 						else
 						{
 							char b=convert<char>(*begin1_);
 							char* t=(char*)b;
-							//strcat(pAlignment, &b );
 							pAlignment+=b;
-							if(i==0) seq1Alignment+=b;//strcat(seq1Alignment,&b);					
-							else if(i==2)seq2Alignment +=b;//strcat(seq2Alignment,&b);					
+							if(i==0) seq1Alignment+=b;
+							else if(i==2)seq2Alignment +=b;
 						}
 					}
 				}
@@ -428,20 +436,20 @@ namespace seqan
 					{
 						if ((!isGap(row(align, (i-1)/2), begin_+j)) &&(!isGap(row(align, (i+1)/2), begin_+j)) && (row(align, (i-1)/2)[begin_+j]==row(align, (i+1)/2)[begin_+j]))
 						{
-							pAlignment+="|";//strcat(pAlignment,"|");
+							pAlignment+="|";
 						}
 						else
 					 {
-						pAlignment+=" ";//strcat(pAlignment," ");
+						pAlignment+=" ";
 						}
 					} 
 				}
-				pAlignment+="\n";//strcat(pAlignment,"\n");
+				pAlignment+="\n";
 			}
-			pAlignment+="\n";//strcat(pAlignment,"\n");
+			pAlignment+="\n";
 			begin_+=50;
 		}
-	pAlignment+="\n";//	strcpy(pAlignment,"\n");
+	pAlignment+="\n";
 
 
 	*str1 = new char[seq1Alignment.length()+1];
@@ -453,6 +461,7 @@ namespace seqan
 	return pp;
 	}
 /*
+//overload function with seqan pointers does not work......
 	char* printAlignment(seqan::Align<seqan::String<seqan::Dna5> >&align,char **str1,char **str2)
 	{
 		return _printAlignment(align,str1,str2);
