d571cc3cdc143d7ff461560650ed7ce25e482524
holtgrew@PCPOOL.MI.FU-BERLIN.DE <holtgrew@PCPOOL.MI.FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-05-03 22:30:11 +0000

    s/class/struct/ for Score.
    git-svn-id: http://svn.seqan.de/seqan/trunk@5981 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/apps/razers2/outputFormat.h
+++ seqan/projects/library/apps/razers2/outputFormat.h
@@ -39,7 +39,7 @@ namespace SEQAN_NAMESPACE_MAIN
 	struct Quality;
 
 	template <typename TValue, typename TQualityString>
-	class Score<TValue, Quality<TQualityString> >
+	struct Score<TValue, Quality<TQualityString> >
 	{
 	public:
 		TValue data_match;
