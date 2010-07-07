0e373fbee55b796af49f4fc38dc79c3c3549e6fd
holtgrew@FU-BERLIN.DE <holtgrew@FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-07-07 09:27:50 +0000

    Adjusting identifier parsing routine.
    git-svn-id: http://svn.seqan.de/seqan/trunk@7175 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/misc/misc_parsing.h
+++ seqan/projects/library/seqan/misc/misc_parsing.h
@@ -125,7 +125,7 @@ template<typename TChar>
 inline bool
 _parse_isAlphanumericChar(TChar const c)
 {
-	return ((_parse_isDigit(c)) || (_parse_isLetter(c)) || (c == '_') || (c == '.') || (c == '-') || (c == '|') || (c == '/'));
+	return ((_parse_isDigit(c)) || (_parse_isLetter(c)) || (c == '_') || (c == '.') || (c == '-') || (c == '|') || (c == '/') || (c == ':'));
 }
 
 //////////////////////////////////////////////////////////////////////////////
