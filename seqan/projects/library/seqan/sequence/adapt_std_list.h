4dfecf344fa17db09fa11faf2c4780cbe175776e
holtgrew@FU-BERLIN.DE <holtgrew@FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-07-17 07:26:32 +0000

    Updates to STL list adaption.
    git-svn-id: http://svn.seqan.de/seqan/trunk@7324 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/sequence/adapt_std_list.h
+++ seqan/projects/library/seqan/sequence/adapt_std_list.h
@@ -15,20 +15,37 @@
   Lesser General Public License for more details.
 
  ============================================================================
-  $Id$
+  Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
  ==========================================================================*/
 
+// SEQAN_NO_GENERATED_FORWARDS: No forwards are generated for this file.
 
-//SEQAN_NO_GENERATED_FORWARDS: no forwards are generated for this file
-
-#ifndef SEQAN_HEADER_ADAPT_STD_LIST_H
-#define SEQAN_HEADER_ADAPT_STD_LSIT_H
+#ifndef SEQAN_SEQUENCE_ADAPT_STD_LIST_H_
+#define SEQAN_SEQUENCE_ADAPT_STD_LIST_H_
 
 #include <list>
 
+namespace seqan {
 
-namespace SEQAN_NAMESPACE_MAIN
+// ===========================================================================
+// Enums, Tags, Classes, Specializations
+// ===========================================================================
+
+// ===========================================================================
+// Metafunctions
+// ===========================================================================
+
+// TODO(holtgrew): dddoc this
+
+template <typename TValue>
+struct Size<std::list<TValue> >
 {
+	typedef size_t Type;
+};
+
+template <typename TValue>
+struct Size<std::list<TValue> const>
+        : Size<std::list<TValue> > {};
 
 template <typename TValue>
 struct Value<std::list<TValue> >
@@ -43,33 +60,97 @@ struct Iterator<std::list<TValue> >
 };
 
 template <typename TValue>
-struct Iterator<const std::list<TValue> >
+struct Iterator<std::list<TValue> const>
 {
 	typedef typename std::list<TValue>::const_iterator Type;
 };
 
-template<typename TValue>
-inline typename Iterator<std::list<TValue>, Standard >::Type
-begin(std::list<TValue> &list_){
-	return list_.begin();
-}
+template <typename TValue>
+struct Reference<std::list<TValue> >
+{
+	typedef typename std::list<TValue>::iterator _TIterator;
+    typedef typename std::iterator_traits<_TIterator>::reference Type;
+};
+
+template <typename TValue>
+struct Reference<std::list<TValue> const>
+{
+	typedef typename std::list<TValue>::const_iterator _TIterator;
+    typedef typename std::iterator_traits<_TIterator>::reference Type;
+};
+
+// ===========================================================================
+// Functions
+// ===========================================================================
 
 template<typename TValue>
 inline typename Iterator<std::list<TValue>, Standard >::Type
-end(std::list<TValue> &list_){
-	return list_.end();
+begin(std::list<TValue> & list)
+{
+	return list.begin();
 }
 
 template<typename TValue>
-inline typename Iterator<const std::list<TValue>, Standard >::Type
-begin(const std::list<TValue> &list_){
-	return list_.begin();
+inline typename Iterator<std::list<TValue>, Standard >::Type
+end(std::list<TValue> & list)
+{
+	return list.end();
 }
 
-template<typename TValue>
-inline typename Iterator<const std::list<TValue>, Standard >::Type
-end(const std::list<TValue> &list_){
-	return list_.end();
+template <typename TValue>
+inline typename Iterator<std::list<TValue> const, Standard >::Type
+begin(const std::list<TValue> & list)
+{
+	return list.begin();
+}
+
+template <typename TValue>
+inline typename Iterator<std::list<TValue> const, Standard >::Type
+end(std::list<TValue> const & list)
+{
+	return list.end();
+}
+
+template <typename TValue>
+inline typename Reference<std::list<TValue> >::Type
+front(std::list<TValue> & list)
+{
+    return list.front();
+}
+
+template <typename TValue>
+inline typename Reference<std::list<TValue> const>::Type
+front(std::list<TValue> const & list)
+{
+    return list.front();
+}
+
+template <typename TValue>
+inline typename Reference<std::list<TValue> >::Type
+back(std::list<TValue> & list)
+{
+    return list.back();
+}
+
+template <typename TValue>
+inline typename Reference<std::list<TValue> const>::Type
+back(std::list<TValue> const & list)
+{
+    return list.back();
+}
+
+template <typename TValue>
+inline typename Size<std::list<TValue> >::Type
+length(std::list<TValue> & list)
+{
+    return list.size();
+}
+
+template <typename TValue>
+inline typename Size<std::list<TValue> const>::Type
+length(std::list<TValue> const & list)
+{
+    return list.size();
 }
 
 template <typename T, typename T2>
@@ -80,6 +161,6 @@ appendValue(std::list<T> & list,
 	list.push_back(value);
 }
 
-}
+}  // namespace seqan
 
-#endif //#ifndef SEQAN_HEADER_
+#endif  // #ifndef SEQAN_SEQUENCE_ADAPT_STD_LIST_H_
