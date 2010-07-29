a1241a21cca6ffd967782505802d5cbf9ebff5f2
holtgrew@FU-BERLIN.DE <holtgrew@FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-07-29 22:29:00 +0000

    Seeds2 banded alignment code.
    git-svn-id: http://svn.seqan.de/seqan/trunk@7434 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/seeds2/align_dynprog_banded_linear.h
+++ seqan/projects/library/seqan/seeds2/align_dynprog_banded_linear.h
@@ -2,7 +2,7 @@
                 SeqAn - The Library for Sequence Analysis
                           http://www.seqan.de 
  ============================================================================
-  Copyright (C) 2007-2010
+  Copyright (C) 2010
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -64,7 +64,36 @@ _alignBanded_initGutter(Matrix<TScoreValue, 2> & matrix, Score<TScoreValue, Simp
 {
     SEQAN_CHECKPOINT;
 
-    SEQAN_ASSERT_FAIL("Write me!");
+    SEQAN_ASSERT_EQ_MSG(scoreGapOpen(scoringScheme), scoreGapExtend(scoringScheme),
+                        "Only linear gap costs allowed for Needleman-Wunsch.");
+
+    typedef Matrix<TScoreValue, 2> TMatrix;
+    typedef typename Iterator<TMatrix>::Type TIterator;
+    typedef typename Position<TMatrix>::Type TPosition;
+    
+    // Initialize the left gutter with infima.
+    TIterator it = begin(matrix);
+    goNext(it, 0);
+    for (TPosition i = 0, iend = length(matrix, 0); i < iend; ++i, goNext(it, 0))
+        *it = InfimumValue<TScoreValue>::VALUE / 2;
+    // Initialize the top gutter according to the AlignConfig.
+    it = begin(matrix);
+    if (BEGIN0_FREE) {
+        for (TPosition i = 0, iend = length(matrix, 1); i < iend; ++i, goNext(it, 1))
+            *it = 0;
+    } else {
+        TScoreValue gapScore = scoreGap(scoringScheme);
+        TScoreValue x = 0;
+        for (TPosition i = 0, iend = length(matrix, 1); i < iend; ++i, goNext(it, 1)) {
+            *it = x;
+            x += gapScore;
+        }
+    }
+    goPrevious(it, 1);
+    goNext(it, 0);
+    // Initialize the right gutter with infima.
+    for (TPosition i = 0, iend = length(matrix, 0) - 1; i < iend; ++i, goNext(it, 0))
+        *it = InfimumValue<TScoreValue>::VALUE / 2;
 }
 
 
@@ -74,7 +103,42 @@ _alignBanded_fillMatrix(Matrix<TScoreValue, 2> & matrix, TSequence const & seque
 {
     SEQAN_CHECKPOINT;
 
-    SEQAN_ASSERT_FAIL("Write me!");
+    SEQAN_ASSERT_EQ_MSG(scoreGapOpen(scoringScheme), scoreGapExtend(scoringScheme),
+                        "Only linear gap costs allowed for Needleman-Wunsch.");
+
+    typedef Matrix<TScoreValue, 2> TMatrix;
+    typedef typename Iterator<TMatrix>::Type TMatrixIterator;
+    typedef typename Iterator<TSequence, Standard>::Type TSequenceIterator;
+
+    // We use three iterators for filling the matrix.  itLeftmost
+    // always points to the leftmost cell of the current row, itLeft
+    // points to the field left of the current cell, itTop points to
+    // the cell to the upper left of the current cell.
+    TMatrixIterator itLeftmost = begin(matrix);
+    TMatrixIterator itLeft;
+    TMatrixIterator itTop;
+
+    // Compute score values so they are available locally and it maybe
+    // is easier to the compiler to save some indirect adressing.
+    TScoreValue matchScore = scoreMatch(scoringScheme);
+    TScoreValue mismatchScore = scoreMismatch(scoringScheme);
+    TScoreValue gapScore = scoreGap(scoringScheme);
+    
+    // TODO(holtgrew): Because we band in dimension 0, we have to fill the alignment matrix row-wise.  This, however is not cache-efficient.  Maybe tranpose matrix here?
+
+    for (TSequenceIterator it0 = begin(sequence0), it0End = end(sequence0); it0 != it0End; ++it0) {
+        itTop = itLeftmost;
+        goNext(itLeftmost, 0);
+        itLeft = itLeftmost;
+        for (TSequenceIterator it1 = begin(sequence0), it1End = end(sequence0); it1 != it1End; ++it1) {
+            TScoreValue scoreDiagonal = *itTop + ((*it0 == *it1) ? matchScore : mismatchScore);
+            goNext(itTop, 1);
+            TScoreValue scoreVertical = *itTop + gapScore;
+            TScoreValue scoreHorizontal = *itLeft + gapScore;
+            goNext(itLeft, 0);
+            *itLeft = _max(scoreDiagonal, _max(scoreVertical, scoreHorizontal));
+        }
+    }
 }
 
 }  // namespace seqan
