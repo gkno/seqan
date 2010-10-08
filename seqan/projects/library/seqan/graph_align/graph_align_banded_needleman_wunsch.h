432da87fbcdcaafa3be115f8f288fb10bfe83228
emde@FU-BERLIN.DE <emde@FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-10-08 13:52:10 +0000

    added banded global alignment functions needed for split alignment
    git-svn-id: http://svn.seqan.de/seqan/trunk@7860 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/graph_align/graph_align_banded_needleman_wunsch.h
+++ seqan/projects/library/seqan/graph_align/graph_align_banded_needleman_wunsch.h
@@ -257,6 +257,183 @@ _align_banded_nw(TTrace& trace,
 	}
 	return (overallMaxValue[0] > overallMaxValue[1]) ? overallMaxValue[0] : overallMaxValue[1];
 }
+template <typename TTrace, typename TStringSet, typename TScore, typename TValPair, typename TIndexPair, typename TDiagonal, typename TAlignConfig>
+inline typename Value<TScore>::Type
+_align_banded_nw(TTrace& trace,
+			TStringSet const& str,
+			TScore const & sc,
+			TValPair& overallMaxValue,
+			TIndexPair& overallMaxIndex,
+			TDiagonal diagL,
+			TDiagonal diagU,
+			TAlignConfig const,
+			String<Pair<typename Value<TScore>::Type,int> > & maxCols,
+			unsigned minColNum)
+{
+	SEQAN_CHECKPOINT
+	typedef typename Value<TTrace>::Type TTraceValue;
+	typedef typename Value<TScore>::Type TScoreValue;
+	typedef typename Value<TStringSet>::Type TString;
+	typedef typename Size<TTrace>::Type TSize;
+
+//      ::std::cout << "hierrrrrrrrrr banded ali\n";
+	// Initialization
+	TTraceValue Diagonal = 0; TTraceValue Horizontal = 1; TTraceValue Vertical = 2;
+	TString const& str1 = str[0];
+	TString const& str2 = str[1];
+	TSize len1 = length(str1) + 1;
+	TSize len2 = length(str2) + 1;
+//      ::std::cout << "len1="<<len1 << " len2=" << len2 << ::std::endl;
+	TSize diagonalWidth = (TSize) (diagU - diagL + 1);
+	TSize hi_diag = diagonalWidth;
+	TSize lo_diag = 0;
+	if (diagL > 0) lo_diag = 0;
+	else lo_diag = (diagU < 0) ? hi_diag : (TSize) (1 - diagL);
+	TSize lo_row = (diagU <= 0) ? -diagU : 0;
+	TSize hi_row = len2;
+	if (len1 - diagL < hi_row) hi_row = len1 - diagL;
+	TSize height = hi_row - lo_row;
+
+	clear(maxCols);
+
+	typedef String<TScoreValue> TRow;
+	TRow mat;
+	resize(mat, diagonalWidth);
+	resize(trace, height * diagonalWidth);
+//      ::std::cout <<height << "<-hieght\n";
+	overallMaxValue[0] = InfimumValue<TScoreValue>::VALUE;
+	overallMaxValue[1] = InfimumValue<TScoreValue>::VALUE;
+	overallMaxIndex[0] = diagonalWidth;     overallMaxIndex[1] = height;
+	overallMaxIndex[2] = diagonalWidth;     overallMaxIndex[3] = height;
+
+	//// Debug stuff
+	//String<TScoreValue> originalMat;
+	//resize(originalMat, len1 * len2);
+	//TSize count = 0;
+	//for(TSize i=0; i<len2; ++i) {
+	//      for(TSize j=0; j<len1; ++j) {
+	//              value(originalMat, i * len1 + j) = count;
+	//              std::cerr << count << ',';
+	//              ++count;
+	//      }
+	//      std::cerr << std::endl;
+	//}
+//std::cerr << std::endl;
+
+	// Classical DP with affine gap costs
+	typedef typename Iterator<TRow, Standard>::Type TRowIter;
+	typedef typename Iterator<TTrace, Standard>::Type TTraceIter;
+	TSize actualCol = 0;
+	TSize actualRow = 0;
+	TScoreValue verti_val = 0;
+	TScoreValue hori_val = 0;
+	for(TSize row = 0; row < height; ++row) {
+		TScoreValue maxRowVal = InfimumValue<TScoreValue>::VALUE;
+		unsigned maxRowCol = 0;
+		actualRow = row + lo_row;
+		if (lo_diag > 0) --lo_diag;
+		if (row + lo_row >= len1 - diagU) --hi_diag;
+		TTraceIter traceIt = begin(trace, Standard()) + row * diagonalWidth + lo_diag;
+		TRowIter matIt = begin(mat, Standard()) + lo_diag;
+		hori_val = InfimumValue<TScoreValue>::VALUE;
+		for(TSize col = lo_diag; col<hi_diag; ++col, ++matIt, ++traceIt) {
+			actualCol = col + diagL + actualRow;
+			//std::cerr << row << ',' << col << ':' << value(originalMat, actualRow * len1 + actualCol) << std::endl;
+
+			if ((actualRow != 0) && (actualCol != 0)) {
+				// Get the new maximum for mat
+				*matIt += score(const_cast<TScore&>(sc), ((int) actualCol - 1), ((int) actualRow - 1), str1, str2);
+				*traceIt = Diagonal;
+				if ((verti_val = (col < diagonalWidth - 1) ? *(matIt+1) + scoreGapExtendVertical(sc, ((int) actualCol - 1), ((int) actualRow - 1), str1, str2) : InfimumValue<TScoreValue>::VALUE) > *matIt) {
+					*matIt = verti_val;
+					*traceIt = Vertical;
+				}
+				if ((hori_val = (col > 0) ? hori_val + scoreGapExtendHorizontal(sc, ((int) actualCol - 1), ((int) actualRow - 1), str1, str2) : InfimumValue<TScoreValue>::VALUE) > *matIt) {
+					*matIt = hori_val;
+					*traceIt = Horizontal;
+				}
+				hori_val = *matIt;
+			} else {
+				// Usual initialization for first row and column
+				if (actualRow == 0) _initFirstRow(TAlignConfig(), *matIt, (TScoreValue) actualCol * scoreGapExtendHorizontal(sc, ((int) actualCol - 1), -1, str1, str2));
+				else {
+					_initFirstColumn(TAlignConfig(), *matIt, (TScoreValue) actualRow * scoreGapExtendVertical(sc, -1, ((int) actualRow - 1), str1, str2));
+					hori_val = *matIt;
+				}
+			}
+			if(*matIt > maxRowVal && actualCol >= minColNum)
+			{
+				maxRowVal = *matIt;
+				maxRowCol = actualCol;
+			}
+			// Store the maximum
+			if (actualCol == len1 - 1) _lastColumn(TAlignConfig(), overallMaxValue, overallMaxIndex, *matIt, row, col);
+			if (actualRow == len2 - 1) _lastRow(TAlignConfig(), overallMaxValue, overallMaxIndex, *matIt, row, col);
+			//std::cerr << row << ',' << col << ':' << *matIt << std::endl;
+		}
+		appendValue(maxCols,Pair<typename Value<TScore>::Type,int>(maxRowVal,maxRowCol));
+	//      std::cout << maxRowVal << ","<<maxRowCol << std::endl;
+	}
+	return (overallMaxValue[0] > overallMaxValue[1]) ? overallMaxValue[0] : overallMaxValue[1];
+}
+	
+template<typename TAlign, typename TStringSet, typename TScore, typename TAlignConfig, typename TDiagonal>
+inline typename Value<TScore>::Type
+_globalAlignment(TAlign& align,
+			TStringSet const& str,
+			TScore const& sc,
+			TAlignConfig const,
+			TDiagonal diag1,
+			TDiagonal diag2,
+			String<Pair<typename Value<TScore>::Type,int> > &maxCols,
+			unsigned minColNum,
+			BandedNeedlemanWunsch)
+{
+	SEQAN_CHECKPOINT
+	typedef typename Value<TScore>::Type TScoreValue;
+	typedef typename Size<TStringSet>::Type TSize;
+	
+	// Maximum value
+	TScoreValue overallMaxValue[2];
+	TSize overallMaxIndex[4];
+	
+	// Create the trace
+	String<TraceBack> trace;
+	TScoreValue maxScore = _align_banded_nw(trace, str, sc, overallMaxValue, overallMaxIndex, (int) diag1, (int) diag2, TAlignConfig(),maxCols, minColNum);
+	
+	// Follow the trace and create the graph
+	_align_banded_nw_trace(align, str, trace, overallMaxValue, overallMaxIndex, (int) diag1, (int) diag2);
+	
+	return maxScore;
+}
+	
+	//////////////////////////////////////////////////////////////////////////////
+	
+template<typename TStringSet, typename TScore, typename TAlignConfig, typename TDiagonal>
+inline typename Value<TScore>::Type
+_globalAlignment(TStringSet const& str,
+			TScore const& sc,
+			TAlignConfig const,
+			TDiagonal diag1,
+			TDiagonal diag2,
+			String<Pair<typename Value<TScore>::Type,int> > &maxCols,
+			unsigned minColNum,
+			BandedNeedlemanWunsch)
+{
+	SEQAN_CHECKPOINT
+	typedef typename Value<TScore>::Type TScoreValue;
+	typedef typename Size<TStringSet>::Type TSize;
+	
+	// Maximum value
+	TScoreValue overallMaxValue[2];
+	TSize overallMaxIndex[4];
+	
+	// Calculate the score
+	String<TraceBack> trace;
+	return _align_banded_nw(trace, str, sc, overallMaxValue, overallMaxIndex, (int) diag1, (int) diag2, TAlignConfig(),maxCols,minColNum);
+}
+
+
 
 ////////////////////////////////////////////////////////////////////////////
 
