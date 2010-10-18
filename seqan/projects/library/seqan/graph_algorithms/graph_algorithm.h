a864269384cc234fc32b09050223095f315080ea
holtgrew@FU-BERLIN.DE <holtgrew@FU-BERLIN.DE@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-10-18 09:34:04 +0000

    Fixing another problem in transitive closure computation.
    git-svn-id: http://svn.seqan.de/seqan/trunk@7984 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/graph_algorithms/graph_algorithm.h
+++ seqan/projects/library/seqan/graph_algorithms/graph_algorithm.h
@@ -1368,10 +1368,10 @@ transitive_closure(Graph<TSpec> const& g,
 	for (TSize k=0;k<len;++k) {
 		for(TSize i=0;i<len;++i) {
 			for(TSize j=0;j<len;++j) {
-				TMatrixVal t_ij = getValue(closure, i*len+j);
-				TMatrixVal t_ik = getValue(closure, i*len+k);
-				TMatrixVal t_kj = getValue(closure, k*len+j);
-				assignValue(local, i*len+j, t_ij | (t_ik & t_kj));
+				bool t_ij = getValue(closure, i*len+j) > 0;
+				bool t_ik = getValue(closure, i*len+k) > 0;
+				bool t_kj = getValue(closure, k*len+j) > 0;
+				assignValue(local, i*len+j, t_ij || (t_ik && t_kj));
 			}
 		}
 		closure = local;
