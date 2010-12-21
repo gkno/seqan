61cbe72dbde9688b0d8ddcec3c15cd7702336901
(no author) <(no author)@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-12-21 18:14:22 +0000

    Cosmetics
    git-svn-id: http://svn.seqan.de/seqan/trunk@8431 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan.h
+++ seqan/projects/library/seqan.h
@@ -1,29 +1,39 @@
-/*==========================================================================
-                SeqAn - The Library for Sequence Analysis
-                          http://www.seqan.de 
- ============================================================================
-  Copyright (C) 2009
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 3 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-  Lesser General Public License for more details.
-
- ============================================================================
-  $Id: cstream.h 954 2007-07-27 11:48:23Z doering@PCPOOL.MI.FU-BERLIN.DE $
- ============================================================================
-  This is the musketeer header ("one for all and all for one").
- ==========================================================================*/
+// ==========================================================================
+//                 SeqAn - The Library for Sequence Analysis
+// ==========================================================================
+// Copyright (c) 2006-2010, Knut Reinert, FU Berlin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Knut Reinert or the FU Berlin nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+//
+// ==========================================================================
+//  This is the musketeer header ("one for all and all for one").
+// ==========================================================================
 
 #ifndef SEQAN_H_
 #define SEQAN_H_
-
-// TODO(holtgrewe): Some headers are missing here: statistical_index.h.
 
 #include <seqan/align.h>
 #include <seqan/basic.h>
@@ -45,7 +55,7 @@
 #include <seqan/score.h>
 #include <seqan/seeds.h>
 #include <seqan/sequence.h>
-//#include <seqan/statistical_index.h> //vanished
+#include <seqan/statistics.h>
 #include <seqan/system.h>
 
 #endif  // SEQAN_H_
