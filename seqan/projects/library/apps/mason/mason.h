54c1c4376c108e7fd932f00b9f360394ec5f7f0d
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2010-12-21 11:13:21 +0000

    Header changes in Mason app.
    git-svn-id: http://svn.seqan.de/seqan/trunk@8400 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/apps/mason/mason.h
+++ seqan/projects/library/apps/mason/mason.h
@@ -1,27 +1,26 @@
-/*==========================================================================
-  SeqAn - The Library for Sequence Analysis
-  http://www.seqan.de 
-  ===========================================================================
-  Copyright (C) 2010
-  
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  ===========================================================================
-  Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
-  ===========================================================================
-  Globally shared code for the Mason read simulator.
-  ===========================================================================
-*/
+// ==========================================================================
+//                          Mason - A Read Simulator
+// ==========================================================================
+// Copyright (C) 2010 Manuel Holtgrewe, FU Berlin
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ==========================================================================
+// Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
+// ==========================================================================
+// Globally shared code for the Mason read simulator.
+// ==========================================================================
 
 #ifndef MASON_H_
 #define MASON_H_
--- seqan/projects/library/apps/mason/simulate_454_base_calling.h
+++ seqan/projects/library/apps/mason/simulate_454_base_calling.h
@@ -1,27 +1,26 @@
-/*==========================================================================
-  SeqAn - The Library for Sequence Analysis
-  http://www.seqan.de 
-  ===========================================================================
-  Copyright (C) 2010
-  
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  ===========================================================================
-  Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
-  ===========================================================================
-  Base-calling support code for 454 reads.
-  ===========================================================================
-*/
+// ==========================================================================
+//                          Mason - A Read Simulator
+// ==========================================================================
+// Copyright (C) 2010 Manuel Holtgrewe, FU Berlin
+//
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ==========================================================================
+// Author: Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>
+// ==========================================================================
+// Base-calling support code for 454 reads.
+// ==========================================================================
 
 #ifndef SIMULATE_454_BASE_CALLING_H_
 #define SIMULATE_454_BASE_CALLING_H_
