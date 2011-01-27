65e7567f6c2158617a7a8e021041c32fadeaab51
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2011-01-27 10:26:46 +0000

    Fixing copyring headers, still had `<COPYRIGHT HOLDER>` in them.
    git-svn-id: http://svn.seqan.de/seqan/trunk@8648 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/tests/pipe/test_pipe.h
+++ seqan/projects/tests/pipe/test_pipe.h
@@ -19,9 +19,9 @@
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// ARE DISCLAIMED. IN NO EVENT SHALL KNUT REINERT OR THE FU BERLIN BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
 // SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 // CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
 // LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
@@ -33,51 +33,100 @@
 // ==========================================================================
 
 #ifndef SEQAN_HEADER_TEST_PIPE_H
+
 #define SEQAN_HEADER_TEST_PIPE_H
 
+
+
 namespace SEQAN_NAMESPACE_MAIN
+
 {
 
+
+
 	template < typename TBuffer >
+
 	void permute(TBuffer buf) {
+
 		typename Size<TBuffer>::Type i, j, s = length(buf);
+
 //        srand( (unsigned)time( NULL ) );
+
 		for(i = 0; i < s; i++)
+
 			buf[i] = s-i-1;
+
 		for(i = 0; i < s; i++) {
+
             if (i > 0) {
+
 			    j = i - (rand() % i) - 1;
+
                 assert(/*0 <= j && */j < s);
+
             } else
+
                 j = 0;
+
 			unsigned tmp = buf[i];
+
 			buf[i] = buf[j];
+
 			buf[j] = tmp;
+
 		}
+
 	}
+
+
 
 	template < typename TBuffer >
+
 	void randomize(TBuffer buf) {
+
 		typename Size<TBuffer>::Type i, s = length(buf);
+
 		for(i = 0; i < s; i++)
+
             buf[i] = rand() % s;
+
 	}
 
-	template < typename TValue >
-	struct IdentityMap : public ::std::unary_function< TValue, TValue > {
-		inline TValue operator() (TValue const i) { return i; }
-	};
+
 
 	template < typename TValue >
-	struct SimpleCompare : public ::std::binary_function< TValue const, TValue const, int > {
-		inline int operator() (TValue const a, TValue const b) const {
-            if (a < b) return -1;
-            if (a > b) return 1;
-            return 0;
-        }
+
+	struct IdentityMap : public ::std::unary_function< TValue, TValue > {
+
+		inline TValue operator() (TValue const i) { return i; }
+
 	};
+
+
+
+	template < typename TValue >
+
+	struct SimpleCompare : public ::std::binary_function< TValue const, TValue const, int > {
+
+		inline int operator() (TValue const a, TValue const b) const {
+
+            if (a < b) return -1;
+
+            if (a > b) return 1;
+
+            return 0;
+
+        }
+
+	};
+
 	
+
 }
 
+
+
 #endif
+
+
 
