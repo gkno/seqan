bf7332d87283a0663f8a7ef432b4edbfa4b81f21
h4nn3s <h4nn3s@e6417c60-b987-48fd-844e-b20f0fcc1017>
2011-03-18 11:21:04 +0000

    more annotations[iorev]
    git-svn-id: http://svn.seqan.de/seqan/trunk@9021 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/seqan/misc/misc_parsing.h
+++ seqan/projects/library/seqan/misc/misc_parsing.h
@@ -37,6 +37,26 @@
 
 #include <cmath>
 
+/* IOREV
+ *
+ * _tested_
+ * _nodoc_
+ *
+ *
+ * used in different places
+ * except one function non are documented
+ *
+ * NOTE atof is used in some functions, but according to standard this is
+ * locale dependent (',' or '.' as decimal seperator). Has someone experienced
+ * issues with this? we should probably replace it with a local independent
+ * version to make sure
+ *
+ * Also in general atof should be replaced with strod, atoi with strtol etc
+ * 
+ */
+
+
+
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -56,7 +76,7 @@ template<typename TFile, typename TChar>
 inline void 
 _parseSkipLine(TFile& file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ treats \n as EOL 
 	if (c == '\n') {
 		c = _streamGet(file);
 		return;
@@ -74,7 +94,7 @@ template<typename TFile, typename TChar>
 inline void 
 _parseSkipWhitespace(TFile& file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _bug_ WRONG NAME, should be parseSkipNonprintable, but even that is wrong, because printable begins at c >= 32 not c > 32, also 127 (DEL) is non-printable; name does not indicate whether multiple chars are skipped or only one
 	if ((unsigned) c > 32) return;
 	while (!_streamEOF(file)) {
 		c = _streamGet(file);
@@ -86,7 +106,7 @@ template<typename TFile, typename TChar>
 inline void 
 _parseSkipSpace(TFile& file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ WRONG NAME: should be _parseSkipSpaceTab; name does not indicate whether multiple chars are skipped or only one
 	if (c != '\t' && c != ' ') return;
 	while (!_streamEOF(file)) {
 		c = _streamGet(file);
@@ -108,7 +128,7 @@ template<typename TFile, typename TChar>
 inline void 
 _parseSkipUntilChar(TFile& file, const TChar &x, TChar& c)
 {
-//IOREV _todo_
+//IOREV _doc_ _hasCRef_ _duplicate_ use _parseUntil() instead
 	if (c == x) return;
 	while (!_streamEOF(file)) {
 		c = _streamGet(file);
@@ -122,7 +142,7 @@ template<typename TChar>
 inline bool
 _parseIsDigit(TChar const c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ lacks Ref, maybe rename to _is* because there is no fileparsing
 	return (((unsigned) c >  47) && ((unsigned) c <  58));
 }
 
@@ -132,7 +152,7 @@ template<typename TChar>
 inline bool
 _parseIsLetter(TChar const c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ lacks Ref, maybe rename to _is* because there is no fileparsing
 	return ( (((unsigned) c > 64) && ((unsigned) c < 91)) || (((unsigned) c > 96) && ((unsigned) c < 123)) );
 }
 
@@ -143,7 +163,7 @@ template<typename TChar>
 inline bool
 _parseIsAlphanumericChar(TChar const c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _bug_ lacks Ref; maybe rename to _is* because there is no fileparsing; rename to sth that describes this regex, ITS NOT alphnumeric
 	return ((_parseIsDigit(c)) || (_parseIsLetter(c)) || (c == '_') || (c == '.') || (c == '-') || (c == '|') || (c == '/') || (c == ':'));
 }
 
@@ -153,7 +173,7 @@ template<typename TFile, typename TChar>
 inline int
 _parseReadNumber(TFile & file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ rename to something more generic, e.g. parseInt; strtol() / strtoll() is recommended over atoi in C99
 	// Read number
 	String<char> str(c);
 	while (!_streamEOF(file)) {
@@ -170,7 +190,7 @@ template<typename TFile, typename TChar>
 inline double
 _parseReadDouble(TFile & file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ rename to something more generic, e.g. parseDouble; _bug_ allows multiple dots in one number; strtod is recommended over atof in C99
 	// Read number
 	String<char> str(c);
 	while (!_streamEOF(file)) {
@@ -187,7 +207,7 @@ template<typename TFile, typename TChar>
 inline String<char>
 _parseReadIdentifier(TFile & file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ rename to something more specific (is this related to FASTA?)
 	// Read identifier
 	String<char> str(c);
 	while (!_streamEOF(file)) {
@@ -204,7 +224,7 @@ template<typename TFile, typename TChar>
 inline char
 _parseReadChar(TFile & file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ what exactly is the point of this?
     char result = c;
     if (!_streamEOF(file))
         c = _streamGet(file);
@@ -217,7 +237,7 @@ template<typename TFile, typename TString, typename TChar>
 inline void
 _parseReadIdentifier(TFile & file, TString& str, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _duplicate_ or just:  str = _parseReadIdentifier(file, c) ?
 	// Read identifier
 	append(str, c, Generous());
 	while (!_streamEOF(file)) {
@@ -233,7 +253,7 @@ template<typename TFile, typename TChar>
 inline String<char>
 _parseReadWord(TFile & file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ rename to _parseLetters()
 	// Read word
 	String<char> str(c);
 	while (!_streamEOF(file)) {
@@ -250,7 +270,7 @@ template<typename TFile, typename TChar, typename TSize>
 inline String<char>
 _parseReadWord(TFile & file, TChar& c, TSize max_len)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ rename to _parseNLetters()
 	// Read word
 	String<char> str(c);
 	--max_len;
@@ -272,7 +292,7 @@ template<typename TFile, typename TChar>
 inline String<char>
 _parseReadFilepath(TFile& file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ rename to something more generic _parseLineStripTrailingWhitespace; simplify code
 	String<char> str(c);
 	if (c == '\n' || (c == '\r' && _streamPeek(file) != '\n')) {
 		c = _streamGet(file);
@@ -301,7 +321,7 @@ template<typename TFile, typename TChar>
 inline String<char>
 _parseReadWordUntilWhitespace(TFile& file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ rename to _parseCharsUntilWhitespace(); simplify code and use proper whitespace definition
 	String<char> str(c);
 	if (c == '\n' || (c == '\r' && _streamPeek(file) != '\n')) {
 		c = _streamGet(file);
@@ -324,7 +344,7 @@ _parseReadSequenceData(TFile & file,
 						TChar & c,
 						TString& str)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _duplicate_ use _parseReadWord instead
 	SEQAN_CHECKPOINT
 
 	append(str, c);
@@ -343,7 +363,7 @@ template<typename TFile, typename TChar>
 inline void 
 _parseSkipBlanks(TFile& file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ what is a blank? change to whitespace or define blank as (' '|'\t') 
 	if ((c != ' ') && (c != '\t')) return;
 	while (!_streamEOF(file)) {
 		c = _streamGet(file);
@@ -355,7 +375,7 @@ template<typename TFile, typename TChar>
 inline void 
 _parseSkipLine2(TFile& file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _duplicate_ obviously duplicate, but different (non-expected) behaviour: will return '\n' not 'f' of string f o o \r \n b a r
 	if (c != '\n' && c != '\r')
 		while (!_streamEOF(file)) {
 			c = _streamGet(file);
@@ -372,7 +392,7 @@ template<typename TFile, typename TChar>
 inline double
 _parseReadEValue(TFile & file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ probably for blast-parsing, rename to resemble this; use strtod instead of atofs
 SEQAN_CHECKPOINT
 
 	// Read number
@@ -408,7 +428,7 @@ template<typename TFile, typename TChar>
 inline float
 _parseReadFloat(TFile & file, TChar& c)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _duplicate_ use _parseReadDouble() instead.   
 SEQAN_CHECKPOINT
 	// Read number
 	String<char> str(c);
@@ -430,7 +450,7 @@ template<typename TFile, typename TChar>
 inline bool
 _parseUntilBeginLine(TFile & file, TChar& c, TChar x)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ rename to something more understandable; does a "reset", if seek fails; unify behaviour and doc with similar functions
 SEQAN_CHECKPOINT
 	_parseSkipWhitespace(file,c);
 	typename Position<TFile>::Type pos = _streamTellG(file);
@@ -453,7 +473,7 @@ template<typename TFile, typename TChar, typename TSize>
 inline bool
 _parseUntilBeginLine(TFile & file, TChar& c, String<TChar> & word, TSize len)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ rename to something more understandable; does a "reset", if seek fails; unify behaviour and doc with similar functions
 SEQAN_CHECKPOINT
 	_parseSkipWhitespace(file,c);
 	typename Position<TFile>::Type pos = _streamTellG(file);
@@ -479,7 +499,7 @@ template<typename TFile, typename TChar, typename TSize>
 inline bool
 _parseUntilBeginLine(TFile & file, TChar& c, String<TChar> & word, TSize len, TSize num_lines)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ rename to something more understandable; does a "reset", if seek fails; unify behaviour and doc with similar functions
 SEQAN_CHECKPOINT
 	_parseSkipWhitespace(file,c);
 	typename Position<TFile>::Type pos = _streamTellG(file);
@@ -513,7 +533,7 @@ template<typename TFile, typename TChar, typename TSize>
 inline bool
 _parseUntilBeginLineOneOf(TFile & file, TChar& c, String<TChar> & x, TSize len)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ rename to something more understandable; does a "reset", if seek fails; unify behaviour and doc with similar functions
 SEQAN_CHECKPOINT
 	_parseSkipWhitespace(file,c);
 	typename Position<TFile>::Type pos = _streamTellG(file);
@@ -544,7 +564,7 @@ template<typename TFile, typename TChar>
 inline bool
 _parseUntil(TFile & file, TChar& c, TChar x)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ does "reset" if x not found
 SEQAN_CHECKPOINT
 	typename Position<TFile>::Type pos = _streamTellG(file);
 	TChar c_before = c;
@@ -566,7 +586,7 @@ template<typename TFile, typename TChar, typename TSize>
 inline bool
 _parseUntil(TFile & file, TChar& c, String<TChar> & word, TSize len)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ does "reset" if word not found
 SEQAN_CHECKPOINT
 	typename Position<TFile>::Type pos = _streamTellG(file);
 	TChar c_before = c;
@@ -590,7 +610,7 @@ template<typename TFile, typename TChar>
 inline bool
 _parseLineUntil(TFile & file, TChar& c, TChar x)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ does "reset" if x not found in line
 SEQAN_CHECKPOINT
 	typename Position<TFile>::Type pos = _streamTellG(file);
 	TChar c_before = c;
@@ -617,7 +637,7 @@ template<typename TFile, typename TChar, typename TSize>
 inline bool
 _parseLineUntil(TFile & file, TChar& c, String<TChar> & word, TSize len)
 {
-//IOREV _todo_
+//IOREV _nodoc_ _hasCRef_ _requiresSeek_ does "reset" if word not found in line
 SEQAN_CHECKPOINT
 	typename Position<TFile>::Type pos = _streamTellG(file);
 	TChar c_before = c;
