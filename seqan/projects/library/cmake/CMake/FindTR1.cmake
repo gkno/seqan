9a9d32265b7a4672c10d761d65a1b1cc7815b2be
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2011-04-03 22:36:42 +0000

    CMake find support for tr1::unordered_map<,>
    git-svn-id: http://svn.seqan.de/seqan/trunk@9148 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/projects/library/cmake/CMake/FindTR1.cmake
+++ seqan/projects/library/cmake/CMake/FindTR1.cmake
@@ -12,7 +12,7 @@ cmake_minimum_required(VERSION 2.6.3)
 cmake_policy(POP)
 
 # ---------------------------------------------------------------------------
-# std::tr1::shard_ptr<T>
+# std::tr1::shared_ptr<T>
 # ---------------------------------------------------------------------------
 
 check_cxx_source_compiles(
@@ -45,4 +45,39 @@ endif (TR1_SHARED_PTR_USE_MEMORY)
 mark_as_advanced (TR1_SHARED_PTR_FOUND)
 mark_as_advanced (TR1_SHARED_PTR_USE_TR1_MEMORY)
 mark_as_advanced (TR1_SHARED_PTR_USE_MEMORY)
+
+# ---------------------------------------------------------------------------
+# std::tr1::unordered_map<K, V>
+# ---------------------------------------------------------------------------
+
+check_cxx_source_compiles(
+    "
+        #include <tr1/unordered_map>
+        int main() {
+            std::tr1::unordered_map<int, int> m;
+            return 0;
+        }
+    "
+    TR1_UNORDERED_MAP_USE_TR1_UNORDERED_MAP)
+check_cxx_source_compiles(
+    "
+        #include <unordered_map>
+        int main() {
+            std::tr1::unordered_map<int, int> m;
+            return 0;
+        }
+    "
+    TR1_UNORDERED_MAP_USE_UNORDERED_MAP)
+
+set (TR1_UNORDERED_MAP -NOTFOUND)
+if (TR1_UNORDERED_MAP_USE_TR1_UNORDERED_MAP)
+  set (TR1_UNORDERED_MAP_FOUND TRUE)
+endif (TR1_UNORDERED_MAP_USE_TR1_UNORDERED_MAP)
+if (TR1_UNORDERED_MAP_USE_UNORDERED_MAP)
+  set (TR1_UNORDERED_MAP_FOUND TRUE)
+endif (TR1_UNORDERED_MAP_USE_UNORDERED_MAP)
+
+mark_as_advanced (TR1_UNORDERED_MAP_FOUND)
+mark_as_advanced (TR1_UNORDERED_MAP_USE_TR1_UNORDERED_MAP)
+mark_as_advanced (TR1_UNORDERED_MAP_USE_UNORDERED_MAP)
 
