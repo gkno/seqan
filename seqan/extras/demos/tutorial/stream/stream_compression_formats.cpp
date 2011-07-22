f934574cd32c834f6763d4a242bc901c3a9267d1
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2011-07-22 22:19:22 +0000

    Example for memory mapped strings.
    git-svn-id: http://svn.seqan.de/seqan/trunk@10110 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/extras/demos/tutorial/stream/stream_compression_formats.cpp
+++ seqan/extras/demos/tutorial/stream/stream_compression_formats.cpp
@@ -20,7 +20,7 @@ int openGz(char const * filename)
     gzFile f = gzopen(filename, "rb");
     if (gzdirect(f))
     {
-        std::cerr << std::endl << "ERROR: GZip file has the wrong format!" << std::endl;
+        std::cerr << "ERROR: GZip file has the wrong format!" << std::endl;
         gzclose(f);
         return 1;
     }
@@ -32,8 +32,11 @@ int openGz(char const * filename)
         char c = '\0';
         int res = streamReadChar(c, f2);
         if (res != 0)
+        {
+            std::cerr << "ERROR: Reading byte from GZip file." << std::endl;
             return 1;
-        std::cerr << c;
+        }
+        std::cout << c;
     }
     
     gzclose(f);
@@ -46,39 +49,42 @@ int openGz(char const * filename)
 // FRAGMENT(open-bz2)
 int openBz2(char const * filename)
 {
-#if SEQAN_HAS_ZLIB
+#if SEQAN_HAS_BZIP2
     FILE * f = fopen(filename, "rb");
     if (!f)
     {
-        std::cerr << std::endl << "ERROR: Could not open input file!" << std::endl;
+        std::cerr << "ERROR: Could not open input file!" << std::endl;
         return 1;
     }
     int err = BZ_OK;
     BZFILE * f2 = BZ2_bzReadOpen(&err, f, 0, 0, NULL, 0);
     if (err != BZ_OK)
     {
-        std::cerr << std::endl << "ERROR: Could not open bzip2 file!" << std::endl;
+        std::cerr << "ERROR: Could not open bzip2 file!" << std::endl;
         BZ2_bzReadClose(&err, f2);
         if (err != BZ_OK)
-            std::cerr << std::endl << "ERROR: Could not close bzip2 file!" << std::endl;
+            std::cerr << "ERROR: Could not close bzip2 file!" << std::endl;
         fclose(f);
     }
     Stream<BZ2File> f3(f2);
-    
+
     // Totally inefficient char-wise writing of characters from .bz2 file to stderr.
     while (!streamEof(f3))
     {
         char c = '\0';
         int res = streamReadChar(c, f3);
         if (res != 0)
+        {
+            std::cout << "ERROR: Reading byte from BZ2 file." << std::endl;
             return 1;
+        }
         std::cerr << c;
     }
     
     BZ2_bzReadClose(&err, f2);
     if (err != BZ_OK)
     {
-        std::cerr << std::endl << "ERROR: Could not close bzip2 file!" << std::endl;
+        std::cerr << "ERROR: Could not close bzip2 file!" << std::endl;
         fclose(f);
     }
     fclose(f);
