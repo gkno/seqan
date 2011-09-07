ecd944f125ee7e351bcc311e5cc2f865f490160e
bjornson <bjornson@e6417c60-b987-48fd-844e-b20f0fcc1017>
2011-09-07 14:52:23 +0000

    Automatically open browser after cmake if INST exists
    git-svn-id: http://svn.seqan.de/seqan/trunk@10337 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/misc/seqan_instrumentation/bin/seqan_instrumentation.py
+++ seqan/misc/seqan_instrumentation/bin/seqan_instrumentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import webbrowser
 from bkahlert import DiffCollector
 
 def main():
@@ -8,12 +9,22 @@ def main():
 	# sys.argv[2]: cmake binary dir (e.g. [SEQAN]/build/Debug)
 	# sys.argv[3]: seqan src directory
 	diffCollector = DiffCollector(sys.argv[2], sys.argv[3])
+	id = diffCollector.getID().get()
         
 	# cmake run
 	if(sys.argv[1] == "cmake"):
 		diffCollector.prepare()
-		print("[NOTE] Your ID is " + diffCollector.getID().get())
+		print("[NOTE] Your ID is " + id)
 		print("[NOTE] Your documented data are saved in file\n       " + diffCollector.getStatsFile() + ".")
+		print("--------------------------------------------")
+		print("| If not happened automatically please     |")
+		print("| open the following page in your          |")
+		print("| favourite browser:                       |")
+		print("|                                          |")
+		print("| http://www.seqan.de/?id=" + id + " |")
+		print("--------------------------------------------")
+		webbrowser.open(url="http://www.seqan.de/?id=" + id, new=2)
+		
 		diffCollector.build()
 		return 0
 
