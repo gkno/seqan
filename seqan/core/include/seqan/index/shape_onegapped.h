31bd5734b6acf19728c314b96d329a09bdadc50a
weese <weese@e6417c60-b987-48fd-844e-b20f0fcc1017>
2011-09-21 17:42:36 +0000

    minor fixes
    git-svn-id: http://svn.seqan.de/seqan/trunk@10415 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/core/include/seqan/index/shape_onegapped.h
+++ seqan/core/include/seqan/index/shape_onegapped.h
@@ -89,14 +89,21 @@ namespace SEQAN_NAMESPACE_MAIN
 //____________________________________________________________________________
 
 		Shape():
-			hValue(0) {}
+			blockLen1(0),
+			gapLen(0),
+			blockLen2(0),
+			hValue(0),
+			leftChar(0),
+			factor1(0),
+			factor2(0) {}
 
 		// c'tor for ungapped shapes
 		Shape(unsigned _blockLen1, unsigned _gapLen, unsigned _blockLen2):
 			blockLen1(_blockLen1),
 			gapLen(_gapLen),
 			blockLen2(_blockLen2),
-			hValue(0)
+			hValue(0),
+			leftChar(0)
 		{
 		SEQAN_CHECKPOINT
 			typedef typename Value< Shape<TValue, OneGappedShape> >::Type	THValue;
