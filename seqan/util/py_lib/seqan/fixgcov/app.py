df32e7f66a1839c770c9e51612b4b6d6d0a7e695
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2011-09-27 06:33:05 +0000

    Updates to fixgcov tool.
    git-svn-id: http://svn.seqan.de/seqan/trunk@10453 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/util/py_lib/seqan/fixgcov/app.py
+++ seqan/util/py_lib/seqan/fixgcov/app.py
@@ -73,7 +73,7 @@ class VisitAllowedRule(object):
     def visitAllowed(self, node):
         """Return True if visiting is allowed."""
         # TODO(holtgrew): For this application, stopping at compound statements would be enough.
-        print node._kind_id
+        #print node._kind_id
         if node.kind == ci.CursorKind.TRANSLATION_UNIT:
             return True
         if not _hasFileLocation(node):
@@ -112,7 +112,8 @@ class AstTraverser(object):
         args = ['-I%s' % s for s in self.options.include_dirs]
         # print args
         tu = index.parse(filename, args=args)
-        print 'Translation unit: %s.' % tu.spelling
+        if self.options.verbosity >= 1:
+            print 'Translation unit: %s.' % tu.spelling
         return self._recurse(tu.cursor)
     
     @classmethod
@@ -131,18 +132,35 @@ def main():
     parser.add_option('-s', '--src-file', dest='source_files', default=[],
                       type='string', help='Specify compilation units.',
                       action='append')
+    parser.add_option('--src-file-files', dest='source_files_files', default=[],
+                      type='string', help='Specify file with paths to compilation units.',
+                      action='append')
     parser.add_option('-l', '--location-file', dest='location_file',
                       default='locations.dat', type='string',
                       help='Path to file with compound statement locations.')
     parser.add_option('-g', '--gcov-file', dest='gcov_files', default=[],
                       type='string', help='Specify gcov files to process.',
                       action='append')
+    parser.add_option('--gcov-files', dest='gcov_files_files', default=[],
+                      type='string', help='Specify gcov files to process.',
+                      action='append')
+    parser.add_option('-q', '--quiet', dest='verbosity', default=1,
+                      action='store_const', const=0, help='Fewer message.')
+    parser.add_option('-v', '--verbose', dest='verbosity', default=1,
+                      action='store_const', const=2, help='More messages.')
     options, args = parser.parse_args()
     if len(args) != 0:
         parser.error('Incorrect number of arguments!')
         return 1
 
     options.include_dirs += [os.path.abspath(os.path.dirname(s)) for s in options.source_files]
+
+    for path in options.source_files_files:
+      with open(path, 'rb') as f:
+        options.source_files += [x.strip() for x in f.readlines()]
+    for path in options.gcov_files_files:
+      with open(path, 'rb') as f:
+        options.gcov_files += [x.strip() for x in f.readlines()]
 
     if not options.source_files and not options.gcov_files:
         parser.error('Neither source nor gcov file given!')
@@ -152,14 +170,18 @@ def main():
         # If any source file is given, all given source files are parsed and all
         # lines with compound statements in all included files are written to
         # the location file.
-        print >>sys.stderr, 'Building Locations'
-        print >>sys.stderr, '=================='
+        if options.verbosity >= 1:
+            print >>sys.stderr, 'Building Locations'
+        if options.verbosity >= 2:
+            print >>sys.stderr, '=================='
 
         # Fire off AST traversal.
-        print >>sys.stderr, 'AST Traversal'
+        if options.verbosity >= 1:
+            print >>sys.stderr, 'AST Traversal'
         node_visitor = CollectCompoundStatementNodeVisitor(options)
         for src in options.source_files:
-            print >>sys.stderr, '  Compilation Unit', src
+            if options.verbosity >= 2:
+                print >>sys.stderr, '  Compilation Unit', src
             AstTraverser.visitFile(src, node_visitor, options)
 
         # Convert locations into points.
@@ -170,23 +192,28 @@ def main():
                 locations.setdefault(filename, set()).add(i)
 
         # Write out the source locations.
-        print >>sys.stderr, 'Writing out locations to', options.location_file
+        if options.verbosity >= 1:
+            print >>sys.stderr, 'Writing out locations to', options.location_file
         with open(options.location_file, 'wb') as f:
             pickle.dump(locations, f)
 
     if options.gcov_files:
         # If no source files and gcov files are given then
-        print >>sys.stderr, 'Updating gcov Results'
-        print >>sys.stderr, '====================='
+        if options.verbosity >= 1:
+            print >>sys.stderr, 'Updating gcov Results'
+        if options.verbosity >= 2:
+            print >>sys.stderr, '====================='
 
         if not options.source_files:
-            print >>sys.stderr, 'Loading locations from', options.location_file
+            if options.verbosity >= 1:
+                print >>sys.stderr, 'Loading locations from', options.location_file
             with open(options.location_file, 'rb') as f:
                 locations = pickle.load(f)
 
         for filename in options.gcov_files:
             filename = os.path.abspath(filename)
-            print >>sys.stderr, 'Processing', filename
+            if options.verbosity >= 2:
+                print >>sys.stderr, 'Processing', filename
             with open(filename, 'rb') as f:
                 lines = f.readlines()
             pos0 = lines[0].find(':')
@@ -202,7 +229,8 @@ def main():
                 if txt.startswith('Source:'):
                     source = os.path.abspath(txt[len('Source:'):].strip())
                     if not locations.has_key(source):
-                        print >>sys.stderr, '  Skipping.'
+                        if options.verbosity >= 2:
+                            print >>sys.stderr, '  Skipping.'
                         skip = True
                         break
                 if not source or lineno == 0:
@@ -214,7 +242,9 @@ def main():
             # Write back file if not skipped.
             if skip:
                 continue
-            print ''.join(result)
+            with open (filename, 'wb') as f:
+              f.write(''.join(result))
+            #print ''.join(result)
 
 if __name__ == '__main__':
     sys.exit(main())
