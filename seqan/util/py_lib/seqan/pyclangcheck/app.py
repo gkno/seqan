b162b666d00f3ae09782440d6cad455d76f52af2
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2011-09-28 10:06:15 +0000

    Experimental fixgcov app, needs patch to clang trunk currently.
    git-svn-id: http://svn.seqan.de/seqan/trunk@10474 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/util/py_lib/seqan/pyclangcheck/app.py
+++ seqan/util/py_lib/seqan/pyclangcheck/app.py
@@ -10,96 +10,147 @@ License:   3-clause BSD (see LICENSE)
 __author__ = 'Manuel Holtgrewe <manuel.holtgrewe@fu-berlin.de>'
 
 import optparse
+import os
+import os.path
 import sys
 
 import rules
 import clang.cindex as ci
 
 
-class RulesDispatcher(object):
-    def __init__(self, rules, options):
-        self.rules = rules
+def _hasFileLocation(node):
+    """Return True if node has a file lcoation."""
+    if not hasattr(node, 'location'):
+        return False
+    if not hasattr(node.location, 'file'):
+        return False
+    if not node.location.file:
+        return False
+    if not hasattr(node.location.file, 'name'):
+        return False
+    if not node.location.file.name:
+        return False
+    return True
+
+
+class CollectViolationsVisitor(object):
+    """Visitor for AST nodes that collects rule violations."""
+    
+    def __init__(self, options, rules):
         self.options = options
-        self.violations = {}
+        self.rules = []
+        self.stack = []
+        self.violations = []
     
     def enterNode(self, node):
-        pass
-
-    def exitNode(self, node):
-        pass
-    
-    def check(self, node):
+        """Called when a node is entered ("pre-order" traversal)."""
+        self.stack.append(node)
+        if self.options.verbosity >= 2:
+            print ' ' * len(self.stack), 'Entering', node.kind, node.spelling
         violations = []
-        for x in self.rules:
-            if x.allowVisit(node):
-                violations += x.check(node)
+        for rule in self.rules:
+            violations += rule.check(node)
         for v in violations:
-            print v
+            if self.options.verbosity >= 2:
+                print v
             self.violations[v.key()] = v
 
+    def exitNode(self, node):
+        """Called when a node is left ("post-order" traversa)."""
+        self.stack.pop()
 
-class AstVisitor(object):
-    def __init__(self, dispatcher, recurse_rules, check_rules, options):
-        self.dispatcher = dispatcher
-        self.recurse_rules = recurse_rules
-        self.check_rules = check_rules
+
+class VisitAllowedRule(object):
+    """Decides whether a AST node and its children is visited."""
+    
+    def __init__(self, options):
         self.options = options
+        self.include_dirs = [os.path.abspath(x) for x in options.include_dirs]
+        self.cache = {}
 
-    def _recurseAllowed(self, node):
-        for r in self.recurse_rules:
-            if not r.allowRecurse(node):
-                # print 'NOT recursing into', ci.Cursor_displayname(node), node.kind
-                return False  # visiting not allowed
-        return True
+    def visitAllowed(self, node):
+        """Return True if visiting is allowed."""
+        # would be enough.  Visit if translation unit.
+        if node.kind == ci.CursorKind.TRANSLATION_UNIT:
+            return True
+        # Don't visit if it has no location (built-in).
+        if not _hasFileLocation(node):
+            return False
+        # Try to hit cache.
+        if self.cache.has_key(node.location.file.name):
+            return self.cache[node.location.file.name]
+        # Check whether node's location is below the include directories.  It is
+        # only visited if this is the case.
+        filename = os.path.abspath(node.location.file.name)
+        result = False
+        for x in self.include_dirs:
+            if filename.startswith(x):
+                # print filename, x
+                result = True
+                break
+        self.cache[node.location.file.name] = result  # Save in cache.
+        return result
+
+
+class AstTraverser(object):
+    """Traverses AST tree and applies given visitor object."""
     
-    def _visitAllowed(self, node):
-        # if rules._hasFileLocation(node):
-        #     print node.location.file.name
-        for r in self.recurse_rules:
-            if not r.allowVisit(node):
-                # print 'NOT visiting', ci.Cursor_displayname(node), node.kind
-                return False  # visiting not allowed
-        return True
-    
+    def __init__(self, node_visitor, options):
+        self.node_visitor = node_visitor
+        self.options = options
+        self.visit_allowed_rule = VisitAllowedRule(options)
+
     def _recurse(self, node):
-        if not self._visitAllowed(node):
+        """Recursion helper."""
+        if not self.visit_allowed_rule.visitAllowed(node):
             return False  # We did not visit this node.
-        self.dispatcher.enterNode(node)
-        self.dispatcher.check(node)
-        if self._recurseAllowed(node):
-            for c in node.get_children():
-                self._recurse(c)
-        self.dispatcher.exitNode(node)
+        self.node_visitor.enterNode(node)
+        for c in node.get_children():
+            self._recurse(c)
+        self.node_visitor.exitNode(node)
         return True
-    
+
     def run(self, filename):
+        """Main entry point."""
         index = ci.Index.create()
         args = ['-I%s' % s for s in self.options.include_dirs]
         # print args
         tu = index.parse(filename, args=args)
-        print 'Translation unit: %s.' % tu.spelling
+        if self.options.verbosity >= 1:
+            print 'Translation unit: %s.' % tu.spelling
         return self._recurse(tu.cursor)
     
     @classmethod
-    def visitFile(klass, filename, options, recurse_rules, check_rules):
-        dispatcher = RulesDispatcher(check_rules, options)
-        visitor = AstVisitor(dispatcher, recurse_rules, check_rules, options)
-        res = visitor.run(filename)
-        for k in sorted(dispatcher.violations.keys()):
-            print dispatcher.violations[k]
-        return res == True
+    def visitFile(klass, filename, node_visitor, options):
+        """Don't instantiate AstTraverser yourself, use this function."""
+        if options.verbosity >= 1:
+            print >>sys.stderr, 'Checking', filename
+        traverser = AstTraverser(node_visitor, options)
+        res = traverser.run(filename)
+        return res != True
 
 
 def main():
+    # ========================================================================
     # Parse command line arguments.
+    # ========================================================================
     parser = optparse.OptionParser("USAGE: %prog [options] file.cpp")
+    parser.add_option('-s', '--source-file', dest='source_files', default=[],
+                      type='string', help='Specify source (.cpp) files.',
+                      action='append')
     parser.add_option('-I', '--include-dir', dest='include_dirs', default=[],
                       type='string', help='Specify include directories',
                       action='append')
+    parser.add_option('-q', '--quiet', dest='verbosity', default=1,
+                      action='store_const', const=0, help='Fewer message.')
+    parser.add_option('-v', '--verbose', dest='verbosity', default=1,
+                      action='store_const', const=2, help='More messages.')
     options, args = parser.parse_args()
-    if len(args) != 1:
+
+    if len(args) != 0:
         parser.error('Incorrect number of arguments!')
         return 1
+
     # Recursion Rule: Only check symbols within the include directories.
     recurse_rules = []
     recurse_rules.append(rules.InIncludeDirsRule(options.include_dirs + ['.']))
@@ -130,7 +181,14 @@ def main():
     ]
 
     # Fire off AST traversal.
-    return AstVisitor.visitFile(args[0], options, recurse_rules, check_rules)
+    node_visitor = CollectViolationsVisitor(options, check_rules)
+    for filename in options.source_files:
+        res = AstTraverser.visitFile(filename, node_visitor, options)
+        if res:
+            break
+    print node_visitor.violations
+    return len(node_visitor.violations) > 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
