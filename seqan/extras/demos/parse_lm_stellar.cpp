26b53d56173a4f8a23a5f2ec0d5f169b3abb6ad5
ktrappe <ktrappe@e6417c60-b987-48fd-844e-b20f0fcc1017>
2011-10-28 17:03:24 +0000

    Adjusted order of subject and query cout.
    git-svn-id: http://svn.seqan.de/seqan/trunk@10665 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/extras/demos/parse_lm_stellar.cpp
+++ seqan/extras/demos/parse_lm_stellar.cpp
@@ -77,16 +77,21 @@ int main(int argc, char const ** argv)
 
     // Dump records to stdout again.
     std::cout << "# Reverse strandness is indicated by end < begin\n"
-              << "#db\tdb_beg\tdb_end\tquery\tq_beg\tq_end\tcigar\n";
+              << "#db\tdb_beg\tdb_end\t+/-\tquery\tq_beg\tq_end\tcigar\n";
     for (unsigned i = 0; i < length(lmStore.matchStore); ++i)
     {
         
-        std::cout << lmStore.sequenceNameStore[lmStore.matchStore[i].queryId] << "\t"
-                  << lmStore.matchStore[i].queryBeginPos << "\t"
-                  << lmStore.matchStore[i].queryEndPos << "\t"
-                  << lmStore.sequenceNameStore[lmStore.matchStore[i].subjectId] << "\t"
+        std::cout << lmStore.sequenceNameStore[lmStore.matchStore[i].subjectId] << "\t"
                   << lmStore.matchStore[i].subjectBeginPos << "\t"
                   << lmStore.matchStore[i].subjectEndPos << "\t";
+		if (lmStore.matchStore[i].subjectBeginPos < lmStore.matchStore[i].subjectEndPos)
+			std::cout << "+\t";
+		else
+			std::cout << "-\t";
+		std::cout << lmStore.sequenceNameStore[lmStore.matchStore[i].queryId] << "\t"
+                  << lmStore.matchStore[i].queryBeginPos << "\t"
+                  << lmStore.matchStore[i].queryEndPos << "\t";
+
         if (length(lmStore.cigarStore) > lmStore.matchStore[i].id)
         {
             String<CigarElement<> > const & cigar = lmStore.cigarStore[lmStore.matchStore[i].id];
