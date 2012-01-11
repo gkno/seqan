28f4e5231e5f815678939a82e00e0295445f9eff
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-01-11 17:12:13 +0000

    Adjusting CTest drop location.
    git-svn-id: http://svn.seqan.de/seqan/trunk@11046 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/util/cmake/CTestConfig.cmake
+++ seqan/util/cmake/CTestConfig.cmake
@@ -8,6 +8,6 @@ set(CTEST_PROJECT_NAME "SeqAn")
 set(CTEST_NIGHTLY_START_TIME "23:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.seqan.de")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=SeqAn")
+set(CTEST_DROP_SITE "cdash.seqan.de")
+set(CTEST_DROP_LOCATION "/submit.php?project=SeqAn")
 set(CTEST_DROP_SITE_CDASH TRUE)
