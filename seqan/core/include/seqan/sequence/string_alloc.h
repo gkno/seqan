9b44e4e7de6878ce1a77a52a7a3fc4a68ae3216c
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-03-21 12:47:34 +0000

    Quick fix for copy constructio of Alloc Strings with empty source.
    git-svn-id: http://svn.seqan.de/seqan/trunk@11503 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/core/include/seqan/sequence/string_alloc.h
+++ seqan/core/include/seqan/sequence/string_alloc.h
@@ -85,7 +85,8 @@ public:
           data_capacity(0)
     {
         SEQAN_CHECKPOINT;
-        assign(*this, source);
+        if (length(source) > 0u)
+            assign(*this, source);
         SEQAN_ASSERT_LEQ_MSG(data_begin, data_end, "String end is before begin!");
     }
     
@@ -96,7 +97,8 @@ public:
           data_capacity(0)
     {
         SEQAN_CHECKPOINT;
-        assign(*this, source);
+        if (length(source) > 0u)
+            assign(*this, source);
         SEQAN_ASSERT_LEQ_MSG(data_begin, data_end, "String end is before begin!");
     }
     
@@ -106,7 +108,8 @@ public:
           data_capacity(0)
     {
         SEQAN_CHECKPOINT;
-        assign(*this, source);
+        if (length(source) > 0u)
+            assign(*this, source);
         SEQAN_ASSERT_LEQ_MSG(data_begin, data_end, "String end is before begin!");
     }
     
@@ -127,7 +130,8 @@ public:
               data_capacity(0)
     {
         SEQAN_CHECKPOINT;
-        assign(*this, source, limit);
+        if (length(source) > 0u)
+            assign(*this, source, limit);
         SEQAN_ASSERT_LEQ_MSG(data_begin, data_end, "String end is before begin!");
     }
     
@@ -138,7 +142,8 @@ public:
               data_capacity(0)
     {
         SEQAN_CHECKPOINT;
-        assign(*this, source, limit);
+        if (length(source) > 0u)
+            assign(*this, source, limit);
         SEQAN_ASSERT_LEQ_MSG(data_begin, data_end, "String end is before begin!");
     }
 
