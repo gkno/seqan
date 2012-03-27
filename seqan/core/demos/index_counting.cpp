3e9f116d811794e493aafc8d06ca3d4ea88356b3
reinert <reinert@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-03-27 12:42:47 +0000

    added isLeaf
    git-svn-id: http://svn.seqan.de/seqan/trunk@11537 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/core/demos/index_counting.cpp
+++ seqan/core/demos/index_counting.cpp
@@ -20,7 +20,7 @@ int main ()
 	while (!atEnd(tdIterator)) 
 	{
 ///We print out the representatives of all nodes that have more than 3 children and the number of occurrences. Also, we print a message if a node is a leaf.
-		count = countChildren(tdIterator);
+        count = countChildren(tdIterator);
         if( count >= 3 )
             ::std::cout << "Representative " << representative(tdIterator) << " has " <<  count << " children  and " << countOccurrences(tdIterator) << " occurrences " << ::std::endl;
         if ( isLeaf(tdIterator) )
