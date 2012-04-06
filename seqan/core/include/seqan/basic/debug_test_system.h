86b8e65afbf4305811938666e288878344c8b79d
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-04-06 23:48:24 +0000

    Fixing `SEQAN_CHECK` documentation.
    git-svn-id: http://svn.seqan.de/seqan/trunk@11629 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/core/include/seqan/basic/debug_test_system.h
+++ seqan/core/include/seqan/basic/debug_test_system.h
@@ -106,7 +106,7 @@ bool foo(MyEnum x) {
 .Macro.SEQAN_CHECK
 ..cat:Miscellaneous
 ..summary:Force abortion of program if a condition is not met, regardless of debugging settings.
-..signature:SEQAN_FAIL(condition, msg[, args])
+..signature:SEQAN_CHECK(condition, msg[, args])
 ..param.msg:A format string.
 ..param.args:An optional list of arguments.
 ..remarks:Use this if something really unexpected happens inside your functions and there is no way to report this through the API. A good example would be logic errors, e.g. invalid values.
