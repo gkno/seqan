066bd30ad740e05ac886e2c98d955d2b8f1ed11c
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-06-01 12:42:33 +0000

    [FIX] Adjusting Rabema tests to new program.
    git-svn-id: http://svn.seqan.de/seqan/trunk@12016 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/core/apps/rabema/tests/generate_outputs.sh
+++ seqan/core/apps/rabema/tests/generate_outputs.sh
@@ -5,7 +5,9 @@
 # We use the current trunk version of 2011-10-13 (r10463) for building the
 # reference.
 RAZERS=../../../../build/Release/core/apps/razers2/razers2
-RABEMA=../../../../build/Release/core/apps/rabema/rabema
+RABEMA_PREPARE=../../../../build/Release/core/apps/rabema/rabema_prepare_sam
+RABEMA_BUILD=../../../../build/Release/core/apps/rabema/rabema_build_gold_standard
+RABEMA_EVALUATE=../../../../build/Release/core/apps/rabema/rabema_evaluate
 
 # ============================================================
 # Map reads for gold standard and with too low error rate.
@@ -21,17 +23,30 @@ ${RAZERS} -vv -of 4 -ds -i 96     -o razers2-adeno-hamming-04.sam adeno-genome.f
 ${RAZERS} -vv -of 4 -ds -i 96 -id -o razers2-adeno-edit-04.sam    adeno-genome.fa reads.fasta
 
 # ============================================================
+# Prepare SAM.
+# ============================================================
+
+${RABEMA_PREPARE} gold-adeno-hamming-08.sam > gold-adeno-hamming-08.by_qname.sam
+samtools view -Sb gold-adeno-hamming-08.by_qname.sam > gold-adeno-hamming-08.by_qname.bam
+samtools sort gold-adeno-hamming-08.by_qname.bam gold-adeno-hamming-08.by_coordinate
+samtools view gold-adeno-hamming-08.by_coordinate.bam > gold-adeno-hamming-08.by_coordinate.sam
+${RABEMA_PREPARE} gold-adeno-edit-08.sam > gold-adeno-edit-08.by_qname.sam
+samtools view -Sb gold-adeno-edit-08.by_qname.sam > gold-adeno-edit-08.by_qname.bam
+samtools sort gold-adeno-edit-08.by_qname.bam gold-adeno-edit-08.by_coordinate
+samtools view gold-adeno-edit-08.by_coordinate.bam > gold-adeno-edit-08.by_coordinate.sam
+
+# ============================================================
 # Build Gold Standard
 # ============================================================
 
-${RABEMA} build_standard -d hamming -e 8 -o gold-adeno-hamming-08.wit adeno-genome.fa gold-adeno-hamming-08.sam > gold-adeno-hamming-08.stdout
-${RABEMA} build_standard -d edit    -e 8 -o gold-adeno-edit-08.wit    adeno-genome.fa gold-adeno-edit-08.sam > gold-adeno-edit-08.stdout
+${RABEMA_BUILD} --distance-metric hamming -e 8 -o gold-adeno-hamming-08.gsi --reference adeno-genome.fa --in-sam gold-adeno-hamming-08.by_coordinate.sam > gold-adeno-hamming-08.stdout
+${RABEMA_BUILD} --distance-metric edit    -e 8 -o gold-adeno-edit-08.gsi    --reference adeno-genome.fa --in-sam gold-adeno-edit-08.by_coordinate.sam > gold-adeno-edit-08.stdout
 
 # ============================================================
 # Compare Against Gold Standard
 # ============================================================
 
-${RABEMA} compare -d hamming -e 8 adeno-genome.fa razers2-adeno-hamming-08.sam gold-adeno-hamming-08.wit > razers2-adeno-hamming-08.stdout
-${RABEMA} compare -d hamming -e 8 adeno-genome.fa razers2-adeno-hamming-04.sam gold-adeno-hamming-08.wit > razers2-adeno-hamming-04.stdout
-${RABEMA} compare -d edit    -e 8 adeno-genome.fa razers2-adeno-edit-08.sam    gold-adeno-edit-08.wit > razers2-adeno-edit-08.stdout
-${RABEMA} compare -d edit    -e 8 adeno-genome.fa razers2-adeno-edit-04.sam    gold-adeno-edit-08.wit > razers2-adeno-edit-04.stdout
+${RABEMA_EVALUATE} --distance-metric hamming -e 8 --reference adeno-genome.fa --in-sam razers2-adeno-hamming-08.sam --in-gsi gold-adeno-hamming-08.gsi > razers2-adeno-hamming-08.stdout
+${RABEMA_EVALUATE} --distance-metric hamming -e 8 --reference adeno-genome.fa --in-sam razers2-adeno-hamming-04.sam --in-gsi gold-adeno-hamming-08.gsi > razers2-adeno-hamming-04.stdout
+${RABEMA_EVALUATE} --distance-metric edit    -e 8 --reference adeno-genome.fa --in-sam razers2-adeno-edit-08.sam    --in-gsi gold-adeno-edit-08.gsi > razers2-adeno-edit-08.stdout
+${RABEMA_EVALUATE} --distance-metric edit    -e 8 --reference adeno-genome.fa --in-sam razers2-adeno-edit-04.sam    --in-gsi gold-adeno-edit-08.gsi > razers2-adeno-edit-04.stdout
