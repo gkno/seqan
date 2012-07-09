141d9ad28a2a88e1c11b42c400b562b409b8d38d
holtgrew <holtgrew@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-07-09 11:06:29 +0000

    [DOC] Adding more boxes to `TextBoxes` trac plugin.
    git-svn-id: http://svn.seqan.de/seqan/trunk@12257 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/misc/trac_plugins/TextBoxes/setup.py
+++ seqan/misc/trac_plugins/TextBoxes/setup.py
@@ -2,8 +2,12 @@ from setuptools import setup
 
 setup(
     name='TextBoxesMacros',
-    version='0.1.2',
+    version='0.1.3',
     packages=['text_boxes'],
-    package_data={'text_boxes' : ['htdocs/css/*.css']},
+    package_data={'text_boxes' : ['htdocs/css/*.css',
+                                  'htdocs/dialog-information.png',
+                                  'htdocs/dialog-warning.png',
+                                  'htdocs/emblem-important.png',
+                                  ]},
     entry_points = {'trac.plugins': ['textboxesmacros = text_boxes.macro']},
     )
--- seqan/misc/trac_plugins/TextBoxes/text_boxes/macro.py
+++ seqan/misc/trac_plugins/TextBoxes/text_boxes/macro.py
@@ -21,8 +21,11 @@ Success!
 
 import StringIO
 
+from pkg_resources import resource_filename
+
 from trac.core import *
 from trac.web.chrome import ITemplateProvider, add_stylesheet
+from trac.web.api import IRequestFilter
 from trac.wiki.macros import WikiMacroBase
 
 import trac.wiki
@@ -32,38 +35,55 @@ from genshi.builder import tag
 CLASSES = {
     'ShellBox' : 'shell_box',
     'IdeBox' : 'ide_box',
-    'WarningBox': 'system-message',  # Use trac class.
+    'WarningBox': 'warning_box',  # Use trac class.
+    'InfoBox': 'info_box',  # Use trac class.
+    'ImportantBox': 'important_box',  # Use trac class.
 }
 
-STYLES = {
-    'ShellBox' : 'background-color: black; color: lightgray;',
-    'IdeBox' : 'background-color: white; color: black; border:1px solid black;',
-    'WarningBox' : None,
+ICONS = {
+    'WarningBox': 'dialog-warning',
+    'InfoBox': 'dialog-information',
+    'ImportantBox': 'emblem-important',
 }
 
 class TextBoxMacro(WikiMacroBase):
-    implements(ITemplateProvider)
+    implements(ITemplateProvider, IRequestFilter)
     
     def get_macros(self):
         yield 'ShellBox'
         yield 'IdeBox'
         yield 'WarningBox'
+        yield 'InfoBox'
+        yield 'ImportantBox'
 
     def expand_macro(self, formatter, name, content, args):
+        add_stylesheet(formatter.req, 'text_boxes/css/text_boxes.css')
         # TODO(holtgrew): Actually, we would like to add a style sheet but this does not work. Thus we use a style tag below.
         #add_stylesheet(formatter.req, 'text_boxes/css/text_boxes.css')
         className = CLASSES.get(name, 'ShellBox')
-        if name == 'WarningBox':
+        if name in ['WarningBox', 'InfoBox', 'ImportantBox']:
             content_html = self.format_wiki(formatter, content)
-            return tag.div(genshi.core.Markup(content_html), class_=className, style=STYLES.get(name))
+            img = ''
+            if ICONS.get(name):
+                img = tag.img(src=formatter.href.chrome('text_boxes', '%s.png' % ICONS.get(name)), style='float: left;')
+            return tag.div(img + genshi.core.Markup(content_html) + tag.br(style='clear:both;'), class_=className)
         else:
-            return tag.pre(content, class_='wiki ' + className, style=STYLES.get(name))
+            return tag.pre(content, class_='wiki ' + className)
 
     def format_wiki(self, formatter, wiki_string):
         """Format the given string wiki_string to HTML."""
         out = StringIO.StringIO()
         trac.wiki.Formatter(self.env, formatter.context).format(wiki_string, out)
         return out.getvalue()
+
+    ### IRequestFilter methods
+    
+    def pre_process_request(self, req, handler):
+        return handler
+
+    def post_process_request(self, req, template, data, content_type):
+        add_stylesheet(req, 'text_boxes/css/text_boxes.css')
+        return template, data, content_type
 
     ### ITemplateProvider methods
 
