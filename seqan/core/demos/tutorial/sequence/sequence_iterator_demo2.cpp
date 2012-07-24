9368193e2a7a80df584605c4bf220b0f1ce88bd3
sabrina7 <sabrina7@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-07-24 13:18:26 +0000

    [DOC] Added demo for new iterator tutorial
    git-svn-id: http://svn.seqan.de/seqan/trunk@12384 e6417c60-b987-48fd-844e-b20f0fcc1017

---
+++ seqan/core/demos/tutorial/sequence/sequence_iterator_demo2.cpp
@@ -0,0 +1,40 @@
+// The comment lines containing FRAGMENT(fragment-line) are there for the
+// documentation system.  You can ignore them when reading this file.
+// FRAGMENT(includes)
+// This is the draft for the new iterator tutorial
+#include <iostream>
+#include <seqan/sequence.h>
+#include <seqan/file.h>
+
+using namespace seqan;
+
+int main() {
+// FRAGMENT(metafunctions)
+	Dna5String genome = "TATANNNGCGCG";
+	Iterator<Dna5String >::Type it = begin(genome);
+	Iterator<Dna5String >::Type itEnd = end(genome);
+// FRAGMENT(iterators)
+	while (it != itEnd) {
+		std::cout << *it;
+		++it;
+	}
+	std::cout << std::endl;
+// FRAGMENT(rooted-iterators)
+	Iterator<Dna5String, Rooted >::Type it2 = begin(genome);
+	for (goBegin(it2); !atEnd(it2); goNext(it2)) {
+		if (getValue(it2) == 'N')
+		    value(it2) = 'A';
+	}
+// FRAGMENT(iterator-reverse)
+	goEnd(it2);
+        while (!atBegin(it2)) {
+		goPrevious(it2);
+		std::cout << getValue(it2);
+	}
+	std::cout << std::endl;
+// FRAGMENT(assign-value)
+	assignValue(begin(genome), 'N');
+	std::cout << genome << std::endl;
+	
+	return 0;
+}
