96d957ff196226e9fd6645cf537c637828de8443
sabrina7 <sabrina7@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-08-07 09:04:01 +0000

    [DOC] Changed demo code for online tutorial sequences.
    git-svn-id: http://svn.seqan.de/seqan/trunk@12438 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/extras/demos/tutorial/sequences/assignmentSegments_solution.cpp
+++ seqan/extras/demos/tutorial/sequences/assignmentSegments_solution.cpp
@@ -1,42 +1,20 @@
 #include <iostream>
 #include <seqan/sequence.h>
+#include <seqan/file.h>
 
 using namespace seqan;
 
 // Function to print simple alignment between two sequences with the same length
-// .. for two sequences of the same type
-template <typename TText>
-void printAlign(TText const & genomeFragment, TText const & read)
+// .. for two sequences of different types
+template <typename TText1, typename TText2>
+void printAlign(TText1 const & genomeFragment, TText2 const & read)
 {
         std::cout <<  "Alignment " << std::endl;
         std::cout << "  genome : ";
-        for (unsigned j = 0; j < length(genomeFragment); ++j){
-            std::cout << genomeFragment[j];
-        }  
-        std::cout << std::endl;
+        std::cout << genomeFragment << std::endl;
         std::cout << "  read   : ";
-        for (unsigned j = 0; j < length(read); ++j){
-            std::cout << read[j];
-        }     
-        std::cout << std::endl;
+        std::cout << read << std::endl;
 }
-// .. for one infix and one other sequence type
-template <typename TInfix, typename TText>
-void printAlign(TInfix const & genomeFragment, TText const & read)
-{
-        std::cout <<  "Alignment " << std::endl;
-        std::cout << "  genome : ";
-        for (unsigned j = 0; j < length(genomeFragment); ++j){
-            std::cout << genomeFragment[j];
-        }  
-        std::cout << std::endl;
-        std::cout << "  read   : ";
-        for (unsigned j = 0; j < length(read); ++j){
-            std::cout << read[j];
-        }     
-        std::cout << std::endl;
-}
-
 
 int main()
 {
@@ -47,11 +25,9 @@ int main()
     // And the begin position of a given alignment between the read and the genome
     unsigned beginPosition = 1;
 
-    // Create Infix of type Dna5String
-    Infix<Dna5String>::Type inf;       
-    unsigned endPosition = begin + length(read);
-    // Get the corresponding infix sequence of genome
-    inf = infix(genome, begin, endPosition);
+    // Create Infix of type Dna5String and get the corresponding infix sequence of genome
+    Infix<Dna5String>::Type inf = infix(genome, beginPosition, beginPosition + length(read));
+    // Call of our function to print the simple alignment
     printAlign(inf, read);
     return 0;
 }
