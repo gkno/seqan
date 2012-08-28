6046e631851fe37806d6f1421abc5ab20cc34979
bkehr <bkehr@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-08-28 14:30:13 +0000

    [DOC] Remove code that is introduced later.
    git-svn-id: http://svn.seqan.de/seqan/trunk@12668 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/extras/demos/tutorial/first_steps/solution_4.cpp
+++ seqan/extras/demos/tutorial/first_steps/solution_4.cpp
@@ -5,16 +5,6 @@
 #include <seqan/sequence.h>
 #include <seqan/score.h>
 
-template <typename TText>
-int computeLocalScore(TText const & subText, seqan::String<seqan::AminoAcid> const & pattern)
-{
-    int localScore = 0;
-    for (unsigned i = 0; i < seqan::length(pattern); ++i)
-        localScore += seqan::score(seqan::Blosum62(), subText[i], pattern[i]);
-    
-    return localScore;
-}
-
 template <typename TText, typename TPattern>
 int computeLocalScore(TText const & subText, TPattern const & pattern)
 {
@@ -50,13 +40,6 @@ int main()
     seqan::String<char> text = "This is an awesome tutorial to get to now SeqAn!";
     seqan::String<char> pattern = "tutorial";
     seqan::String<int> score = computeScore(text, pattern);
-
-    print(text);
-    // > This is an awesome tutorial to get to now SeqAn!
-    print(pattern);
-    // > tutorial
     print(score);
-    // > 1 0 1 0 0 1 0 0 0 0 0 0 1 0 0 0 0 1 0 8 0 1 0 0 0 0 2 0 1 0 0 1 0 3 0 1 1 0 0 0 0 0 0 0 0 0 0 0
-
     return 0;
 }
