54db2bfb067d05dfc80acca2b0c3103cf526d884
ktrappe <ktrappe@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-08-30 13:51:47 +0000

    [DOC] Revised Iterators tutorial demo cpp files.
    git-svn-id: http://svn.seqan.de/seqan/trunk@12726 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/extras/demos/tutorial/iterators/iterators_assignment_3_workshop_solution.cpp
+++ seqan/extras/demos/tutorial/iterators/iterators_assignment_3_workshop_solution.cpp
@@ -18,8 +18,8 @@ int main(int, char const **)
     DnaString chr1 = "TATAATATTGCTATCGCGATATCGCTAGCTAGCTACGGATTATGCGCTCTGCGATATATCGCGCTAGATGTGCAGCTCGATCGAATGCACGTGTGTGCGATCGATTAGCGTCGATCATCGATCTATATTAGCGCGCGGTATCGGACGATCATATTAGCGGTCTAGCATTTAG";
 
     // Build List containing all reads
-    typedef String<DnaString> DnaList;
-    DnaList readList;
+    typedef String<DnaString> TDnaList;
+    TDnaList readList;
     resize(readList, 4);
     readList[0] = "TTGCTATCGCGATATCGCTAGCTAGCTACGGATTATGCGCTCTGCGATATATCGCGCT";
     readList[1] = "TCGATTAGCGTCGATCATCGATCTATATTAGCGCGCGGTATCGGACGATCATATTAGCGGTCTAGCATT";
@@ -36,7 +36,7 @@ int main(int, char const **)
         std::cout << readList[i] << std::endl;
 
     // Assume we have mapped the 4 reads to chr1 (and chr2) and now have the mapping start positions (no gaps).
-    // Store the start position in a String: 7, 100, 172, 272
+    // Store the start position in a String alignPosList: 7, 100, 172, 272
     String<unsigned> alignPosList;
     resize(alignPosList, 4);
     alignPosList[0] = 7;
@@ -48,7 +48,8 @@ int main(int, char const **)
     std::cout << " \n Print alignment using Segment: " << std::endl;
     for(unsigned i = 0; i < length(readList); ++i)
     {
-        // Begin and end position of a given alignment between the read and the genome
+        // Temporary copy of begin and end position (beginPosition) from alignPosList
+        // of a given alignment between the read and the genome
         unsigned beginPosition = alignPosList[i];
         unsigned endPosition = beginPosition + length(readList[i]);
         // Build infix
@@ -59,15 +60,16 @@ int main(int, char const **)
     // Iterators :)
     // Print alignments using Iterators: Do the same as above, but use Iterators to iterate over the read list.
     // First, use Standard Iterators.
-    Iterator<DnaList>::Type it = begin(readList);
-    Iterator<DnaList, Standard>::Type itEnd = end(readList); //same Iterator as above
+    Iterator<TDnaList>::Type it = begin(readList);
+    Iterator<TDnaList, Standard>::Type itEnd = end(readList); //same Iterator as above
     
     std::cout << " \n Print alignment using Standard Iterators: " << std::endl;
     for(; it != itEnd; goNext(it))
     {
         // Get the right index for alignPosList
         int i = position(it, readList);
-        // Begin and end position of a given alignment between the read and the genome
+        // Temporary copy of begin and end position (beginPosition) from alignPosList
+        // of a given alignment between the read and the genome
         unsigned beginPosition = alignPosList[i];
         unsigned endPosition = beginPosition + length(value(it));
         // Build Infix
