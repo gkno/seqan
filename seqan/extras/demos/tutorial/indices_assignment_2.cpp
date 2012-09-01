d171426642909c636c6227a713d6ec99a1d297bc
bkehr <bkehr@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-09-01 19:38:55 +0000

    [NOP] Fixing indentation in assignment solution.
    git-svn-id: http://svn.seqan.de/seqan/trunk@12763 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/extras/demos/tutorial/indices_assignment_2.cpp
+++ seqan/extras/demos/tutorial/indices_assignment_2.cpp
@@ -5,14 +5,14 @@ using namespace seqan;
 
 int main()
 {
-String<Dna5> genome = "TTATTAAGCGTATAGCCCTATAAATATAA";
-Index<String<Dna5>, IndexEsa<> > esaIndex(genome); 
-Finder<Index<String<Dna5>, IndexEsa<> > > esaFinder(esaIndex);
+    String<Dna5> genome = "TTATTAAGCGTATAGCCCTATAAATATAA";
+    Index<String<Dna5>, IndexEsa<> > esaIndex(genome); 
+    Finder<Index<String<Dna5>, IndexEsa<> > > esaFinder(esaIndex);
 
-while(find(esaFinder, "TATAA"))
-{
-    std::cout << position(esaFinder) << std::endl;
-}
+    while(find(esaFinder, "TATAA"))
+    {  
+        std::cout << position(esaFinder) << std::endl;
+    }
 
-return 0;
+    return 0;
 }
