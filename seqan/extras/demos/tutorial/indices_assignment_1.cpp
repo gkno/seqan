6d718526a77da944c37f76a439a650381d9fff83
bkehr <bkehr@e6417c60-b987-48fd-844e-b20f0fcc1017>
2012-09-01 19:36:39 +0000

    [NOP] Fixing indentation in assignment solution.
    git-svn-id: http://svn.seqan.de/seqan/trunk@12762 e6417c60-b987-48fd-844e-b20f0fcc1017

--- seqan/extras/demos/tutorial/indices_assignment_1.cpp
+++ seqan/extras/demos/tutorial/indices_assignment_1.cpp
@@ -5,17 +5,17 @@ using namespace seqan;
 
 int main()
 {
-// One possible solution to the first sub assignment
-String<Dna> text = "ACGTTTGACAGCT";
-Index<String<Dna>, IndexEsa<> > index(text);
+    // One possible solution to the first sub assignment
+    String<Dna> text = "ACGTTTGACAGCT";
+    Index<String<Dna>, IndexEsa<> > index(text);
 
-// One possible solution to the second sub assignment
-StringSet<String<Dna> > stringSet;
-appendValue(stringSet, "ACGTCATCAT");
-appendValue(stringSet, "ACTTTG");
-appendValue(stringSet, "CACCCCCCTATTT");
+    // One possible solution to the second sub assignment
+    StringSet<String<Dna> > stringSet;
+    appendValue(stringSet, "ACGTCATCAT");
+    appendValue(stringSet, "ACTTTG");
+    appendValue(stringSet, "CACCCCCCTATTT");
 
-Index<StringSet<String<Dna> >, IndexEsa<> > indexSet(stringSet);
+    Index<StringSet<String<Dna> >, IndexEsa<> > indexSet(stringSet);
 
-return 0;
+    return 0;
 }
